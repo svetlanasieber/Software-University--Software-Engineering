1fdb93b48f84394d0b7e4bebb9cf4a05916a001c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-21 22:48:52 +0200

    Create Demo.java

---
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Lab/Demo.java
@@ -0,0 +1,12 @@
+package Lek_BasicsSyntaxOverviews;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, Java!");
+
+    }
+}
