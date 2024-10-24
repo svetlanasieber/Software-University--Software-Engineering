c17db1389043f985c2c9e7ac168966450af9fa64
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-24 11:33:52 +0200

    Create Main.java

---
+++ JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/BookSell/Main.java
@@ -0,0 +1,10 @@
+package BookSell;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book myFirstBook = new Book("Harry Potter", "J.K.Rowling", 19.80);
+
+        myFirstBook.sell();
+    }
+}
