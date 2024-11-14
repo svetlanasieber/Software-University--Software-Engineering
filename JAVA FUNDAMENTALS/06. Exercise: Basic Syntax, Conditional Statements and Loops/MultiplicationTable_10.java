716a6de9f79d42cfcc9c65d1bb9440d7e27475b8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:13:13 +0100

    Update MultiplicationTable_10.java

--- JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/MultiplicationTable_10.java
+++ JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/MultiplicationTable_10.java
@@ -7,20 +7,15 @@ public class MultiplicationTable_10 {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
-        //for
-        //1. начало: 1
-        //2. край: 10
-        //3. повтаряме: отпечатваме number * друго число = резултат
-        //4. промяна: +1
+      
 
         for (int multiplier = 1; multiplier <= 10 ; multiplier++) {
             System.out.printf("%d X %d = %d%n", number, multiplier, number * multiplier);
-            //System.out.println(number + " X " + multiplier + " = " + number * multiplier);
+           
         }
 
-        //стоп: втория множител > 10
-        //продължавам: втория множител <= 10
-        int multiplier = 1; //втория множител
+    
+        int multiplier = 1; 
         while (multiplier <= 10) {
             System.out.printf("%d X %d = %d%n", number, multiplier, number * multiplier);
             multiplier++;
