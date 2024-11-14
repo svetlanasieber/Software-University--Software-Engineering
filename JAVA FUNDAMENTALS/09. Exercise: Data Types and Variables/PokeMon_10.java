7063c3557aee822aa5f035dfd2cd93401d90a735
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:18:28 +0100

    Update PokeMon_10.java

--- JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/PokeMon_10.java
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/PokeMon_10.java
@@ -15,11 +15,11 @@ public class PokeMon_10 {
 
         while (power >= distance) {
 
-            // 1. Отива до целта (power -= distance)
+         
             power -= distance;
-            // 2. Отброяваме успешно намушкване
+          
             countPokes++;
-            // 3. Проверяваме дали сме стигнали фактор на умора
+        
             if (power == startPower / 2) {
                 if (factor != 0) {
                     power /= factor;
