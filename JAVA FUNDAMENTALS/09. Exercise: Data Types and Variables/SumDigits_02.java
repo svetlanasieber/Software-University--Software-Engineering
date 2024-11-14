f3daba47f991f6b28824c0c830b5bd7c55f414be
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:19:12 +0100

    Update SumDigits_02.java

--- JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/SumDigits_02.java
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/SumDigits_02.java
@@ -9,10 +9,6 @@ public class SumDigits_02 {
         int number = Integer.parseInt(scanner.nextLine());
         int sum = 0;
 
-        // Повтарям: числото > 0
-        // 1. Вземам последната цифра от числото (числото % 10)
-        // 2. Прибавям последната цифра към сумата (sum += lastDigit)
-        // 3. Премахваме последната цифра от числото (числото / 10)
 
         while (number > 0) {
             int lastDigit = number % 10;
