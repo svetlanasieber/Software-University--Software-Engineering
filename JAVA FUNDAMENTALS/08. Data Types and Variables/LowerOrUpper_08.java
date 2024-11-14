3fbad201610da381fb04a0c828e5bf72edf32a7a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:16:16 +0100

    Update LowerOrUpper_08.java

--- JAVA FUNDAMENTALS/08. Data Types and Variables/LowerOrUpper_08.java
+++ JAVA FUNDAMENTALS/08. Data Types and Variables/LowerOrUpper_08.java
@@ -6,28 +6,12 @@ public class LowerOrUpper_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //на 0 позиция стои първата буква
         char symbol = scanner.nextLine().charAt(0);
 
-        //проверка дали буквата е главна или малка
-
-        //начин 1
-        /*if (Character.isUpperCase(symbol)) {
-            //главна буква
-            System.out.println("upper-case");
-        } else {
-            //малка буква
-            System.out.println("lower-case");
-        }*/
-
-        //начин 2
-
-        int code = (int) symbol; //аски код на въведения символ
+        int code = (int) symbol; 
         if (code >= 97 && code <= 122) {
-            //малка буква - код: [97; 122]
             System.out.println("lower-case");
         } else if (code >= 65 && code <= 90) {
-            //голяма буква - код: [65; 90]
             System.out.println("upper-case");
         }
     }
