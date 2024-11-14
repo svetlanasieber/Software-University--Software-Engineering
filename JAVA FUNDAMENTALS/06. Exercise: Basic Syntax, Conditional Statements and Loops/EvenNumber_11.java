77f3d3224787a21eea4f82ffb0bfb7bb582594ae
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:12:22 +0100

    Update EvenNumber_11.java

--- JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/EvenNumber_11.java
+++ JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/EvenNumber_11.java
@@ -6,22 +6,17 @@ public class EvenNumber_11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //while
-        //повтаряме: въвеждаме цяло число
-        //стоп: число е четно
-        //продължаваме: число е нечетно
 
         int number = Integer.parseInt(scanner.nextLine());
 
         while (number % 2 != 0) {
-            //нечетно число
+      
             System.out.println("Please write an even number.");
 
-            //прочитаме
             number = Integer.parseInt(scanner.nextLine());
         }
 
-        //четно число -> прекратили цикъла
+     
         System.out.println("The number is: " + Math.abs(number));
 
     }
