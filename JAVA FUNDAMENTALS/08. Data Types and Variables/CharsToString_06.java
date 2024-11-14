bbaf9eb488cd4fa474d63d10a77674e054f2e32d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:14:51 +0100

    Update CharsToString_06.java

--- JAVA FUNDAMENTALS/08. Data Types and Variables/CharsToString_06.java
+++ JAVA FUNDAMENTALS/08. Data Types and Variables/CharsToString_06.java
@@ -10,10 +10,7 @@ public class CharsToString_06 {
         char secondSymbol = scanner.nextLine().charAt(0);
         char thirdSymbol = scanner.nextLine().charAt(0);
 
-        //начин 1
-        //System.out.println("" + firstSymbol + secondSymbol + thirdSymbol);
-
-        //начин 2
+      
         System.out.printf("%c%c%c", firstSymbol, secondSymbol, thirdSymbol);
 
     }
