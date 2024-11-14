5fd65c1b316892c77eda4915bbba3ef9c42a7601
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-14 17:13:35 +0100

    Update StudentInformation_01.java

--- JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/StudentInformation_01.java
+++ JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/StudentInformation_01.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class StudentInformation_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine(); //име на студента
-        int age = Integer.parseInt(scanner.nextLine()); //възраст на судента
-        double averageGrade = Double.parseDouble(scanner.nextLine()); //средна оценка на студента
+        String name = scanner.nextLine(); 
+        int age = Integer.parseInt(scanner.nextLine()); 
+        double averageGrade = Double.parseDouble(scanner.nextLine()); 
 
         System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, averageGrade);
     }
