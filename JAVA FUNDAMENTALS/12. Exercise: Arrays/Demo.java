ddb28077a480f00f8dcbf424498af79b3e2162fe
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-23 18:17:35 +0100

    Update Demo.java

--- JAVA FUNDAMENTALS/12. Exercise: Arrays/Demo.java
+++ JAVA FUNDAMENTALS/12. Exercise: Arrays/Demo.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Demo {
     public static void main(String[] args) {
 
-        //изпълнител -> песен -> времетраене в мин
+    
        Map<String, Person> singerSongsTime = new LinkedHashMap<>();
     }
 }
