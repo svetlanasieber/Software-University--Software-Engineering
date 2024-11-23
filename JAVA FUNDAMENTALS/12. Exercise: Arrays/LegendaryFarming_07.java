06b9a259813709cf84db4d9f4c5221c9c0ce78bc
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-23 18:19:00 +0100

    Update LegendaryFarming_07.java

--- JAVA FUNDAMENTALS/12. Exercise: Arrays/LegendaryFarming_07.java
+++ JAVA FUNDAMENTALS/12. Exercise: Arrays/LegendaryFarming_07.java
@@ -19,17 +19,17 @@ public class LegendaryFarming_07 {
 
         while (!isLegendaryItemBought) {
 
-            //3 Motes 5 stones 5 Shards
+          
             String input = scanner.nextLine();
             String[] inputData = input.split(" ");
 
             for (int index = 0; index <= inputData.length - 1; index += 2) {
 
-                // 1. Вземаме количество и име на предмета
+              
                 int quantity = Integer.parseInt(inputData[index]);
                 String material = inputData[index + 1].toLowerCase();
 
-                // 2. Проверка дали предмета е важен
+             
                 if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
                     int existingQuantity = materials.get(material);
                     materials.put(material, existingQuantity + quantity);
@@ -42,7 +42,7 @@ public class LegendaryFarming_07 {
                     }
                 }
 
-                // 3. Проверка дали не сме събрали 250 броя от важните предмети
+              
                 for (Map.Entry<String, Integer> entry : materials.entrySet()) {
 
                     if (entry.getValue() >= 250) {
