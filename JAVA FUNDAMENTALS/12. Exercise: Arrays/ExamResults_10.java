41f996e005ba4baec8f06eb0d9c68015819a5778
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-23 18:18:13 +0100

    Update ExamResults_10.java

--- JAVA FUNDAMENTALS/12. Exercise: Arrays/ExamResults_10.java
+++ JAVA FUNDAMENTALS/12. Exercise: Arrays/ExamResults_10.java
@@ -7,24 +7,23 @@ import java.util.Scanner;
 public class ExamResults_10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //username -> най-височия брой изкарани точки
+      
         Map<String, Integer> userPoints = new LinkedHashMap<>();
-        //language -> брой на събмитите
+     
         Map<String, Integer> languageCount = new LinkedHashMap<>();
 
         String input = scanner.nextLine();
         while (!input.equals("exam finished")) {
             String username = input.split("-")[0];
             if (input.contains("banned")) {
-                //КОМАНДА ЗА БАНВАНЕ
-                //input = "{username}-banned"
+              
                 userPoints.remove(username);
             } else {
-                //input = "{username}-{language}-{points}"
+             
                 String language = input.split("-")[1];
                 int points = Integer.parseInt(input.split("-")[2]);
 
-                //проверка дали този потребител е предавал нещо
+               
                 if (!userPoints.containsKey(username)) {
                     userPoints.put(username, points);
                 } else {
@@ -34,7 +33,6 @@ public class ExamResults_10 {
                     }
                 }
 
-                //проверка за програмния език
                 if (!languageCount.containsKey(language)) {
                     languageCount.put(language, 1);
                 } else {
@@ -46,14 +44,14 @@ public class ExamResults_10 {
         }
 
 
-        //1. отпечатваме потребителите и техните точки
+      
         System.out.println("Results:");
-        //userPoints: key(username) -> value (max points)
+      
         userPoints.entrySet().forEach(entry -> System.out.println(entry.getKey() + " | " + entry.getValue()));
 
-        //2. отпечатваме програмните езици и техния брой решения
+      
         System.out.println("Submissions:");
-        //languageCount: key(language) -> value(бр. решения)
+  
         languageCount.entrySet().forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
     }
 }
