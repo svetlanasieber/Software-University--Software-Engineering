aa314a51143611f0ca92ae876477b40335d9f6e9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-23 18:27:25 +0100

    Update MiddleCharacters_06.java

--- JAVA FUNDAMENTALS/14. Methods/MiddleCharacters_06.java
+++ JAVA FUNDAMENTALS/14. Methods/MiddleCharacters_06.java
@@ -16,20 +16,12 @@ public class MiddleCharacters_06 {
         int length = text.length();
 
         if (length % 2 != 0) {
-            //aString -> 'r' -> 3 index
-            //length 7
-            //Vik -> 'i' -> 1 index
-            //length 3
-            //Carts -> 'r' ->  2 index
-            //length 5
+           
 
             int indexOfMiddleChar = length / 2;
             System.out.println(text.charAt(indexOfMiddleChar));
         } else {
-            //Desi -> 'e' -> 1 index ; 's' -> 2 index
-            //length 4
-            //Viktor -> 'k' -> 2 index ; 't' -> 3 index
-            //length 6
+        
 
             int indexOfFirstMiddleChar = length / 2 - 1;
             int indexOfSecondMiddleChar = length / 2;
