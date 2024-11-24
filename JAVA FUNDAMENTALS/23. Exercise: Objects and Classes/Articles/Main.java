05fd018d70bebfa63cd11e0b0c173110afb94f7f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 14:46:53 +0100

    Update Main.java

--- JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Articles/Main.java
+++ JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Articles/Main.java
@@ -6,22 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String articleData = scanner.nextLine(); // "Holy Ghost, content, John Sandford"
-        //"Holy Ghost, content, John Sandford".split(", ") -> ["Holy Ghost", "content", "John Sandford"]
-        String title = articleData.split(", ")[0]; //заглавие
-        String content = articleData.split(", ")[1]; //съдържание
-        String author = articleData.split(", ")[2]; //автор
+        String articleData = scanner.nextLine(); 
+      
+        String title = articleData.split(", ")[0]; 
+        String content = articleData.split(", ")[1]; 
+        String author = articleData.split(", ")[2]; 
 
         Article article = new Article(title, content, author);
 
-        int countCommands = Integer.parseInt(scanner.nextLine()); //брой команди за изпълнение
+        int countCommands = Integer.parseInt(scanner.nextLine()); 
         for (int count = 1; count <= countCommands; count++) {
             String command = scanner.nextLine();
-            //1. "Edit: Test".split(": ")               -> ["Edit", "Test"]
-            //2. "ChangeAuthor: Ivan Vazov".split(": ") -> ["ChangeAuthor", "Ivan Vazov"]
-            //3. "Rename: Title 1".split(": ")          -> ["Rename", "Title 1"]
-            String commandName = command.split(": ")[0]; // "Edit", "ChangeAuthor", "Rename"
-            String parameter = command.split(": ")[1]; //параметър, с който работи командата
+            
+            String commandName = command.split(": ")[0]; 
+            String parameter = command.split(": ")[1]; 
 
             switch (commandName) {
                 case "Edit" -> article.edit(parameter);
