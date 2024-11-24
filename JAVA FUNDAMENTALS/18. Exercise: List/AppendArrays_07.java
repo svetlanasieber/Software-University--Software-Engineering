2ee606e0bfff448364e76e8a97fa7c0e732cb4d3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 13:45:38 +0100

    Update AppendArrays_07.java

--- JAVA FUNDAMENTALS/18. Exercise: List/AppendArrays_07.java
+++ JAVA FUNDAMENTALS/18. Exercise: List/AppendArrays_07.java
@@ -8,18 +8,18 @@ public class AppendArrays_07 {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        //"7 | 4  5|1 0| 2 5 |3".split("|") -> ["7 ", " 4 5", "1 0", " 2 5 ", "3"]
+
 
         List<String> texts = Arrays.stream(input.split("\\|")).collect(Collectors.toList());
-        //texts = {"7 ", " 4 5", "1 0", " 2 5 ", "3"}
-        Collections.reverse(texts);
-        //резултат: texts = {"3", " 2 5 ", "1 0", " 4 5", "7 "}
 
-        System.out.println(texts.toString() //"[3,  2 5 , 1 0,  4  5, 7 ]"
-                        .replace("[", "") //"3,  2 5 , 1 0,  4  5, 7 ]"
-                        .replace("]", "") //"3,  2 5 , 1 0,  4  5, 7 "
-                        .replaceAll(",", "") //"3  2 5  1 0  4  5 7 "
-                        .replaceAll("\\s+", " ") //"3 2 5 1 0 4 5 7 "
+        Collections.reverse(texts);
+     
+
+        System.out.println(texts.toString() 
+                        .replace("[", "") 
+                        .replace("]", "")
+                        .replaceAll(",", "")
+                        .replaceAll("\\s+", " ") 
                         .trim()); //"3 2 5 1 0 4 5 7"
 
     }
