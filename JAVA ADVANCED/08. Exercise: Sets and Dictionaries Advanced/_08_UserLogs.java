e7a3a53ec8fd7a1e6e3b5aa3aeac5eb57c9752ca
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 15:24:27 +0100

    Update _08_UserLogs.java

--- JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/_08_UserLogs.java
+++ JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/_08_UserLogs.java
@@ -12,28 +12,26 @@ public class _08_UserLogs {
         String input = scanner.nextLine();
         while (!input.equals("end")) {
 
-            //IP=192.23.30.41 message='Hello&yall.' user=destroyer
+         
             String IP = input.split(" ")[0].split("=")[1]; // 192.23.30.41
             String username = input.split(" ")[2].split("=")[1]; // destroyer
 
-            // Проверявам имам ли запис за този потребите
+         
             if (tracking.containsKey(username)) {
-                // а. Ако има:
-                //    -> вземам мапа за този потребител (IP и брой пъти изпратено съобщение за този IP адрес)
+          
                 Map<String, Integer> IPAndCount = tracking.get(username);
                 if (IPAndCount.containsKey(IP)) {
-                    //    -> Ако потребителя е пращал съобщение от този IP адрес, увеличавам бройката с 1
+                
                     int count = IPAndCount.get(IP) + 1;
                     IPAndCount.put(IP, count);
                 } else {
-                    //    -> Ако не е –> нов запис, че е пратил 1 съобщение от този IP адрес
+                 
                     IPAndCount.put(IP, 1);
                 }
             } else {
-                // б. Ако няма:
-                //    -> правя нов запис с празен мап за неговите IP адреси
+             
                 tracking.put(username, new LinkedHashMap<>());
-                //    -> добавям към празният мап на този потребите, че е изпратил съобщение 1 от този IP адрес (IP -> 1)
+         
                 tracking.get(username).put(IP, 1);
             }
             input = scanner.nextLine();
@@ -44,10 +42,9 @@ public class _08_UserLogs {
             String username = entry.getKey();
             Map<String, Integer> IPAndCounts = entry.getValue();
 
-            //{IP} => {count}
-            // .map -> променя от нещо към нещо
-            String output = IPAndCounts.entrySet().stream()  // съвкупност от записи
-                    .map(IPentry -> String.format("%s => %d", IPentry.getKey(), IPentry.getValue())) // съвкупност от текстове
+       
+            String output = IPAndCounts.entrySet().stream()  
+                    .map(IPentry -> String.format("%s => %d", IPentry.getKey(), IPentry.getValue()))
                     .collect(Collectors.joining(", "));
 
             System.out.println(username + ":");
