b387c98584e1fec0ed9a75da1bc08bd80935c461
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 14:49:48 +0100

    Update Main.java

--- JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Students/Main.java
+++ JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Students/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Student> studentList = new ArrayList<>(); //празен списък
+        List<Student> studentList = new ArrayList<>(); 
         int countStudents = Integer.parseInt(scanner.nextLine());
 
         for (int count = 1; count <= countStudents; count++) {
             String personalData = scanner.nextLine();
-            //"Lakia Eason 3.90".split(" ") -> ["Lakia", "Eason", "3.90"]
-            String[] splitData = personalData.split(" "); //["Lakia", "Eason", "3.90"]
+         
+            String[] splitData = personalData.split(" "); 
             String firstName = splitData[0];
             String lastName = splitData[1];
             double grade = Double.parseDouble(splitData[2]);
@@ -21,22 +21,15 @@ public class Main {
             studentList.add(student);
         }
 
-        //лист със студенти
-
-        //сортираме
-        //ascending order -> нарастващ ред
-        //descending order -> намаляващ ред
+      
         studentList
-                .sort(Comparator.comparingDouble(Student::getGrade) //ascending order
+                .sort(Comparator.comparingDouble(Student::getGrade) 
                 .reversed()); //descending order
 
-        //отпечатаме
-        //"{first name} {last name}: {grade}"
+   
         for (Student student : studentList) {
-            //начин 1: отпечатване на обект -> достъпваме всички полета и ги отпечатваме
-            //System.out.printf("%s %s: %.2f", student.getFirstName(), student.getLastName(), student.getGrade());
+        
 
-            //начин 2
             System.out.println(student.toString());
         }
     }
