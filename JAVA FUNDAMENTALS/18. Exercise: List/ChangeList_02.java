d988e61885ab7a59da876d4cbd04079b91b7d93b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 13:46:43 +0100

    Update ChangeList_02.java

--- JAVA FUNDAMENTALS/18. Exercise: List/ChangeList_02.java
+++ JAVA FUNDAMENTALS/18. Exercise: List/ChangeList_02.java
@@ -13,18 +13,16 @@ public class ChangeList_02 {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        //"Delete {element}" - delete all elements in the array which are equal to the given element
-        //"Insert {element} {position}" - insert element at the given position
 
         String command = scanner.nextLine();
         while (!command.equals("end")) {
 
             if (command.contains("Delete")) {
-                // 1. "Delete {element}".split(" ") -> ["Delete", "45"]
+            
                 int element = Integer.parseInt(command.split(" ")[1]);
                 numbers.removeAll(List.of(element));
             } else if (command.contains("Insert")) {
-                // 2. "Insert {element} {position}".split(" ") -> ["Insert", "29", "2"]
+              
                 int element = Integer.parseInt(command.split(" ")[1]);
                 int position = Integer.parseInt(command.split(" ")[2]);
                 numbers.add(position, element);
