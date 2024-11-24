f73cd1dd95a80814201e1447a0658254afcb58b0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 13:37:16 +0100

    Update CharactersInRange_03.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/CharactersInRange_03.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/CharactersInRange_03.java
@@ -14,14 +14,14 @@ public class CharactersInRange_03 {
     }
 
     public static void printCharactersInRange (char firstSymbol, char secondSymbol) {
-        //започваме със символа, който е с по-малък аски код
+
         if (firstSymbol < secondSymbol) {
-            //започваме от firstSymbol -> приключваме в secondSymbol
+          
             for (int ascii = firstSymbol + 1; ascii < secondSymbol; ascii++) {
                 System.out.print((char)ascii + " ");
             }
         } else { //firstSymbol >= secondSymbol
-            //започваме от secondSymbol -> приключваме в firstSymbol
+           
             for (char symbol = (char)(secondSymbol + 1); symbol < firstSymbol; symbol++) {
                 System.out.print(symbol + " ");
             }
