420ca57be075f279ae9ac4044d1203a78e9acad4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 14:13:31 +0100

    Update ShoppingList_02.java

--- JAVA FUNDAMENTALS/20. Exam Preparation/ShoppingList_02.java
+++ JAVA FUNDAMENTALS/20. Exam Preparation/ShoppingList_02.java
@@ -9,10 +9,10 @@ public class ShoppingList_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //списък с продукти
-        List<String> productsList = Arrays.stream(scanner.nextLine()      //"Tomatoes!Potatoes!Bread"
-                                    .split("!"))                    //["Tomatoes", "Potatoes", "Bread"]
-                                    .collect(Collectors.toList());         //{"Tomatoes", "Potatoes", "Bread"}
+       
+        List<String> productsList = Arrays.stream(scanner.nextLine()     
+                                    .split("!"))                   
+                                    .collect(Collectors.toList());        
 
 
         String command = scanner.nextLine();
@@ -20,36 +20,31 @@ public class ShoppingList_02 {
         while (!command.equals("Go Shopping!")) {
 
             String [] commandParts = command.split(" ");
-            String commandName = commandParts[0]; //"Urgent", "Unnecessary", "Correct", "Rearrange"
-            String item = commandParts[1]; //името на артикула, с който ще работим в командата
+            String commandName = commandParts[0]; 
+            String item = commandParts[1]; 
 
-            //проверка коя е въведената команда
             switch (commandName) {
                 case "Urgent":
-                    //добавя артикула в началото на списъка, ако го няма
-                    //1. command = "Urgent {item}".split(" ")-> ["Urgent", "{item}"]
+     
                     if (!productsList.contains(item)) {
-                        productsList.add(0, item); //вмъкваме на определено място в списъка
+                        productsList.add(0, item);
                     }
                     break;
                 case "Unnecessary":
-                    //премахваме артикула от списъка, ако го има
-                    //2. command = "Unnecessary {item}".split(" ")-> ["Unnecessary", "{item}"]
-                    productsList.remove(item); //премахва само ако елементът е в списъка
+                 
+                    productsList.remove(item); 
                     break;
                 case "Correct":
-                    //item = артикул, който ще заменяме
-                    //3. command = "Correct {oldItem} {newItem}".split(" ") -> ["Correct", "{item}", "{newItem}"]
-                    String newItem = commandParts[2]; //артикул, който ще заменя item
-                    //заменяме артикула с новия в списъка, ако го има
+                 
+                    String newItem = commandParts[2]; 
+                   
                     if (productsList.contains(item)) {
                         int position = productsList.indexOf(item);
-                        productsList.set(position, newItem); //замяна
+                        productsList.set(position, newItem); 
                     }
                     break;
                 case "Rearrange":
-                    //4. command = "Rearrange {item}".split(" ")            -> ["Rearrange", "{item}"]
-                    //премахваме артикула от списъка и го добавяме в края, ако го има
+              
                     if (productsList.contains(item)) {
                         productsList.remove(item);
                         productsList.add(item);
@@ -60,26 +55,13 @@ public class ShoppingList_02 {
             command = scanner.nextLine();
         }
 
-        //отпечатваме артикули в списъка разделени със запетая и интервал
-        //начин 1: String.join -> работи само за списък от текстове
+
         System.out.println(String.join(", ", productsList));
 
-        //начин 2: productsList = {"Tomatoes, Potatoes, Lettuce"}
-        /*System.out.println(productsList.toString() //["Tomatoes, Potatoes, Lettuce"]
-                .replace("[", "")  //"Tomatoes, Potatoes, Lettuce"]
-                .replace("]", "")); //"Tomatoes, Potatoes, Lettuce"*/
 
-        //начин 3: productsList = {"Tomatoes, Potatoes, Lettuce"}
-        /*for (int position = 0; position <= productsList.size() - 1; position++) {
-            String currentItem = productsList.get(position); //текущия артикул
-            //само след последния елемент да не слагаме ", "
-            if (position == productsList.size() - 1) {
-                //последната позиция = последния елемент
-                System.out.print(currentItem);
-            } else {
-                System.out.print(currentItem + ", ");
-            }
-        }*/
+                .replace("[", "")  
+                .replace("]", "")); 
 
-    }
+        
+        }
 }
