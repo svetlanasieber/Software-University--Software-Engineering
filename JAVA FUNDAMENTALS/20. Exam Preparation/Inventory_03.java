e15a71ec33b374e9400461d02447004925592243
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 14:11:47 +0100

    Update Inventory_03.java

--- JAVA FUNDAMENTALS/20. Exam Preparation/Inventory_03.java
+++ JAVA FUNDAMENTALS/20. Exam Preparation/Inventory_03.java
@@ -9,28 +9,27 @@ public class Inventory_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<String> elementsList = Arrays.stream(scanner.nextLine() //"Iron, Wood, Sword"
-                                                  .split(", ")) //["Iron", "Wood", "Sword"]
-                                                  .collect(Collectors.toList()); //{"Iron", "Wood", "Sword"}
+        List<String> elementsList = Arrays.stream(scanner.nextLine() 
+                                                  .split(", ")) 
+                                                  .collect(Collectors.toList()); 
 
         String command = scanner.nextLine();
 
         while (!command.equals("Craft!")) {
 
             if (command.contains("Collect")) {
-                //command = "Collect - {item}".split(" - ") -> ["Collect", "{item}"]
+              
                 String elementToAdd = command.split(" - ")[1];
                 if (!elementsList.contains(elementToAdd)) {
                     elementsList.add(elementToAdd);
                 }
             } else if (command.contains("Drop")) {
-                //command = "Drop - {item}".split(" - ") -> ["Drop", "{item}"]
+           
                 String elementToRemove = command.split(" - ")[1];
-                elementsList.remove(elementToRemove); //remove -> 1. проверява има ли го този елемент   2. премахва
+                elementsList.remove(elementToRemove); 
             } else if (command.contains("Combine Items")) {
-                //command = "Combine Items - {old_item}:{new_item}"
-                //command = "Combine Items - Sword:Bow".split(" - ") -> ["Combine Items", "Sword:Bow"]
-                //[1] -> "Sword:Bow".split(":") -> ["Sword", "Bow"]
+               
+  
                 String oldElement = command.split(" - ")[1].split(":")[0];
                 String newElement = command.split(" - ")[1].split(":")[1];
 
@@ -40,7 +39,7 @@ public class Inventory_03 {
                 }
 
             } else if (command.contains("Renew")) {
-                //command = "Renew – {item}".split(" - ") -> ["Renew", "{item}"]
+              
                 String elementToMove = command.split(" - ")[1];
                 if (elementsList.contains(elementToMove)) {
                     elementsList.remove(elementToMove);
