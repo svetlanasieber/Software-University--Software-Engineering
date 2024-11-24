61871e1f9bf9a53baba871959ca73bfd3b032a5c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 15:25:04 +0100

    Update _12_SerbeanUnleashed.java

--- JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/_12_SerbeanUnleashed.java
+++ JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/_12_SerbeanUnleashed.java
@@ -11,7 +11,7 @@ public class _12_SerbeanUnleashed {
 
         while (!input.equals("End")) {
 
-            //Lepa Brena @Sunny Beach 25 3500
+    
             if (!input.contains("@") || input.charAt(input.indexOf("@") - 1) != ' ') {
                 input = scanner.nextLine();
                 continue;
@@ -19,7 +19,7 @@ public class _12_SerbeanUnleashed {
 
             String singer = input.substring(0, input.indexOf("@") - 1);
             int firstIndexOfNumber = 0;
-            //@Sunny Beach 25
+        
             for (int i = input.indexOf("@"); i < input.length(); i++) {
                 char currentSymbol = input.charAt(i);
                 if (Character.isDigit(currentSymbol) && input.charAt(i - 1) == ' ') {
@@ -34,7 +34,7 @@ public class _12_SerbeanUnleashed {
             }
 
             String venue = input.substring(input.indexOf("@") + 1, firstIndexOfNumber - 1);
-            String numbers = input.substring(firstIndexOfNumber);//"25 3000"
+            String numbers = input.substring(firstIndexOfNumber);
             String[] numbersArray = numbers.split("\\s+");
             if (numbersArray.length != 2) {
                 input = scanner.nextLine();
@@ -43,7 +43,7 @@ public class _12_SerbeanUnleashed {
             int price = Integer.parseInt(numbersArray[0]);
             int capacity = Integer.parseInt(numbersArray[1]);
 
-            //venue -> {singer -> price}
+        
 
             if (!concerts.containsKey(venue)) {
                 LinkedHashMap<String, Integer> singers = new LinkedHashMap<>();
