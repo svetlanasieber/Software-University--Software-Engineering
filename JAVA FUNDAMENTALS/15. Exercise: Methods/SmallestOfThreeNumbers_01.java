c3b57e9b936974ec55374f651bc7d32d0afdcf72
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 13:40:49 +0100

    Update SmallestOfThreeNumbers_01.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/SmallestOfThreeNumbers_01.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/SmallestOfThreeNumbers_01.java
@@ -6,7 +6,7 @@ public class SmallestOfThreeNumbers_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //входни данни
+     
         int firstNumber = Integer.parseInt(scanner.nextLine());
         int secondNumber = Integer.parseInt(scanner.nextLine());
         int thirdNumber = Integer.parseInt(scanner.nextLine());
@@ -14,7 +14,7 @@ public class SmallestOfThreeNumbers_01 {
         System.out.println(getSmallestNumber(firstNumber, secondNumber, thirdNumber));
     }
 
-    //метод, който отпечатва най-малкото число
+   
     private static int getSmallestNumber (int n1, int n2, int n3) {
         return Math.min(Math.min(n1, n2), n3);
     }
