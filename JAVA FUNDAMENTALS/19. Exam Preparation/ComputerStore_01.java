0ab1cef85bf2cc8a8c5309259cecd42a0a3d7674
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 13:52:52 +0100

    Update ComputerStore_01.java

--- JAVA FUNDAMENTALS/19. Exam Preparation/ComputerStore_01.java
+++ JAVA FUNDAMENTALS/19. Exam Preparation/ComputerStore_01.java
@@ -11,31 +11,28 @@ public class ComputerStore_01 {
         double priceWithoutTaxes = 0;
         double taxes = 0;
 
-        //Създаваме си while цикъл докато не получим команда special or regular
+      
         while (!command.equals("special") && !command.equals("regular")){
-            //превръщаме прочетения стринг в double -> това ще бъде цената за един компонент
+           
             double currentPrice = Double.parseDouble(command);
 
-            //проверяваме ако цената е отрицателно число -> принтираме "Invalid price" -> продължаваме да четем от конзолата
+           
             if(currentPrice < 0){
                 System.out.println("Invalid price!");
                 command = scanner.nextLine();
                 continue;
             }
 
-            //добавяме текущата цена към общата сума без такси
             priceWithoutTaxes += currentPrice;
 
-            //пресмятаме и добавяме таксата за текущата покупка
+          
             taxes += currentPrice * 0.20;
 
             command = scanner.nextLine();
         }
 
-        //пресметнем колко е общата сума за всички компоненти + такси
         double totalSum = priceWithoutTaxes + taxes;
 
-        //проверяваме дали клиентът е special -> пресмятаме 10% отстъпка
         if(command.equals("special")){
             totalSum = totalSum * 0.9;
         }
@@ -46,14 +43,6 @@ public class ComputerStore_01 {
                 String.format("-----------%n") +
                 String.format("Total price: %.2f$", totalSum);
 
-
-        //Congratulations you've just bought a new computer!
-        //Price without taxes: {total price without taxes}$
-        //Taxes: {total amount of taxes}$
-        //-----------
-        //Total price: {total price with taxes}$"
-
-        //проверим дали общата сума е <= 0 -> принтираме "Invalid order"
         if(totalSum <= 0){
             System.out.println("Invalid order!");
         }else {
