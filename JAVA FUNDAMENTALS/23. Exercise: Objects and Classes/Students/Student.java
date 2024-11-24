24a963fe9916fbb4a8c30439aa39386693bdfd66
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-24 14:50:09 +0100

    Update Student.java

--- JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Students/Student.java
+++ JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Students/Student.java
@@ -1,39 +1,35 @@
 package Students;
 
 public class Student {
-    //полета -> характеристики
+ 
     private String firstName;
     private String lastName;
     private double grade;
 
-    //конструктор -> създаваме обекти
+
     public Student (String firstName, String lastName, double grade) {
-        //нов празен обект
+ 
         this.firstName = firstName;
         this.lastName = lastName;
         this.grade = grade;
     }
 
-    //методи -> действия
 
-    //getters -> осигуряват достъп до стойността в полето
-
-    //getter за полето firstName
     public String getFirstName() {
         return this.firstName;
     }
 
-    //getter за полето lastName
+
     public String getLastName() {
         return this.lastName;
     }
 
-    //getter за полето grade
+  
     public double getGrade() {
         return this.grade;
     }
 
-    //toString -> вграден метод; преобразува обекта в текст -> "{package}.{class}@{address}"
+
     @Override
     public String toString() {
         //представя обекта по формата на текст по мой избор
