f7438a30cca53d44acc06cf1b389486843791738
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-25 15:35:25 +0100

    Create ArrayRecap_01.java

---
+++ JAVA FUNDAMENTALS/12. Exercise: Arrays/ArrayRecap_01.java
@@ -0,0 +1,22 @@
+package G03_Arrays;
+
+import java.util.Scanner;
+
+public class ArrayRecap_01 {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        numbers[0] = 5;
+        numbers[1] = 20;
+        numbers[2] = 25; //numbers: [5, 20, 25]
+
+        Scanner scanner = new Scanner(System.in);
+        String[] towns = scanner.nextLine().split(" ");
+
+        System.out.println();
+    }
+
+
+
+}
