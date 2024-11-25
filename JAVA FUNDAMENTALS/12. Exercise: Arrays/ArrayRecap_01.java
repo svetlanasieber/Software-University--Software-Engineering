6363f8307e828e37cb20e22c447301edab2a0b39
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-25 15:41:33 +0100

    Update ArrayRecap_01.java

--- JAVA FUNDAMENTALS/12. Exercise: Arrays/ArrayRecap_01.java
+++ JAVA FUNDAMENTALS/12. Exercise: Arrays/ArrayRecap_01.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class ArrayRecap_01 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[3];
 
         numbers[0] = 5;
         numbers[1] = 20;
         numbers[2] = 25; //numbers: [5, 20, 25]
 
-        Scanner scanner = new Scanner(System.in);
+
         String[] towns = scanner.nextLine().split(" ");
 
-        System.out.println();
+        //System.out.println();
+
+        // foreach loop
+        for (String town : towns) {
+            System.out.println(town + " is a nice city.");
+
+            
+        }
     }
 
 
