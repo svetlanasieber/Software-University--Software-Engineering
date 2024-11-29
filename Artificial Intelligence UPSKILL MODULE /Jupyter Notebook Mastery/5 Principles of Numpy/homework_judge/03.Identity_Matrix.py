62a51539eeb9b2684211aa31111346d1ce6334c7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-29 15:33:41 +0100

    Delete Artificial Intelligence UPSKILL MODULE  directory

--- Artificial Intelligence UPSKILL MODULE /Jupyter Notebook Mastery/5 Principles of Numpy/homework_judge/03.Identity_Matrix.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def create_and_print_identity_matrix(N):
-    identity_matrix = np.eye(N)
-    return identity_matrix
-
-N_example = 3
-create_and_print_identity_matrix(N_example)
