c82fe96d8a23e5b7e4f6024228417873086b426c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-29 14:46:52 +0100

    Add files via upload

---
+++ Python FUNDAMENTALS/15_function_exercise/08_palindrome_integers.py
@@ -0,0 +1,10 @@
+def palindrome_integer(numbers: list):
+    for number in numbers:
+        if number == number[::-1]:
+            print(True)
+            continue
+        print(False)
+
+
+list_numbers = input().split(", ")
+palindrome_integer(list_numbers)
