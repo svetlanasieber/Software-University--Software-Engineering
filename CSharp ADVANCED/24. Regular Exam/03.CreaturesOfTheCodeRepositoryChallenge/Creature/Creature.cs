3347958a6e36f30a4e1fc160d7dfac91204f92b8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-29 15:40:40 +0100

    Create Creature.cs

---
+++ CSharp ADVANCED/24. Regular Exam/03.CreaturesOfTheCodeRepositoryChallenge/Creature/Creature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreaturesOfTheCode
+{
+    public class Creature
+    {
+       
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public int Health { get; set; }
+        public List<string> Abilities { get; set; }
+
+        
+        public Creature(string name, string kind, int health, string abilities)
+        {
+            Name = name;
+            Kind = kind;
+            Health = health;
+            Abilities = abilities.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+       
+        public override string ToString()
+        {
+            return $"{Name} ({Kind}) has {Health} HP" + Environment.NewLine +
+                   $"Abilities: {string.Join(", ", Abilities)}";
+        }
+    }
+}
