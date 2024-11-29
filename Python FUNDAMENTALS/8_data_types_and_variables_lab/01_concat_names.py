f4da591ff784499501e9863e6cb969b0b7981e1c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-29 14:44:36 +0100

    Add files via upload

---
+++ Python FUNDAMENTALS/8_data_types_and_variables_lab/01_concat_names.py
@@ -0,0 +1,10 @@
+#name_one = input()
+#name_two = input()
+#delimiter = input()
+#print(f"{name_one}{delimiter}{name_two}")
+
+name1 = input()
+name2 = input()
+delimiter = input()
+
+print(f"{name1}{delimiter}{name2}")
---
+++ Python FUNDAMENTALS/8_data_types_and_variables_lab/02_convert_meters_to_kilometers.py
@@ -0,0 +1,6 @@
+#meters = int(input())
+#print(f"{meters / 1000:.2f}")
+
+meters = int(input())
+kms = meters/1000
+print(f"{kms:.2f}")
