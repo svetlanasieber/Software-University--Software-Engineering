55304eeb415687985b9f786c5317c834e6a4a75a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-29 14:56:04 +0100

    Delete 25_dictionaries_lab directory

--- 25_dictionaries_lab/05_ascii_values.py
+++
@@ -1,5 +0,0 @@
-chars = input().split(", ")
-
-char_dict = {ch: ord(ch) for ch in chars}
-
-print(char_dict)
--- 25_dictionaries_lab/05_ascii_values_v2.py
+++
@@ -1,5 +0,0 @@
-list_characters = input().split(", ")
-characters = {i: ord(i) for i in list_characters}
-
-
-print(f"{characters}")
