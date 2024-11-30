b7d463c631db517430434f4fac23c7120d19a63a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-30 16:26:05 +0100

    Delete Jupyter Notebook Mastery directory

--- Jupyter Notebook Mastery/5 Principles of Numpy/homework_judge/01.Null_Array.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-N = 12
-null_array = np.zeros(N)
-print(null_array)
--- Jupyter Notebook Mastery/6 Principles of Pandas/02. Doubled Series/doubleSeries.py
+++
@@ -1,8 +0,0 @@
-N = 3 
-sequence_of_integers = [15, 155, 1555]  
-
-
-series = pd.Series(sequence_of_integers)
-doubled_series = series * 2
-
-print(doubled_series)
