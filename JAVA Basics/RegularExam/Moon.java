bbb05ed0a03742855ad54c8e28bcd735589775fb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-30 17:49:57 +0100

    Update Moon.java

--- JAVA Basics/RegularExam/Moon.java
+++ JAVA Basics/RegularExam/Moon.java
@@ -8,12 +8,12 @@ public class Moon_01 {
         double needFuel = Double.parseDouble(scanner.nextLine());
 
         int distanceToMoon = 384400;
-        int totalDistance = 2 * distanceToMoon; // Отиване и връщане
-        double travelTime = totalDistance / speed; // Време за пътуване до Луната и обратно
-        int totalTime = (int) Math.ceil(travelTime + 3); // Закръгляме времето и добавяме 3 часа престой на Луната
-        int fuelNeeded = (int) ((needFuel * totalDistance) / 100); // Общо гориво за изминатото разстояние
+        int totalDistance = 2 * distanceToMoon;
+        double travelTime = totalDistance / speed;
+        int totalTime = (int) Math.ceil(travelTime + 3);
+        int fuelNeeded = (int) ((needFuel * totalDistance) / 100); 
 
-        // Отпечатване на резултатите
+    
         System.out.println(totalTime);
         System.out.println(fuelNeeded);
 
