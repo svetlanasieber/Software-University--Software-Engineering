3538baf6955e450229e4a09b6d03cc490d41e693
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-30 13:31:19 +0100

    Update SecretChat_01.java

--- JAVA FUNDAMENTALS/34. Exam Preparation/ExamPrep-20-11-2024/SecretChat_01.java
+++ JAVA FUNDAMENTALS/34. Exam Preparation/ExamPrep-20-11-2024/SecretChat_01.java
@@ -8,22 +8,22 @@ public class SecretChat_01 {
 
         StringBuilder concealedMessage = new StringBuilder(scanner.nextLine());
 
-        String command = scanner.nextLine();//както инструкция така и команда "Reveal"
+        String command = scanner.nextLine();
 
 
         while (!command.equals("Reveal")){
 
-            //намираме текущата команда, която искаме да изпълним
+          
             //InsertSpace, Reverse, ChangeAll
             String[] currentData = command.split(":\\|:");
-            String currentCommand = currentData[0];// получаваме действието, което трябва да извършим
+            String currentCommand = currentData[0];
 
             switch (currentCommand){
 
                 case "InsertSpace" -> {
-                    //вземаме индекса, на който ще поставим интервал
+                 
                     int index = Integer.parseInt(currentData[1]);
-                    //поставяме интервал на конкретния индекс
+                  
                     concealedMessage.insert(index, " ");//abcdefabc -> 2
                     System.out.println(concealedMessage);
                 }
@@ -32,22 +32,19 @@ public class SecretChat_01 {
                     StringBuilder substring = new StringBuilder(currentData[1]);
                     //Reverse:|:{substring}
 
-                    //проверяваме дали нашето съобщение съдържа текста, който искаме да завъртим
+                   
                     if (!concealedMessage.toString().contains(substring)){
-                        //ако текста не се съдържа в нашето съобщение принтираме error
+                       
                         System.out.println("error");
                     }else {
-                        //abcdefabc
-                        //abc
-                        //за да изтрием първото срещане на нашия substring имаме нужда от първия и последния индекс
+                      
                         int firstIndex = concealedMessage.indexOf(substring.toString());//0
                         int lastIndex = firstIndex + substring.length();//3
-                       // int number = concealedMessage.lastIndexOf(substring.toString()) + 1;
-                        //завъртаме нашия substring -> cba
+                   
                         String reversedString = substring.reverse().toString();
-                        //изтриваме събстринга при първото му срещане
+                      
                         concealedMessage.delete(firstIndex, lastIndex);
-                        //добавяме обърнатия substring
+                      
                         concealedMessage.append(substring);
                         System.out.println(concealedMessage);
 
