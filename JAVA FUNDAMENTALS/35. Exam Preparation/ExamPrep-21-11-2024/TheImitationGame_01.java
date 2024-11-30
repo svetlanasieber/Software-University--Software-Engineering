55595ec663ae56ac3b01e54e77e412d8d939f92f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-30 13:33:57 +0100

    Update TheImitationGame_01.java

--- JAVA FUNDAMENTALS/35. Exam Preparation/ExamPrep-21-11-2024/TheImitationGame_01.java
+++ JAVA FUNDAMENTALS/35. Exam Preparation/ExamPrep-21-11-2024/TheImitationGame_01.java
@@ -6,35 +6,30 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String encryptedMessage = scanner.nextLine(); //криптирано съобщение
-        StringBuilder modifiedMessage = new StringBuilder(encryptedMessage); //модифицираме криптираното съобщение
+        String encryptedMessage = scanner.nextLine(); 
+        StringBuilder modifiedMessage = new StringBuilder(encryptedMessage); 
 
         String command = scanner.nextLine();
         while (!command.equals("Decode")) {
-            //command си държа валидна команда
-            //проверка коя команда е въведена
+       
             if (command.contains("Move")) {
-                //command = "Move|3".split("|") -> ["Move", "3"]
-                //Moves the first n letters to the back of the string
-                int countLetters = Integer.parseInt(command.split("\\|")[1]); //брой на първите букви, които трябва да преместим
-                //"Ivan Ivanov" -> първите 3 букви "Iva" -> "n IvanovIva"
-                String firstLetters = modifiedMessage.substring(0, countLetters); //букви за местене "Iva"
-                modifiedMessage.delete(0, countLetters); //"n Ivanov"
-                modifiedMessage.append(firstLetters); //"n IvanovIva"
+              
+                int countLetters = Integer.parseInt(command.split("\\|")[1]); 
+         
+                String firstLetters = modifiedMessage.substring(0, countLetters);
+                modifiedMessage.delete(0, countLetters); 
+                modifiedMessage.append(firstLetters);
             } else if (command.contains("Insert")) {
-                //command = "Insert|2|abc".split("|") -> ["Insert", "2", "abc"]
-                //Inserts the given value before the given index in the string
-                //"Ivan Ivanov" -> вмъкваме на втора позиция тескта abc -> "Ivabcn Ivanov"
-                int position = Integer.parseInt(command.split("\\|")[1]); //позиция, на която трябва да вмъкнем текст
-                String textToInsert = command.split("\\|")[2]; //текст, който трябва да се вмъкне в съобщение
+          
+                int position = Integer.parseInt(command.split("\\|")[1]); 
+                String textToInsert = command.split("\\|")[2]; 
                 modifiedMessage.insert(position, textToInsert);
             } else if (command.contains("ChangeAll")) {
-                //command = "ChangeAll|abc|cba".split("|") -> ["ChangeAll", "abc", "cba"]
-                //Changes all occurrences of the given substring with the replacement text
-                String textForChange = command.split("\\|")[1]; //текст, който трябва да се замени
-                String replacement = command.split("\\|")[2]; //текст, който ще заменя
-                //"Ivabcn Ivanov" -> "Ivcban Ivanov"
-                String currentMessage = modifiedMessage.toString(); //текущото съобщение
+              
+                String textForChange = command.split("\\|")[1]; 
+                String replacement = command.split("\\|")[2]; 
+             
+                String currentMessage = modifiedMessage.toString();
                 currentMessage = currentMessage.replace(textForChange, replacement);
                 modifiedMessage = new StringBuilder(currentMessage);
             }
