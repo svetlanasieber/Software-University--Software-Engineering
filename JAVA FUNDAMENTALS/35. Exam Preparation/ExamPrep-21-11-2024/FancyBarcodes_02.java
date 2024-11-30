0f1d318d36ed94fdf633258106a58b4b131fcead
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-30 13:32:45 +0100

    Update FancyBarcodes_02.java

--- JAVA FUNDAMENTALS/35. Exam Preparation/ExamPrep-21-11-2024/FancyBarcodes_02.java
+++ JAVA FUNDAMENTALS/35. Exam Preparation/ExamPrep-21-11-2024/FancyBarcodes_02.java
@@ -8,18 +8,17 @@ import java.util.regex.Pattern;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String regex = "[@][#]+[A-Z][A-Za-z0-9]{4,}[A-Z][@][#]+"; //текста на шаблон
+        String regex = "[@][#]+[A-Z][A-Za-z0-9]{4,}[A-Z][@][#]+"; 
         Pattern pattern = Pattern.compile(regex);
 
-        int countBarcodes = Integer.parseInt(scanner.nextLine()); //брой баркодове
+        int countBarcodes = Integer.parseInt(scanner.nextLine()); /
 
         for (int count = 1; count <= countBarcodes; count++) {
             String textBarcode = scanner.nextLine(); //"@#FreshFisH@#"
             Matcher matcher = pattern.matcher(textBarcode);
-            //matcher - съхранявам всички текстове, които отговарят на шаблона
-            //matcher = ["@#FreshFisH@#"]
+         
 
-            //проверка дали е валиден
+          
             if (matcher.find()) {
                 //въведения баркод е валиден
                 String productGroup = calculateProductGroup(textBarcode);
@@ -33,7 +32,7 @@ public class Task2 {
 
     private static String calculateProductGroup(String textBarcode) {
         //textBarcode = "@###Che46sE@##"
-        StringBuilder sbProductGroup = new StringBuilder(); //да долепяме всички налични цифири в баркода
+        StringBuilder sbProductGroup = new StringBuilder(); 
         //textBarcode.toCharArray() -> ['@', '#', '#', '#', ....]
         for (char symbol : textBarcode.toCharArray()) {
             if (Character.isDigit(symbol)) {
@@ -41,9 +40,7 @@ public class Task2 {
             }
         }
 
-        //productGroup -> продуктова група на баркода -> текст с цифри
-        //productGroup е празен -> в баркода няма цифри -> "00"
-        //productGroup не е празен -> в баркода има цифри
+
 
         if (sbProductGroup.isEmpty()) {
             return "00";
