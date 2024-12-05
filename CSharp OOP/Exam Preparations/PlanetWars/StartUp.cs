cce21e00ef05957e1be44d59ecde8bf8fde6e8b0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-05 04:48:01 +0100

    Create StartUp.cs

---
+++ CSharp OOP/Exam Preparations/PlanetWars/StartUp.cs
@@ -0,0 +1,16 @@
+using PlanetWars.Core;
+using System;
+
+namespace PlanetWars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           
+            var engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
