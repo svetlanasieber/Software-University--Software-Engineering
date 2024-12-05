0cf07300bacdb52c4a62dd720465d76361d2969e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-05 03:23:52 +0100

    Update Program.cs

--- CSharp Basics/P0_Problems/01.StacksAndQueues/03.MaximumAndMinimumElement/Program.cs
+++ CSharp Basics/P0_Problems/01.StacksAndQueues/03.MaximumAndMinimumElement/Program.cs
@@ -23,27 +23,27 @@ namespace _03.MaximumAndMinimumElement
 
                 int command = tokens[0];
 
-                if (command == 1) // push the element into the stack
+                if (command == 1) 
                 {
                     int elementToPush = tokens[1];
 
                     stack.Push(elementToPush);
                 }
-                else if (command == 2) // delete the element at the top of the stack
+                else if (command == 2)
                 {
                     if (stack.Count > 0)
                     {
                         stack.Pop();
                     }
                 }
-                else if (command == 3) // print the maximum element in the stack
+                else if (command == 3) 
                 {
                     if (stack.Count > 0)
                     {
                         sb.AppendLine(stack.Max().ToString());
                     }
                 }
-                else if (command == 4) // print the minimum element in the stack
+                else if (command == 4)
                 {
                     if (stack.Count > 0)
                     {
