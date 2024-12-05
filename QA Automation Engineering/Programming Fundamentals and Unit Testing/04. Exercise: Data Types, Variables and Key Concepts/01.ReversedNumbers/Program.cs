db1e5bdc11a5fd1268e1e416717859654efc22bf
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-05 02:32:17 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/04. Exercise: Data Types, Variables and Key Concepts/01.ReversedNumbers/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/04. Exercise: Data Types, Variables and Key Concepts/01.ReversedNumbers/Program.cs
@@ -1,1 +1,11 @@
 
+//floating-point numbers
+
+double firstNum = double.Parse(Console.ReadLine());
+double secondNum = double.Parse(Console.ReadLine());
+double thirdNum = double.Parse(Console.ReadLine());
+
+Console.WriteLine(thirdNum);
+Console.WriteLine(secondNum);
+Console.WriteLine(firstNum);
+
