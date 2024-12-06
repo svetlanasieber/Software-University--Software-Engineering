48195875f562a510284755d6dfc39884da1bd685
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-06 10:51:42 +0100

    Create Demo.java

---
+++ JAVA Basics/Conditional Statements Lab/Demo.java
@@ -0,0 +1,12 @@
+package Conditional_Statements_Lab;
+
+public class Demo {
+    public static void main(String[] args) {
+        
+        int number = 6;
+        double decimalNumber = 4.5;
+        String text = "Desi";
+        char symbol = 'A';
+        boolean isValid = 5 < 4; //true or false
+    }
+}
