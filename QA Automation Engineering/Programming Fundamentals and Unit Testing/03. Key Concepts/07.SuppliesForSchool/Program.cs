bc0ec8dafbef1dcad4ba65ae4adf73b220806eb2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-06 15:48:45 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/07.SuppliesForSchool/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/07.SuppliesForSchool/Program.cs
@@ -1,1 +1,22 @@
+int pencilCount = int.Parse(Console.ReadLine());
+int markersCount = int.Parse(Console.ReadLine());
+int boardCleanerLiters = int.Parse(Console.ReadLine());
+int discountPercent = int.Parse(Console.ReadLine()); 
+
+
+double pencilPrice = 5.80;
+double markersPrice = 7.20;
+double boardPrice = 1.20;
+
+
+
+double totalSum = (pencilCount * pencilPrice) + (markersCount * markersPrice) + (boardCleanerLiters * boardPrice);
+
+
+double discountAsNumber = discountPercent / 100.0; 
+totalSum = totalSum - totalSum * discountAsNumber;
+
+Console.WriteLine(totalSum);
+
+
 
