0f216c9ada02bec0b278f70cef8096644288bea1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-06 15:07:39 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/06.MandatoryLiterature/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/06.MandatoryLiterature/Program.cs
@@ -1,1 +1,8 @@
+int countPages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int needDays = int.Parse(Console.ReadLine());
 
+int totalHours = countPages / pagesPerHour;
+int hoursPerDay = totalHours / needDays;
+
+Console.WriteLine(hoursPerDay);
