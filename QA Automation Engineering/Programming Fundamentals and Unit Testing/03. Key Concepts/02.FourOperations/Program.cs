cf343540fa901bb0a8781d6a5c16ab99329a1033
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-06 16:22:13 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/02.FourOperations/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/02.FourOperations/Program.cs
@@ -1,1 +1,19 @@
+double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
 
+
+double sum = firstNumber + secondNumber;
+
+
+double diff = firstNumber - secondNumber;
+
+
+double product = firstNumber * secondNumber;
+
+double division = firstNumber / secondNumber;
+
+
+Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {sum:F2}");
+Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {diff:F2}");
+Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {product:F2}");
+Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {division:F2}");
