e46b3b0231f84fbc32abef225de0b87aecdc5e9f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-06 16:26:04 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/03.Market/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/03.Market/Program.cs
@@ -1,1 +1,8 @@
+double tomatoPrice = double.Parse(Console.ReadLine());
+double tomatoQuantity  = double.Parse(Console.ReadLine());
 
+double cucumberPrice = double.Parse(Console.ReadLine());
+double cucumberQuiantity = double.Parse(Console.ReadLine());
+
+double totalCost = tomatoPrice * tomatoQuantity +  cucumberPrice * cucumberQuiantity;
+Console.WriteLine($"{totalCost:F2}");
