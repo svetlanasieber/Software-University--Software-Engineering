92c217c94ccaa11ea1d4760ab72105927ca9bd2d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-06 16:54:41 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/04.Tiles/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/04.Tiles/Program.cs
@@ -1,1 +1,17 @@
+double bathWidth = double.Parse(Console.ReadLine()); 
+double bathHeigth = double.Parse(Console.ReadLine()); 
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeigth = double.Parse(Console.ReadLine());
+
+
+double bathArea = bathWidth * bathHeigth;
+
+bathArea = bathArea + 0.10 * bathArea;
+
+double tileArea = tileWidth * tileHeigth;
+double countTiles = bathArea / tileArea;
+
+
+Console.WriteLine(Math.Round(countTiles));
+
 
