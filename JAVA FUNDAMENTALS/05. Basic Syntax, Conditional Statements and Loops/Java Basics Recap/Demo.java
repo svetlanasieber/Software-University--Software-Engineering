50a4b3e3eaec3632cfdaefc2006e77960451033d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-10 20:49:40 +0100

    Update Demo.java

--- JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Java Basics Recap/Demo.java
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Java Basics Recap/Demo.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        String name = "Yuli";
 
+        String name = "Yuli";
+        
+        int number = Integer.parseInt(scanner.nextLine());
+        double salary = Double.parseDouble(scanner.nextLine());
+        char symbol = scanner.nextLine().charAt(0);
+        boolean isValid = false; //false
 
         System.out.println("Works!");
         System.out.println("Hello Java!");
