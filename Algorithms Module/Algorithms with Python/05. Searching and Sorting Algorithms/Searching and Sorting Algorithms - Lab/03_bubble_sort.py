549bb10a5722d6ac04acb928153b30ef6f3f0371
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-11 00:23:48 +0100

    Create 03_bubble_sort.py

---
+++ Algorithms Module/Algorithms with Python/05. Searching and Sorting Algorithms/Searching and Sorting Algorithms - Lab/03_bubble_sort.py
@@ -0,0 +1,13 @@
+nums = [int(x) for x in input().split()]
+
+is_sorted = False
+counter = 0
+while not is_sorted:
+    is_sorted = True
+    for i in range(1, len(nums) - counter):
+        if nums[i] < nums[i - 1]:
+            nums[i], nums[i - 1] = nums[i - 1], nums[i]
+            is_sorted = False
+    counter += 1
+
+print(*nums, sep=" ")
