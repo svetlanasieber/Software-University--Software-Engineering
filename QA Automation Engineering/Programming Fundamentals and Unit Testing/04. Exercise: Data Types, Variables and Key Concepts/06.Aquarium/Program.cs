75cc12399a5520218cd863817b1ea071e7242e42
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-11 13:24:09 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/04. Exercise: Data Types, Variables and Key Concepts/06.Aquarium/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/04. Exercise: Data Types, Variables and Key Concepts/06.Aquarium/Program.cs
@@ -1,1 +1,13 @@
 
+int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percentFillWithSandPlantAndPump = double.Parse(Console.ReadLine());
+
+
+double aquariumAmountOfWater = length * width * height / 1000.0;
+
+double finalLitters = aquariumAmountOfWater * (1 - percentFillWithSandPlantAndPump / 100);
+
+
+Console.WriteLine($"{finalLitters:F2}");
