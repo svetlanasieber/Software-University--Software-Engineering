177d1c6ed6d4b6cf9561a1a4f311de5eae481770
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 16:12:29 +0100

    Update NumbersNTo1_02.java

--- JAVA Basics/For-Loops Lab/NumbersNTo1_02.java
+++ JAVA Basics/For-Loops Lab/NumbersNTo1_02.java
@@ -1,1 +1,15 @@
+package For_Loop;
+
+import java.util.Scanner;
+
+public class NumbersNTo1_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+ 
+        for (int number = n; number >= 1; number--) {
+            System.out.println(number);
+        }
+    }
+}
 
