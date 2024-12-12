cb22e19e8cb27975a16abd69b8c3bd35115e40cc
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 14:17:27 +0100

    Update NumbersFrom1To100_01.java

--- JAVA Basics/For-Loops Lab/NumbersFrom1To100_01.java
+++ JAVA Basics/For-Loops Lab/NumbersFrom1To100_01.java
@@ -1,1 +1,14 @@
+package For_Loop;
+
+import java.util.Scanner;
+
+public class NumbersFrom1To100_01 {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 100; number++) {
+            System.out.println();
+        }
+
+    }
+}
 
