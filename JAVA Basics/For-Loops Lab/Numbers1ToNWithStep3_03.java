05a027c78436a292dea37f206945b229d17eaafc
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 16:23:28 +0100

    Update Numbers1ToNWithStep3_03.java

--- JAVA Basics/For-Loops Lab/Numbers1ToNWithStep3_03.java
+++ JAVA Basics/For-Loops Lab/Numbers1ToNWithStep3_03.java
@@ -1,1 +1,18 @@
+package For_Loop;
+
+import java.util.Scanner;
+
+public class Numbers1NWithStep3_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int number = 1; number <= n; number += 3) {
+            System.out.println(number);
+        }
+
+
+    }
+}
 
