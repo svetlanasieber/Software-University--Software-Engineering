3f535728a747311c253f8d320e451c687d839e49
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 13:18:03 +0100

    Create Demo_03.java

---
+++ JAVA Basics/For-Loops Lab/Demo_03.java
@@ -0,0 +1,17 @@
+package For_Loop;
+
+import java.util.Scanner;
+
+public class Demo_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String role = "Administrator";
+        String password = "SoftUni";
+        if(role.equals("SoftUni")) {
+            if(password.equals("SoftUni")) {
+                System.out.println("Welcome!");
+            }
+        }
+
+    }
+}
