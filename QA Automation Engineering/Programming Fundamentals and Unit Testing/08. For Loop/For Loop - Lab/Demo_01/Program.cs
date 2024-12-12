750b052da76e8bb5d7f9ea9d2c35e077e513eb57
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-13 00:29:07 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/Demo_01/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/Demo_01/Program.cs
@@ -1,6 +1,12 @@
-
 int number = 7;
 
 Console.WriteLine(++number);
 Console.WriteLine(number++);
 Console.WriteLine(number);
+
+Console.WriteLine("--------------------------------------------");
+
+int age = 26;
+Console.WriteLine(--age);
+Console.WriteLine(age--);
+Console.WriteLine(age);
