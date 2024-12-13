e164877ca62baee68b9fc1e776bfefc20e8c218f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-13 05:52:11 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/06.ExamCountdown/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/06.ExamCountdown/Program.cs
@@ -1,1 +1,12 @@
+int days = int.Parse(Console.ReadLine()); 
+
+for (int day = days; day >= 1; day--)
+{
+
+    Console.WriteLine($"{day} days before the exam");
+}
+
+
+Console.WriteLine("The exam has come");
+
 
