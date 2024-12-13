f7e9aa67657a758f2c413e8b073628d52faa99cf
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-13 06:10:21 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/03.SumNumbers/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/03.SumNumbers/Program.cs
@@ -1,1 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
 
+int count = int.Parse(Console.ReadLine()); 
+
+double sum = 0; 
+
+for (int number = 1; number <= count; number++)
+{
+    
+    double value = double.Parse(Console.ReadLine());
+   
+    sum = sum + value; 
+}
+
+Console.WriteLine(sum);
