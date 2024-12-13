4b69bad2c5223c384da3314d9310363676f464d3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-13 05:26:35 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/05.NumbersFrom1ToNOver3/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/05.NumbersFrom1ToNOver3/Program.cs
@@ -1,1 +1,9 @@
+int startNumber = 1;
+int endNumber = int.Parse(Console.ReadLine());
+
+for (int number = startNumber; number <= endNumber; number = number + 3) 
+{
+
+    Console.WriteLine(number);
+}
 
