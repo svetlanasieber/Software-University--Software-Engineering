0609fa9bdd70372c3eb3a19217416f8ffda140f7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-13 05:02:49 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/04.NumbersEndingIn7/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/04.NumbersEndingIn7/Program.cs
@@ -1,1 +1,10 @@
+int startNumber = 7; 
+int endNumber = int.Parse(Console.ReadLine()); 
+
+for (int number = startNumber; number <= endNumber; number = number + 10) 
+{
+
+    Console.WriteLine(number);
+}
+
 
