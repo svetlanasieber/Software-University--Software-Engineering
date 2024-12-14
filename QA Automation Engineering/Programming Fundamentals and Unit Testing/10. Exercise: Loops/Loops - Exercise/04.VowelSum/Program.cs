696a335287ed9dcb2a531af3df02d77b34ff631a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-14 22:55:45 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/04.VowelSum/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/04.VowelSum/Program.cs
@@ -1,1 +1,19 @@
+int num = int.Parse(Console.ReadLine());
 
+int sum = 0;
+
+for (int i = 0; i < num; i++)
+{
+    char letter = char.Parse(Console.ReadLine());
+
+    switch (letter)
+    {
+        case 'a': sum += 1; break;
+        case 'e': sum += 2; break;
+        case 'i': sum += 3; break;
+        case 'o': sum += 4; break;
+        case 'u': sum += 5; break;
+    }
+}
+
+Console.WriteLine(sum);
