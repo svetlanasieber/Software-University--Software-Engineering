93a7973caae9bc328e3a781ca145bb2abaf23644
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-14 21:48:01 +0100

    Create Program.cs

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/01.PowerOfNumber/Program.cs
@@ -0,0 +1,10 @@
+int num = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= power; i++)
+{
+    result *= num;
+}
+Console.WriteLine(result);
