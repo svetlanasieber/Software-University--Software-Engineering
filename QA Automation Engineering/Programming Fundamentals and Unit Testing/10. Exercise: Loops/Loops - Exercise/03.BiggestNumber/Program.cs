e9995f1c66176b6d34ed07035aca4883cd080b84
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-14 22:41:08 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/03.BiggestNumber/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/03.BiggestNumber/Program.cs
@@ -1,1 +1,15 @@
+int num = int.Parse(Console.ReadLine());
 
+int biggestNum = int.MinValue;
+for (int i = 0; i < num; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+
+    if (currentNum > biggestNum)
+    {
+        biggestNum = currentNum;
+    }
+}
+
+
+Console.WriteLine(biggestNum);
