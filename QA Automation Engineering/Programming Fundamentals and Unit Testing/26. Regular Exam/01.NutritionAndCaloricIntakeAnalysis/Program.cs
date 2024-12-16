1cebb9a6732d3c3e86e7734f40efb240a22c505d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-16 08:18:16 +0100

    Add files via upload

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/26. Regular Exam/01.NutritionAndCaloricIntakeAnalysis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class NutritionAnalysis
+{
+    static void Main()
+    {
+    
+        int N = int.Parse(Console.ReadLine());
+
+        if (N <= 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+      
+        int cumulativeCaloricIntakeOfFood = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            
+            int caloricValue = int.Parse(Console.ReadLine());
+
+        
+            cumulativeCaloricIntakeOfFood += caloricValue;
+
+            
+            Console.WriteLine(cumulativeCaloricIntakeOfFood);
+        }
+    }
+}
---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/26. Regular Exam/02.AverageStockPriceInRange/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+class AverageStockPrice
+{
+    static void Main()
+    {
+       
+        string input = Console.ReadLine();
+        int[] averageStockPrices = input.Split(' ').Select(int.Parse).ToArray();
+
+        
+        int N = int.Parse(Console.ReadLine());
+
+        int M = int.Parse(Console.ReadLine());
+
+        
+        if (N < 0 || M >= averageStockPrices.Length || N > M)
+        {
+            Console.WriteLine("Invalid input");
+            return;
+        }
+
+      
+        int sum = 0;
+        for (int i = N; i <= M; i++)
+        {
+            sum += averageStockPrices[i];
+        }
+
+    
+        double averageResult = (double)sum / (M - N + 1);
+
+       
+        Console.WriteLine(averageResult.ToString("F2"));
+    }
+}
