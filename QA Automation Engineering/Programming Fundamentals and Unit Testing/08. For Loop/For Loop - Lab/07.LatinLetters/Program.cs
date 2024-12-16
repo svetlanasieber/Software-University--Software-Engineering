ac8b4534ce7897da07abe54e7bd984dbcc0292d8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-16 08:02:27 +0200

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/07.LatinLetters/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/07.LatinLetters/Program.cs
@@ -1,1 +1,8 @@
 
+char startSymbol = char.Parse(Console.ReadLine());
+char endSymbol = char.Parse(Console.ReadLine());
+
+for (char symbol = startSymbol; symbol <= endSymbol; symbol++)
+{
+    Console.Write(symbol + " ");
+}
