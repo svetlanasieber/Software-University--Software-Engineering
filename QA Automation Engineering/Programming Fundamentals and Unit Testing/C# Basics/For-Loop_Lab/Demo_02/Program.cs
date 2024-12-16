59e12c57cbbe53ea40d2a639d4514b933e4ffa5b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-16 08:51:47 +0200

    Create Program.cs

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/C# Basics/For-Loop_Lab/Demo_02/Program.cs
@@ -0,0 +1,10 @@
+int number = 101;
+if (number >= 1) {
+    Console.WriteLine("Larger than 1");
+}
+
+if (number <= 101)
+{
+    Console.WriteLine("Less than 101");
+    Console.WriteLine("Equal to 101");
+}
