de38679627a8a193a259db945a0cd138e4a2dded
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-16 08:42:33 +0200

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/C# Basics/Conditional Statements Advanced_Lab/SwitchCase/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/C# Basics/Conditional Statements Advanced_Lab/SwitchCase/Program.cs
@@ -1,11 +1,14 @@
+
 int a = 5;
 switch (a)
 {
-  case 5:
-  case 6:
-    a = a + 1;
-    break;
-  default:
-    a = a + 2;
-    break;
+    case 5:
+    case 6:
+        a = a + 1;
+        break;
+    default:
+        a = a + 2;
+        break;
 }
+
+Console.WriteLine(a);
