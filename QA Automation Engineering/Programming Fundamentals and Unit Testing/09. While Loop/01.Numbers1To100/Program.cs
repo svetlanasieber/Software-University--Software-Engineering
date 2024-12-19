2f2eb6d1be3e52c5fdea6ca096bbf5a50349ee1d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-20 00:58:24 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/01.Numbers1To100/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/01.Numbers1To100/Program.cs
@@ -1,6 +1,13 @@
+//while loop
 int number = 1;
 while (number <= 100)
 {
     Console.WriteLine(number);
     number++;
+}
+
+//For loop
+for (int num = 1; num <= 100; num++)
+{
+    Console.WriteLine(num);
 }
