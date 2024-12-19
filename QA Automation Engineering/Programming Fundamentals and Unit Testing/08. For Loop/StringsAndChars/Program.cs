9d735559843616a8c350eb7d97290f588b797d09
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-19 08:42:15 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/StringsAndChars/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/StringsAndChars/Program.cs
@@ -1,11 +1,15 @@
-//ASCII Table
-
-string name = "Desislava";
-
+tring name = "Desislava";
 
 
 char symbol = 'D';
 Console.WriteLine((int)symbol);
 
-int number = 65;
-Console.WriteLine((char)number); 
+int number = 68;
+Console.WriteLine((char)number);
+
+char letters = 'd';
+char letters2 = (char)97;
+int price = 'r';
+Console.WriteLine(letters);
+Console.WriteLine(letters2);
+Console.WriteLine(price);
