c4af7d9ba46728f0003329e30a61ef35566462dd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-20 01:19:05 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/02.DecreasingNumbers/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/02.DecreasingNumbers/Program.cs
@@ -1,3 +1,4 @@
+//while-loop
 int number = int.Parse(Console.ReadLine());
 
 while (number >= 1)
@@ -5,3 +6,13 @@ while (number >= 1)
     Console.WriteLine(number);
     number--;
 }
+
+
+//for-loop
+/*
+int n = int.Parse(Console.ReadLine());
+for (int numberTwo = n; numberTwo >= 1; numberTwo--)
+{
+    Console.WriteLine(numberTwo);
+}
+*/
