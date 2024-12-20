a4848ba990bd9fa1a745c97c46d7e3e44aef8ec1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-20 03:18:32 +0100

    Update P01NumbersEndingIn7.java

--- JAVA Basics/For-Loops Exercise/P01NumbersEndingIn7.java
+++ JAVA Basics/For-Loops Exercise/P01NumbersEndingIn7.java
@@ -6,9 +6,9 @@ public class P01NumbersEndingIn7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 10 == 7) {
-                System.out.println(i);
+        for (int num = 1; num <= 1000; num++) {
+            if (num % 10 == 7) {
+                System.out.println(num);
 
             }
 
@@ -16,7 +16,6 @@ public class P01NumbersEndingIn7 {
         }
     }
 }
-
 /*
 for (int i = 7; i <= 1000; i += 10){
 if (i % 10 = 7){
