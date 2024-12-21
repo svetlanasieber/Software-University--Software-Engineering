14ff14ef251ccd6277058bcb7171b25a61894eda
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-21 09:37:30 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P04CalculateRectangleArea_ReturnValue/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P04CalculateRectangleArea_ReturnValue/Program.cs
@@ -1,1 +1,12 @@
+int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
 
+Console.WriteLine(CalculateRectangleArea(width, length));
+
+//-----------------------------------------------------
+//Method
+static int CalculateRectangleArea(int width, int length)
+{
+    int area = width * length;
+    return area;
+}
