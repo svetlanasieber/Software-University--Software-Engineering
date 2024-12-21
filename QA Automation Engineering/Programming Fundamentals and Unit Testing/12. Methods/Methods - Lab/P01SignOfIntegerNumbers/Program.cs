0667d47fc150c674568c0701674ed8747964d643
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-21 08:23:40 +0100

    Create Program.cs

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P01SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,21 @@
+int number = int.Parse(Console.ReadLine());
+
+PrintNumberType(number);
+
+static void PrintNumberType(int number)
+{
+    if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+    else
+    {
+        //num == 0
+        Console.WriteLine($"The number {number} is zero.");
+    }
+}
+
