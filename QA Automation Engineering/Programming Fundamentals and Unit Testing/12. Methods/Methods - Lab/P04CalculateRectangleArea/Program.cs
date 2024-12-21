1ff5cae457f046b3368b8bb793390a0f8ffe703f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-21 09:19:58 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P04CalculateRectangleArea/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P04CalculateRectangleArea/Program.cs
@@ -1,1 +1,11 @@
 
+int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+
+PrintRectangleArea(width, length);
+
+static void PrintRectangleArea(int width, int length)
+{
+    int area = width * length;
+    Console.WriteLine(area);
+}
