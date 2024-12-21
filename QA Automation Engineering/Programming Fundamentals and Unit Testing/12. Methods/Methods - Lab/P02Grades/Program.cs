2bfac266df60db50c576227957f7c3dddf23bf81
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-21 08:58:28 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P02Grades/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P02Grades/Program.cs
@@ -1,1 +1,57 @@
+double grade = double.Parse(Console.ReadLine());
 
+PrintGradeAsText(grade);
+
+//o	"Fail" - If the grade is in range 2.00 – 2.99 (inclusively)
+//o	"Average" - If the grade is in range 3.00 – 3.49 (inclusively)
+//o	"Good" - If the grade is in range 3.50 – 4.49 (inclusively)
+//o	"Very good" - If the grade is in range 4.50 – 5.49 (inclusively)
+//o	"Excellent" - If the grade is in range 5.50 – 6.00 (inclusively)
+
+if (grade >= 2.00 && grade <= 2.99)
+{
+    Console.WriteLine("Fail");
+}
+else if (grade >= 3.00 && grade <= 3.49)
+{
+    Console.WriteLine("Average");
+}
+else if (grade >= 3.50 && grade <= 4.49)
+{
+    Console.WriteLine("Good");
+}
+else if (grade >= 4.50 && grade <= 5.49)
+{
+    Console.WriteLine("Very good");
+}
+else if (grade >= 5.50 && grade <= 6.00)
+{
+    Console.WriteLine("Excellent");
+}
+
+//--------------------------------------------------------
+
+//method
+static void PrintGradeAsText(double grade)
+{
+    if (grade >= 2.00 && grade <= 2.99)
+    {
+        Console.WriteLine("Fail");
+    }
+    else if (grade >= 3.00 && grade <= 3.49)
+    {
+        Console.WriteLine("Average");
+    }
+    else if (grade >= 3.50 && grade <= 4.49)
+    {
+        Console.WriteLine("Good");
+    }
+    else if (grade >= 4.50 && grade <= 5.49)
+    {
+        Console.WriteLine("Very good");
+    }
+    else if (grade >= 5.50 && grade <= 6.00)
+    {
+        Console.WriteLine("Excellent");
+    }
+}
