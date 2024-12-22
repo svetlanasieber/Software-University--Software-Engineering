ed4b69acc1ae2bef4e6eea9e9a66745cb4af9019
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-22 07:45:34 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P06MathPower/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/12. Methods/Methods - Lab/P06MathPower/Program.cs
@@ -1,1 +1,11 @@
+int number = int.Parse(Console.ReadLine()); 
+int power = int.Parse(Console.ReadLine()); 
+
+PrintPower(number, power);
+
+
+static void PrintPower(int number, int power)
+{
+    Console.WriteLine(Math.Pow(number, power));
+}
 
