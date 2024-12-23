1fd2e3a9076ba129793b9c2305ddc8b46478024a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-23 10:53:23 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/14. Arrays/PDemo/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/14. Arrays/PDemo/Program.cs
@@ -10,6 +10,9 @@ string[] stringArray = new string[10];
 
 
 integerArray[0] = 10;
+integerArray[1] = 34;
+integerArray[2] = 56;
+integerArray[3] = 78;
 doubleArray[0] = 10.5;
 stringArray[0] = "Hello";
 
