34f6343cdcfb13d689ec09dc636f5b2e07afae04
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-28 20:45:43 +0100

    Update Program.cs

--- CSharp DB/Entity Framework Core/29. ADO.NET/AdoNetIntroduction/Program.cs
+++ CSharp DB/Entity Framework Core/29. ADO.NET/AdoNetIntroduction/Program.cs
@@ -15,10 +15,4 @@ using (connection)
     Console.WriteLine($"There are {employeesCount} employees in our company");
 }
 
-/*
-//Async/await Pattern => Asynchronous programming with async and await
-public async Task Main(string args)
-{
 
-}
-*/
