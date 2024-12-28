47f42df7fb2a189558b48d659d56133c33c3bc7e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-28 21:04:32 +0100

    Update Program.cs

--- CSharp DB/Entity Framework Core/29. ADO.NET/AdoNetIntroduction/Program.cs
+++ CSharp DB/Entity Framework Core/29. ADO.NET/AdoNetIntroduction/Program.cs
@@ -6,9 +6,35 @@ connection.Open();
 using (connection)
 {
    
+    SqlCommand command = new SqlCommand("SELECT * FROM WHERE DepartmentID", connection);
+    SqlDataReader reader = command.ExecuteReader(); //SqlDataReader reader = await command.ExecuteReaderAsync();
+
+    using (reader)
+    {
+
+        while (reader.Read())
+        {
+            string? name = reader["FirstName"]?.ToString();
+            string lastName = reader[2]?.ToString();
+
+            Console.WriteLine($"{name} {lastName}");
+        }
+    }
+
+}
+
+/*
+ using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Server=.;Database=SoftUni;User Id=sa;Password=SoftUni!2021;MultipleActiveResultSets=true;Trust Server Certificate=true"); //connection string-a
+connection.Open();
+
+using (connection)
+{
+   
     SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Empleyees", connection);
     int? employeesCount = (int?) await command.ExecuteScalarAsync();
 
     Console.WriteLine($"There are {employeesCount} employees in our company");
 }
-
+ */
