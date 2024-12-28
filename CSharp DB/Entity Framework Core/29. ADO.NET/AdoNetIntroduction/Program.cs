0acec85d3e4321c20a4a9bfb2bbc65aad8e297af
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-28 20:42:47 +0100

    Update Program.cs

--- CSharp DB/Entity Framework Core/29. ADO.NET/AdoNetIntroduction/Program.cs
+++ CSharp DB/Entity Framework Core/29. ADO.NET/AdoNetIntroduction/Program.cs
@@ -1,11 +1,24 @@
-sing Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
-SqlConnection connection = new SqlConnection("Server=.;Database=SoftUni;User Id=sa;Password=SoftUni!2021;MultipleActiveResultSets=true"); //connection string-a
+SqlConnection connection = new SqlConnection("Server=.;Database=SoftUni;User Id=sa;Password=SoftUni!2021;MultipleActiveResultSets=true;Trust Server Certificate=true"); //connection string-a
 connection.Open();
 
 using (connection)
 {
     //1. DataReader
     //string sql = "SELECT * FROM Employee";
+
+    
+    SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Empleyees", connection);
+    int? employeesCount = (int?) await command.ExecuteScalarAsync(); //Asynchronous method
+
+    Console.WriteLine($"There are {employeesCount} employees in our company");
 }
 
+/*
+//Async/await Pattern => Asynchronous programming with async and await
+public async Task Main(string args)
+{
+
+}
+*/
