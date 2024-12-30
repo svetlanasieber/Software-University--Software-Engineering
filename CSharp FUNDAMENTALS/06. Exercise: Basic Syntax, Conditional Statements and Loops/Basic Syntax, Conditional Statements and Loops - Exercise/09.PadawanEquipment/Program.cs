1bdc3410d41f48dfb97928496f7f211fdccaebd1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 18:29:59 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/09.PadawanEquipment/Program.cs
+++ CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/09.PadawanEquipment/Program.cs
@@ -1,1 +1,30 @@
+using System;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        double moneyAmount = double.Parse(Console.ReadLine());
+        int studentsCount = int.Parse(Console.ReadLine());
+        double saberPrice = double.Parse(Console.ReadLine());
+        double robePrice = double.Parse(Console.ReadLine());
+        double beltPrice = double.Parse(Console.ReadLine());
+
+        double percent = (double)studentsCount * 10 / 100;
+        double allSabers = studentsCount + Math.Ceiling(percent);
+        double sabers = saberPrice * allSabers;
+
+        double robes = robePrice * studentsCount;
+
+        double beltsDiss = studentsCount - Math.Ceiling((double)(studentsCount / 6));
+        double belts = beltPrice * beltsDiss;
+
+        double total = sabers + robes + belts;
+        //Console.WriteLine($"Total: {total}, Money: {moneyAmount}");
+        Console.WriteLine(total <= moneyAmount ?
+            $"The money is enough - it would cost {total:F2}lv.":
+            $"John will need {(total - moneyAmount):F2}lv more.");
+    }
+}
 
