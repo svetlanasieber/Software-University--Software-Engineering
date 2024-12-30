8e9e1fb96b0f9acda9e726272254e2d1bc07745d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 18:26:57 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/06.StrongNumber/Program.cs
+++ CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/06.StrongNumber/Program.cs
@@ -1,1 +1,32 @@
+using System;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+      
+        int numberCopy = number;
+        int factorialSum = 0;
+
+        while (numberCopy > 0)
+        {
+            int digit = numberCopy % 10;
+            numberCopy /= 10;
+
+            int factorial = 1;
+            for (int i = 1; i <= digit; i++)
+            {
+                factorial *= i;
+            }
+
+            factorialSum += factorial;
+        }
+
+        string isStrongNumber = factorialSum == number ? "yes" : "no";
+
+        Console.WriteLine(isStrongNumber);
+    }
+}
 
