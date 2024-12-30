e15a374a745689f493f49b7f04df55b3dc3f94cd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 18:12:39 +0100

    Add files via upload

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/26. Regular Exam/BalancedBracketsTests/BalancedBracketsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        string[] input = new string[] { };
+
+        
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        
+
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        string[] input = new string[] { "(", ")", "(", ")" };
+
+        
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        
+
+        Assert.IsTrue(result);
+
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        
+        string[] input = new string[] { "(", "(", ")" };
+
+        
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        
+
+        Assert.IsFalse(result);
+
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        
+        string[] input = new string[] { "(", ")", ")", ")" };
+
+        
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        
+
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        
+        string[] input = new string[] { "(" };
+
+        
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        
+
+        Assert.IsFalse(result);
+    }
+}
