3ee80aba80e4785837505935a529bdf6827a7590
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 20:39:55 +0100

    Update P01DayOfWeek.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P01DayOfWeek.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P01DayOfWeek.java
@@ -1,1 +1,20 @@
+package Lab11_Arrays;
+
+import java.util.Scanner;
+
+public class P01DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int currentDay = Integer.parseInt(scanner.nextLine());
+
+        String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        if (currentDay >= 1 && currentDay <= 7) {
+            System.out.println(weekDays[currentDay - 1]);
+        } else {
+            System.out.println("Invalid day!");
+        }
+
+    }
+}
 
