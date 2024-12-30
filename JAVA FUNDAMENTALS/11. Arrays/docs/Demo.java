820f8177df940a3c6e1833867441f9896306c07d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 22:08:26 +0100

    Delete JAVA FUNDAMENTALS/11. Arrays/docs/Demo.java

--- JAVA FUNDAMENTALS/11. Arrays/docs/Demo.java
+++
@@ -1,9 +0,0 @@
-package Arrays_Lab;
-
-public class Demo {
-    public static void main(String[] args) {
-        
-        int[] days = new int[7];
-        System.out.println(days.length);
-    }
-}
