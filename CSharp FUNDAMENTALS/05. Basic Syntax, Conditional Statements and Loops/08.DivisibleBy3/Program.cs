9123770f2fd03b08ae22dba73b6043a88cda72c7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 18:18:26 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/08.DivisibleBy3/Program.cs
+++ CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/08.DivisibleBy3/Program.cs
@@ -1,1 +1,19 @@
+using System;
+
+namespace DivisibleBy3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
 
