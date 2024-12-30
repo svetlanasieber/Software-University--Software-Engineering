84cd35a0d9e5254dd4f88ea1af0c0532723817fd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-30 16:23:45 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/11. Arrays/Arrays - Lab/P01DayOfWeek/Program.cs
+++ CSharp FUNDAMENTALS/11. Arrays/Arrays - Lab/P01DayOfWeek/Program.cs
@@ -1,1 +1,22 @@
+int number = int.Parse(Console.ReadLine()); 
 
+string[] daysOfWeek = new string[7] {
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine(daysOfWeek[number - 1]);
+}
+else
+{
+    
+    Console.WriteLine("Invalid day!");
+}
