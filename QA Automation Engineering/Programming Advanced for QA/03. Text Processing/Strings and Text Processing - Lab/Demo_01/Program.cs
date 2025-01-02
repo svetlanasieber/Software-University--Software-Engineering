5621cbc1741feee84f39b8ce47ddc10e472bd5af
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-02 18:06:38 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/Demo_01/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/Demo_01/Program.cs
@@ -1,1 +1,45 @@
+string name = "Desislava";
+
+Console.WriteLine(name.Length);
+int length = name.Length;
+
+
+char firstSymbol = name[0];
+Console.WriteLine(name[0]);
+
+
+Console.WriteLine(name[name.Length - 1]);
+
+
+char letter = 'A';
+char symbol = char.Parse(Console.ReadLine());
+
+//string = array of chars
+char[] array = name.ToCharArray();
+//"Desislava" -> ['D', 'e', 's', 'i', 's', 'l', 'a', 'v', 'a']
+foreach (char s in array)
+{
+    Console.WriteLine(s);
+}
+
+//IMMUTABLE = READ ONLY
+Console.WriteLine(name[2]);
+//name[3] = 'U'; //ERROR
+
+string newName = name.Replace("Desislava", "Desislava123"); //name = name.Remove(4);
+Console.WriteLine(newName);
+
+
+//1. foreach 
+foreach (char sym in name)
+{
+   
+    Console.WriteLine(sym);
+}
+//2. for-loop 
+for (int index = 0; index <= name.Length - 1; index++)
+{
+    if (index % 2 == 0)
+    Console.WriteLine(name[index]);
+}
 
