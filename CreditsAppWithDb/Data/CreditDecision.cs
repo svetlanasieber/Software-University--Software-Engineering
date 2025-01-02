8bd7f3f4fc3956aab0a76dd88af86e9063f0a93c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-02 01:20:51 +0100

    Delete CreditsAppWithDb directory

--- CreditsAppWithDb/Data/CreditDecision.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CreditsAppWithDb.Data
-{
-    public class CreditDecision
-    {
-        public int Id { get; set; }
-        public int Score { get; set; }
-        public string Decision { get; set; }
-    }
-}
--- CreditsAppWithDb/Services/ICreditDecisionService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CreditsAppWithDb.Data;
-
-namespace CreditsAppWithDb.Services
-{
-    public interface ICreditDecisionService
-    {
-        CreditDecision GetById(int id);
-    }
-}
