7e01c59ecc8f316498518e6e04d1fe1a8456c76d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 20:35:37 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/SortedDictionary/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/SortedDictionary/Program.cs
@@ -1,1 +1,8 @@
+//SortedDictionary
 
+//student -> grade
+SortedDictionary<string, double> students = new SortedDictionary<string, double>();
+students.Add("Ivan", 5.60);
+students.Add("Alex", 4.50);
+students.Add("Georgi", 5.90);
+students.Add("Martin", 5.35);
