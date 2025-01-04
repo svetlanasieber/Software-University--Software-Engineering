13a2a3c3c3f83de397fca2c032dda4cfff6626ed
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 20:45:57 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/SortedDictionary/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/SortedDictionary/Program.cs
@@ -6,3 +6,14 @@ students.Add("Ivan", 5.60);
 students.Add("Alex", 4.50);
 students.Add("Georgi", 5.90);
 students.Add("Martin", 5.35);
+
+//Iterating Through a SortedDictionary using foreach loop
+
+foreach (KeyValuePair<string, double> entry in students)
+{
+    //запис -> entry
+    //entry.Key => име на студента
+    //entry.Value => оценка
+
+    Console.WriteLine(entry.Key + " -> " + entry.Value);
+}
