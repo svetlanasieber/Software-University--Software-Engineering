ada4dbd60c5f9e46a63259bc238fcc8547d628ce
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 19:15:54 +0100

    Create Program.cs

---
+++ CSharp FUNDAMENTALS/25. Dictionaries, Lambda, LINQ/Lab/Dictionary_Demo/Program.cs
@@ -0,0 +1,9 @@
+// student ages
+Dictionary<string, int> studentAges = new Dictionary<string, int>();
+
+studentAges.Add("Peter", 20);
+studentAges.Add("George", 30);
+studentAges.Add("Maria", 40);
+
+Console.WriteLine(studentAges["Peter"]);
+Console.WriteLine(studentAges["George"]);
