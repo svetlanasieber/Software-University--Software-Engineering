73ee7699ac0cbcdd6923e3c90a772fda8c149566
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 22:45:35 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/CountRealNumbers/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/CountRealNumbers/Program.cs
@@ -1,1 +1,22 @@
+double[]  numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+
+SortedDictionary<double, int> countOccurrences = new SortedDictionary<double, int>();
+
+foreach (double number in numbers)
+{
+    if (countOccurrences.ContainsKey(number))
+    {
+        countOccurrences[number]++;
+    }
+    else
+    {
+        countOccurrences.Add(number, 1);
+    }
+}
+
+foreach (KeyValuePair<double, int> entry in countOccurrences)
+{
+    Console.WriteLine(entry.Key + " -> " + entry.Value);
+}
 
