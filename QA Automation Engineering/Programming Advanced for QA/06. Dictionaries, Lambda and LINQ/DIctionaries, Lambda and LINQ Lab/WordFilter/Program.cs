a16c26155e2b00870aaf0a844c43c15e566f9782
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 23:38:49 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/WordFilter/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/WordFilter/Program.cs
@@ -1,1 +1,11 @@
+string[] wordsWithEvenLength = Console.ReadLine()                     
+                  .Split(" ")                          
+                  .Where(text => text.Length % 2 == 0)  
+                  .ToArray();
 
+
+
+foreach(string word in wordsWithEvenLength)
+{
+    Console.WriteLine(word);
+}
