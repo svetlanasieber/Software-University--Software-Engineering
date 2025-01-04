fa473a0e927dc885bd1eacad15973f9667e556a2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 23:36:03 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/25. Dictionaries, Lambda, LINQ/Lab/P02OddOccurrences/Program.cs
+++ CSharp FUNDAMENTALS/25. Dictionaries, Lambda, LINQ/Lab/P02OddOccurrences/Program.cs
@@ -1,1 +1,32 @@
+string[] words = Console.ReadLine().Split(" ");    
+
+
+Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+
+foreach(string word in words)
+{
+    
+    string wordWithLowerCase = word.ToLower();
+
+    if (!wordsCount.ContainsKey(wordWithLowerCase))
+    {
+      
+        wordsCount.Add(wordWithLowerCase, 1);
+    }
+    else
+    {
+        wordsCount[wordWithLowerCase]++;
+    }
+}
+
+foreach(KeyValuePair<string, int> entry in wordsCount)
+{
+ 
+    int countOccurences = entry.Value;
+    if (countOccurences % 2 != 0)
+    {
+        Console.Write(entry.Key + " ");
+    }
+}
 
