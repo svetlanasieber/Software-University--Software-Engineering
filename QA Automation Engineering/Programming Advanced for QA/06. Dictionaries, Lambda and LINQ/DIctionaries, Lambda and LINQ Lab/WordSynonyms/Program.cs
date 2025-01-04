862e83b62fd5236c43cb68ca6f0ec0c4522852bd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 23:40:19 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/WordSynonyms/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/06. Dictionaries, Lambda and LINQ/DIctionaries, Lambda and LINQ Lab/WordSynonyms/Program.cs
@@ -1,1 +1,32 @@
 
+Dictionary<string, List<string>> wordSynonyms = new Dictionary<string, List<string>>();
+
+int countWords = int.Parse(Console.ReadLine()); 
+
+for (int count = 1; count <= countWords; count++)
+{
+    string word = Console.ReadLine(); 
+    string synonym = Console.ReadLine(); 
+
+    if (wordSynonyms.ContainsKey(word))
+    {
+        
+        wordSynonyms[word].Add(synonym);
+    }
+    else
+    {
+       
+        wordSynonyms.Add(word, new List<string>());
+        wordSynonyms[word].Add(synonym);
+    }
+
+}
+
+
+foreach(KeyValuePair<string, List<string>> entry in wordSynonyms)
+{
+
+    string word = entry.Key;
+    List<string> synonyms = entry.Value;
+    Console.WriteLine(word + " - " + string.Join(", ", synonyms));
+}
