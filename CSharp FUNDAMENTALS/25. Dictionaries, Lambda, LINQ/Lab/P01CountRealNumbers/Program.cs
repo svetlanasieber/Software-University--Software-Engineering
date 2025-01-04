3c466fd018b71a9f6de6adcd8af36561904f38c4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 23:32:53 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/25. Dictionaries, Lambda, LINQ/Lab/P01CountRealNumbers/Program.cs
+++ CSharp FUNDAMENTALS/25. Dictionaries, Lambda, LINQ/Lab/P01CountRealNumbers/Program.cs
@@ -1,1 +1,29 @@
+double[] numbers = Console.ReadLine()    
+                   .Split(" ")           
+                   .Select(double.Parse) 
+                   .ToArray();
 
+SortedDictionary<double, int> countOccurences = new SortedDictionary<double, int>();
+
+
+foreach(double number in numbers)
+{
+
+   if (countOccurences.ContainsKey(number))
+    {
+       
+        countOccurences[number]++;
+    }
+    else
+    {
+        
+        countOccurences.Add(number, 1);
+    }
+}
+
+
+foreach(KeyValuePair<double, int> entry in countOccurences)
+{
+   
+    Console.WriteLine(entry.Key + " -> " + entry.Value);
+}
