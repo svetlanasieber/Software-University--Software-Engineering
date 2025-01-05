ebbd6eadd3f7be3f220aaca0a98eda1220c939ce
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-05 20:34:49 +0100

    Update example_04.py

--- Python FUNDAMENTALS/11_lists_basics_lab/example_04.py
+++ Python FUNDAMENTALS/11_lists_basics_lab/example_04.py
@@ -1,5 +1,12 @@
-list_of_numbers = [1, 5, 7, 3, 1, 2]
+#list_of_numbers = [1, 5, 7, 3, 1, 2]
 
-print(list_of_numbers[0])
-print(list_of_numbers[1])
-print(list_of_numbers[-1])
+#print(list_of_numbers[0])
+#print(list_of_numbers[1])
+#print(list_of_numbers[-1])
+
+pets = ["lion", "cat", "dog", "mouse"]
+
+print(pets[-1])
+print(pets[-2])
+print(pets[-3])
+print(pets[-4])
