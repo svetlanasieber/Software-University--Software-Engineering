977c698f4d17589c5f7e3e62d0b41b34807431a7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-05 17:55:25 +0100

    Create Demo4.java

---
+++ JAVA FUNDAMENTALS/11. Arrays/docs/Demo4.java
@@ -0,0 +1,15 @@
+package Lab11_Arrays;
+
+import java.util.Scanner;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[n]; //[ , , , , ]
+
+        numbers = new int[30]; 
+
+    }
+}
