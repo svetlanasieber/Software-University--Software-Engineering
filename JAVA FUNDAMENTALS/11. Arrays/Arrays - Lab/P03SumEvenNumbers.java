07823ffd39de8016edfcc98792ae82a7bad50ccd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 00:35:05 +0100

    Update P03SumEvenNumbers.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P03SumEvenNumbers.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P03SumEvenNumbers.java
@@ -1,1 +1,25 @@
+package Lab11_Arrays;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.Scanner;
+
+public class P03SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sumEven = 0; 
+        
+        for (int number : numbers) {
+            
+            if (number % 2 == 0) {
+                sumEven += number;
+            }
+        }
+       
+        System.out.println(sumEven);
+    }
+}
 
