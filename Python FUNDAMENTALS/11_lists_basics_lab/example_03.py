41a0f5d754351f47bba90b670df003f07b68ecf0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-05 20:14:55 +0100

    Create example_03.py

---
+++ Python FUNDAMENTALS/11_lists_basics_lab/example_03.py
@@ -0,0 +1,20 @@
+list_example = list() #list(['apple', 'cherry']) ; ["apple", "cherry"]
+
+print(type(list_example))
+
+#split function .split(" ")
+some_text = "a b c d"
+my_list = some_text.split(" ")
+print(my_list)
+
+some_text = "a, b, c, d"
+my_list = some_text.split(", ")
+print(my_list)
+
+numbers = list(map(int, input().split(', ')))
+print(numbers)
+
+#string.join()
+
+
+#.append -> add new element in List
