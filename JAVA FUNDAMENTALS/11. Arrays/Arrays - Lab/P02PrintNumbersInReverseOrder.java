3d66d44a995d08c062f221be3c529c4dd0d8521d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-05 21:56:25 +0100

    Update P02PrintNumbersInReverseOrder.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P02PrintNumbersInReverseOrder.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P02PrintNumbersInReverseOrder.java
@@ -1,1 +1,24 @@
+package Lab11_Arrays;
+
+import java.util.Scanner;
+
+public class P02PrintNumbersInReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        int count = Integer.parseInt(scanner.nextLine()); 
+        int[] numbers = new int[count];
+
+        for (int position = 0; position <= numbers.length - 1; position++) {
+            numbers[position] = Integer.parseInt(scanner.nextLine());
+        }
+
+      
+        for (int position = numbers.length - 1; position >= 0; position--) {
+            System.out.print(numbers[position] + " ");
+        }
+
+    }
+}
 
