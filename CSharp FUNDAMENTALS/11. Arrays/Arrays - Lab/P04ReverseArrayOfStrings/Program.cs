4f433396f6dcf776381cea703d163c0a5accd255
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 14:12:06 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/11. Arrays/Arrays - Lab/P04ReverseArrayOfStrings/Program.cs
+++ CSharp FUNDAMENTALS/11. Arrays/Arrays - Lab/P04ReverseArrayOfStrings/Program.cs
@@ -1,19 +1,1 @@
-using System;
 
-namespace P04ReverseArrayOfStrings
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string input = Console.ReadLine();
-
-            string[] separatedInput = input.Split(' ');
-
-            for (int i = separatedInput.Length - 1; i >= 0; i--)
-            {
-                Console.Write($"{separatedInput[i]} ");
-            }
-        }
-    }
-}
