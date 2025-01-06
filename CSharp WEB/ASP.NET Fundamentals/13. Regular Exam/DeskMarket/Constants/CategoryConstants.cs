d4859de94ec94e38d9092e980439e90df19959ba
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 23:21:01 +0100

    Add files via upload

---
+++ CSharp WEB/ASP.NET Fundamentals/13. Regular Exam/DeskMarket/Constants/CategoryConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DeskMarket.Constants
+{
+    public static class CategoryConstants
+    {
+        //Name
+        public const int NameMinLength = 3;
+        public const int NameMaxLength = 20;
+        public const string NameLengthErrorMessage = "Category must be between {2} and {1} characters long!";
+    }
+}
---
+++ CSharp WEB/ASP.NET Fundamentals/13. Regular Exam/DeskMarket/Constants/ProductConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace DeskMarket.Constants
+{
+    public class ProductConstants
+    {
+        //ProductName 
+        public const int ProductNameMinLength = 2;
+        public const int ProductNameMaxLength = 60;
+
+        public const string ProductNameNameLengthErrorMessage =
+            "ProductName must be between {2} and {1} characters long!";
+
+        //Description 
+        public const int DescriptionMinLength = 10;
+        public const int DescriptionMaxLength = 250;
+        public const string DescriptionLengthErrorMessage = "Description must be between {2} and {1} characters long!";
+
+        //Price
+        public const double PriceRangeLowestValue = 1.00;
+        public const double PriceRangeHighestValue = 3000.00;
+        public const string PriceRangeErrorMessage = "Price must be in the range from 1.00 to 3000.00";
+
+        //DateTime
+        public const string DateTimeFormat = "dd-MM-yyyy";
+        public const string DateTimeErrorMessage = "Invalid DateTime format. The format must be - dd-MM-yyyy";
+    }
+}
