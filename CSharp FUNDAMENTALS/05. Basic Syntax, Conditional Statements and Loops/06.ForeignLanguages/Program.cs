62da4a2d4f5465392d82ffc8cf84d48cad975bf6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 14:12:58 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/06.ForeignLanguages/Program.cs
+++ CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/06.ForeignLanguages/Program.cs
@@ -1,15 +1,1 @@
-string country = Console.ReadLine();
-
-if (country == "England" || country == "USA")
-{
-    Console.WriteLine("English");
-}
-else if(country == "Spain" || country == "Argentina" || country == "Mexico")
-{
-    Console.WriteLine("Spanish");
-}
-else
-{
-    Console.WriteLine("unknown");
-}
 
