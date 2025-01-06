fd145ff136e9c93452f581bf70b2cb2146bc9938
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-06 01:03:21 +0100

    Update P04ReverseArrayOfStrings.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P04ReverseArrayOfStrings.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/P04ReverseArrayOfStrings.java
@@ -1,1 +1,17 @@
+package Lab11_Arrays;
+
+import java.util.Scanner;
+
+public class P04ReverseArrayOfStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] texts = scanner.nextLine().split(" ");
+
+        for (int position = texts.length - 1; position >= 0; position--) {
+            System.out.print(texts[position] + " ");
+        }
+
+    }
+}
 
