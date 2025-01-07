34844956daa7724245b50763315273c082e6d2a3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-07 22:49:39 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/05.DepositCalculator/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/03. Key Concepts/05.DepositCalculator/Program.cs
@@ -1,1 +1,8 @@
+double deposit = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+double interest = double.Parse(Console.ReadLine());
 
+double monthlyInterestAmount = deposit * interest / 100 / 12;
+double totalAmount = deposit + monthlyInterestAmount * months;
+
+Console.WriteLine(totalAmount);
