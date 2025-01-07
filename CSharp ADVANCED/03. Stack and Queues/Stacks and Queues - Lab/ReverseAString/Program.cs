63f3d35f48158f7ba5b832c91b1fd31af0956813
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-08 00:18:09 +0100

    Update Program.cs

--- CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/ReverseAString/Program.cs
+++ CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/ReverseAString/Program.cs
@@ -1,1 +1,16 @@
+//Stack<char>
 
+string input = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    stack.Push(input[i]);
+}
+
+while (stack.Count > 0)
+{
+    char current = stack.Pop();
+    Console.Write(current);
+}
