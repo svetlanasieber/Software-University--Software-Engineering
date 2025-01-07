6630fe5504adaeb2d872a7c96c95c5489839950d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-08 00:05:25 +0100

    Update Program.cs

--- CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/Stack_01/Program.cs
+++ CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/Stack_01/Program.cs
@@ -4,10 +4,14 @@ Stack<int> stack = new Stack<int>();
 stack.Push(1);
 stack.Push(2);
 stack.Push(3);
-stack.Push(4);
-stack.Push(5);
 
-foreach (int item  in stack)
-{
-    Console.WriteLine(item);
-}
+
+//foreach (int item  in stack)
+//{
+//    Console.WriteLine(item);
+//}
+
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine($"Count: {stack.Count}");
