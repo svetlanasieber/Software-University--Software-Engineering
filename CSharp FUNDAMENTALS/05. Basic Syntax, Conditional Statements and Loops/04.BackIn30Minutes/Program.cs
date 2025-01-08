aab6509937d111a1df50b165a622fb56ce0d7d14
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-09 00:39:59 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/04.BackIn30Minutes/Program.cs
+++ CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/04.BackIn30Minutes/Program.cs
@@ -1,1 +1,33 @@
+int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+minutes += 30;
+
+if (minutes > 59)
+{
+    minutes -= 60;
+    hours++;
+}
+if (hours > 23) {
+
+    hours = 0;
+}
+
+Console.WriteLine($"{hours}:{minutes:D2}");
+
+/*
+ 
+ int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+        minutes += 30;
+        if (minutes >= 60)
+        {
+            hours++;
+            minutes -= 60;
+        }
+        if (hours == 24) hours = 0;
+        Console.WriteLine($"{hours}:{minutes:d2}");
+ 
+ 
+ */
 
