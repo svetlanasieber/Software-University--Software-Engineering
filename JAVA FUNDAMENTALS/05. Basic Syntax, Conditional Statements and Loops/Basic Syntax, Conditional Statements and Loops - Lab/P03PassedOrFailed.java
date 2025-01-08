1f708886f5abee9019924e9879a88bb487cacc8a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-08 21:31:07 +0100

    Create P03PassedOrFailed.java

---
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/P03PassedOrFailed.java
@@ -0,0 +1,18 @@
+package Lab05_Basic_Syntax_Recap;
+
+import java.util.Scanner;
+
+public class P03PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if(grade >= 3.00) {
+            System.out.println("Passed!");
+        } else {
+            //grade < 3.00
+            System.out.println("Failed!");
+        }
+    }
+}
