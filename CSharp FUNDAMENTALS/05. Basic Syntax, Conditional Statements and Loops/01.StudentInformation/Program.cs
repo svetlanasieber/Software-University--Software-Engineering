ad569bcb018b71c4166afd29e906c673c521cd05
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-08 23:51:08 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/01.StudentInformation/Program.cs
+++ CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/01.StudentInformation/Program.cs
@@ -1,5 +1,5 @@
 string studentName = Console.ReadLine();
-int age = int.Parse(Console.ReadLine());
-double averageGrade = double.Parse(Console.ReadLine());
+int studentAge = int.Parse(Console.ReadLine());
+double studentGrade = double.Parse(Console.ReadLine());
 
-Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade:F2}");
+Console.WriteLine($"Name: {studentName}, Age: {studentAge}, Grade: {studentGrade:f2}");
