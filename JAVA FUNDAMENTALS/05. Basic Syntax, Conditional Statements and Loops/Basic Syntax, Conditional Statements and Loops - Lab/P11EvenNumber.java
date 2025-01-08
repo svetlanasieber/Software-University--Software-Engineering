ca81aeee94fdf47636966925f11a6ae26a7584e8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-08 21:33:58 +0100

    Create P11EvenNumber.java

---
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/P11EvenNumber.java
@@ -0,0 +1,18 @@
+package Lab05_Basic_Syntax_Recap;
+
+import java.util.Scanner;
+
+public class P11EvenNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int enteredNumber = Integer.parseInt(scanner.nextLine()); 
+        while (enteredNumber % 2 != 0) {
+            System.out.println("Please write an even number.");
+            enteredNumber = Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.println("The number is: " + Math.abs(enteredNumber));
+
+    }
+}
