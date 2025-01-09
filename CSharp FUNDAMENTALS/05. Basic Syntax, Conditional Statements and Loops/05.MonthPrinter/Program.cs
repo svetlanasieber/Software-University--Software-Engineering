74cbb082834a1ddcbdea889742eba626ca91e849
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-09 01:05:07 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/05.MonthPrinter/Program.cs
+++ CSharp FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/05.MonthPrinter/Program.cs
@@ -2,44 +2,43 @@ int month = int.Parse(Console.ReadLine());
 
 switch (month)
 {
-	case 1:
+    case 1:
         Console.WriteLine("January");
-		break;
-		case 2:
+        break;
+    case 2:
         Console.WriteLine("February");
-		break;
-		case 3:
+        break;
+    case 3:
         Console.WriteLine("March");
-		break;
-		case 4:
+        break;
+    case 4:
         Console.WriteLine("April");
-		break;
-		case 5:
-		Console.WriteLine("May");
-		break;
-		case 6:
+        break;
+    case 5:
+        Console.WriteLine("May");
+        break;
+    case 6:
         Console.WriteLine("June");
-		break;
-		case 7:
+        break;
+    case 7:
         Console.WriteLine("July");
-		break;
-		case 8:
+        break;
+    case 8:
         Console.WriteLine("August");
-		break;
-		case 9:
+        break;
+    case 9:
         Console.WriteLine("September");
-		break;
-		case 10:
+        break;
+    case 10:
         Console.WriteLine("October");
-		break;
-		case 11:
+        break;
+    case 11:
         Console.WriteLine("November");
-		break;
-		case 12:
+        break;
+    case 12:
         Console.WriteLine("December");
-		break;
-	default:
+        break;
+    default:
         Console.WriteLine("Error!");
-		break;
+        break;
 }
-
