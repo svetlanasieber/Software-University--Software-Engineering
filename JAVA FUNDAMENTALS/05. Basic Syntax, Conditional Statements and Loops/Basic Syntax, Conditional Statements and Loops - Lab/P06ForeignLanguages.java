b65dfeb9882c74182710056a5e7ba77e7e753d5d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-09 01:23:41 +0100

    Create P06ForeignLanguages.java

---
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/P06ForeignLanguages.java
@@ -0,0 +1,27 @@
+package Lab05_Basic_Syntax_Recap;
+
+import java.util.Scanner;
+
+public class P06ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "USA":
+            case "England":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+                default:
+                    System.out.println("unknown");
+                    break;
+
+        }
+
+    }
+}
