52f76212492b46d24385cd920e1159b6cfe6d879
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-10 22:25:37 +0100

    Create Program.cs

---
+++ CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Ages/Program.cs
@@ -0,0 +1,26 @@
+int ages = int.Parse(Console.ReadLine());
+
+if (ages >= 0 && ages <= 2)
+{
+    Console.WriteLine("baby");
+}
+
+if (ages >= 3 && ages <= 13)
+{
+    Console.WriteLine("child");
+}
+
+if (ages >= 14 && ages <= 19)
+{
+    Console.WriteLine("teenager");
+}
+
+if (ages >= 20 && ages <= 65)
+{
+    Console.WriteLine("adult");
+}
+
+if (ages >= 66)
+{
+    Console.WriteLine("elder");
+}
