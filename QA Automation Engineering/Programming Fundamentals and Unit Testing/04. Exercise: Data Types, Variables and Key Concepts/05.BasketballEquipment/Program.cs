17e73d6c195656d02e5af42de69319576e9c1404
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-10 02:29:18 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/04. Exercise: Data Types, Variables and Key Concepts/05.BasketballEquipment/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/04. Exercise: Data Types, Variables and Key Concepts/05.BasketballEquipment/Program.cs
@@ -1,1 +1,9 @@
+int annualTrainingFee = int.Parse(Console.ReadLine());
 
+double sneakersPrice = 0.60 * annualTrainingFee;
+double outfitPrice = 0.80 * sneakersPrice;
+double ballPrice = 0.25 * outfitPrice;
+double accessoriesPrice = 0.20 * ballPrice;
+double totalPrice = annualTrainingFee + sneakersPrice + outfitPrice + ballPrice + accessoriesPrice;
+
+Console.WriteLine(totalPrice);
