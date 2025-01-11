2307e1020233e828a2148d8933fb46670d246436
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 03:42:57 +0100

    Update FillTheMatrix.java

--- JAVA ADVANCED/06. Exercise: Multidimensional Arrays/FillTheMatrix.java
+++ JAVA ADVANCED/06. Exercise: Multidimensional Arrays/FillTheMatrix.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-public class _01_FillTheMatrix {
+public class FillTheMatrix {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        // "3, A"
+   
         String[] input = scanner.nextLine().split(", ");
         int n = Integer.parseInt(input[0]);
         String pattern = input[1]; //"A" или "B"
 
         int[][] matrix = new int[n][n];
 
-        // 1. Проверяваме шаблона
-        // 2. Извиикваме правиилният метод
+      
         switch (pattern) {
             case "A" -> fillMatrixPatternA(matrix);
             case "B" -> fillMatrixPatternB(matrix);
         }
 
-        // 3. Отпечатваме матрицата
+       
         printMatrix(matrix);
     }
 
@@ -29,13 +28,13 @@ public class _01_FillTheMatrix {
         int counter = 1;
         for (int col = 0; col < matrix.length; col++) {
 
-            // Ако колоната е четно число: 0 ред до последен
+           
             if (col % 2 == 0) {
                 for (int row = 0; row < matrix.length; row++) {
                     matrix[row][col] = counter++;
                 }
             }
-            // Ако колоната е нечетно число: последен до 0 ред
+          
             else {
                 for (int row = matrix.length - 1; row >= 0; row--) {
                     matrix[row][col] = counter++;
