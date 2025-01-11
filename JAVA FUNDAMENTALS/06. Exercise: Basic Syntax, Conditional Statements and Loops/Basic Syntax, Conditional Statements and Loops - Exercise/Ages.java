ca99408a87f14c943a74e85329855b8198824626
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 21:34:56 +0100

    Create Ages.java

---
+++ JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Ages.java
@@ -0,0 +1,30 @@
+package Exercise06_BasicSyntax;
+
+import java.util.Scanner;
+
+public class Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int ages = Integer.parseInt(scanner.nextLine());
+
+        if (ages >= 0 && ages <= 2) {
+            System.out.println("baby");
+        }
+
+        if (ages >= 3 && ages <= 13) {
+            System.out.println("child");
+        }
+
+        if (ages >= 14 && ages <= 19) {
+            System.out.println("teenager");
+        }
+        if (ages >= 20 && ages <= 65) {
+            System.out.println("adult");
+        }
+        if (ages >= 66) {
+            System.out.println("elder");
+        }
+    }
+}
