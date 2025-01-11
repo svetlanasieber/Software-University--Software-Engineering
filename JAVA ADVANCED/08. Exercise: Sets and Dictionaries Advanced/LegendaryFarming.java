a84fdb90bfbe64cf137a6af523ced65a8d4992de
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 03:55:58 +0100

    Update LegendaryFarming.java

--- JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/LegendaryFarming.java
+++ JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/LegendaryFarming.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class _11_LegendaryFarming {
+public class LegendaryFarming {
 
     public static void main(String[] args) {
 
