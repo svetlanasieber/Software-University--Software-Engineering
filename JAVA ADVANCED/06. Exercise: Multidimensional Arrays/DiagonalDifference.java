7c1b9d22c8d6fed50d6052cdd5ddb7fdf5d72178
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 03:42:30 +0100

    Update DiagonalDifference.java

--- JAVA ADVANCED/06. Exercise: Multidimensional Arrays/DiagonalDifference.java
+++ JAVA ADVANCED/06. Exercise: Multidimensional Arrays/DiagonalDifference.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _03_DiagonalDifference {
+public class DiagonalDifference {
 
     public static void main(String[] args) {
 
@@ -9,16 +9,16 @@ public class _03_DiagonalDifference {
         int n = scanner.nextInt();
         int[][] matrix = new int[n][n];
 
-        // 1. Прочиитаме матрицата от конозлата
+     
         fillTheMatrix(matrix, scanner);
 
-        // 2. Намираме сумата на главният диагонал
+       
         int primaryDiagonalSum = getPrimaryDiagonalSum(matrix);
 
-        // 3. Намираме сумата на второстепенният диагонал
+       
         int secondaryDiagonalSum = getSecondaryDiagonalSum(matrix);
 
-        // 4. Отпечатваме абсолютната стойност на разликата на двете суми
+       
         System.out.println(Math.abs(primaryDiagonalSum - secondaryDiagonalSum));
     }
 
@@ -26,7 +26,7 @@ public class _03_DiagonalDifference {
 
         int sum = 0;
 
-        // Локациията е: индекс на реда == индекс на колона
+      
         for (int index = 0; index < matrix.length; index++) {
             int num = matrix[index][index];
             sum += num;
@@ -39,8 +39,7 @@ public class _03_DiagonalDifference {
 
         int sum = 0;
 
-        // Ред: от последен към 0
-        // Колона: 0 докато има редове (всеки път +1)
+     
         int col = 0;
         for (int row = matrix.length - 1; row >= 0; row--) {
             int num = matrix[row][col];
