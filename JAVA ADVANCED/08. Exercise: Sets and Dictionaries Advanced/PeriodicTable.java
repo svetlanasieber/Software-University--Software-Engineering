265a2a955b2e605a2b2ba5824771e9b96352b362
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 03:56:42 +0100

    Update PeriodicTable.java

--- JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/PeriodicTable.java
+++ JAVA ADVANCED/08. Exercise: Sets and Dictionaries Advanced/PeriodicTable.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class _03_PeriodicTable {
+public class PeriodicTable {
 
     public static void main(String[] args) {
 
@@ -11,9 +11,8 @@ public class _03_PeriodicTable {
         Set<String> chemicalElements = new TreeSet<>();
 
         for (int i = 0; i < n; i++) {
-            //"Mo O Ce"
             String compound = scanner.nextLine();
-            // ["Mo", "O", "Ce"]
+          
             String[] elements = compound.split(" ");
             chemicalElements.addAll(List.of(elements));
         }
