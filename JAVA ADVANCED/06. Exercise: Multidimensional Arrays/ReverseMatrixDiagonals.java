eec33c52cd507e094b1423c67c3b530fb5d308d6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 03:45:32 +0100

    Update ReverseMatrixDiagonals.java

--- JAVA ADVANCED/06. Exercise: Multidimensional Arrays/ReverseMatrixDiagonals.java
+++ JAVA ADVANCED/06. Exercise: Multidimensional Arrays/ReverseMatrixDiagonals.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class _11_ReverseMatrixDiagonals {
+public class ReverseMatrixDiagonals {
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
