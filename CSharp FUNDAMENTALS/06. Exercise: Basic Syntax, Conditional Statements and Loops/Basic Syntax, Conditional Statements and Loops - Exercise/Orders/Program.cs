727258446d6849e0a7505d1201fae3ce9cf1fba8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-11 02:56:19 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Orders/Program.cs
+++ CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Orders/Program.cs
@@ -1,1 +1,17 @@
+int ordersCount = int.Parse(Console.ReadLine());
+double total = 0;
+
+for (int i = 1; i <= ordersCount; i++)
+{
+
+    double pricePerCapsule = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsuleCount = int.Parse(Console.ReadLine());
+
+    double price = (days * capsuleCount) * pricePerCapsule;
+    total += price;
+    Console.WriteLine($"The price for the coffee is: ${price:F2}");
+}
+
+Console.WriteLine($"Total: ${total:F2}");
 
