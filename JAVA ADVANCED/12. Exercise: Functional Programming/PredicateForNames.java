5eaf3d5eeb4be38d4661b7be5729b9987a02b770
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 12:43:37 +0100

    Update PredicateForNames.java

--- JAVA ADVANCED/12. Exercise: Functional Programming/PredicateForNames.java
+++ JAVA ADVANCED/12. Exercise: Functional Programming/PredicateForNames.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.function.Predicate;
 
-public class _06_PredicateForNames {
+public class PredicateForNames {
 
     public static void main(String[] args) {
 
@@ -12,15 +12,9 @@ public class _06_PredicateForNames {
         int requiredLength = Integer.parseInt(scanner.nextLine());
         List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).toList();
 
-        // Predicate: аргумент -> израз/действие, който връща булева стойност
+     
         Predicate<String> predicate = name -> name.length() <= requiredLength;
 
-        //Начин 1:
-//        names.stream()
-//                .filter(predicate)
-//                .forEach(name -> System.out.println(name));
-
-        // Начин 2:
         for (String name : names) {
             if (predicate.test(name)) {
                 System.out.println(name);
