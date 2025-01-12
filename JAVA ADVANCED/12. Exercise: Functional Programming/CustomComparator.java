f80225f226a8711dfca4011f75d428236b5008d0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 12:42:05 +0100

    Update CustomComparator.java

--- JAVA ADVANCED/12. Exercise: Functional Programming/CustomComparator.java
+++ JAVA ADVANCED/12. Exercise: Functional Programming/CustomComparator.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-public class _08_CustomComparator {
+public class CustomComparator {
 
     public static void main(String[] args) {
 
@@ -13,26 +13,18 @@ public class _08_CustomComparator {
                 .map(Integer::parseInt)
                 .toList();
 
-        // Comparator: Ламбда израз, приема 2 аргумента и връща цяло число (-1, 0, 1)
-        // sorted(-1) = няма да има размяна
-        // sorted(0) = няма да има размяна
-        // sorted(1) = ще размени елементиите
+
         Comparator<Integer> comparator = (first, second) -> {
-            // първото-нечетно, второто-четно -> разменям числата
+     
             if (first % 2 != 0 && second % 2 == 0) {
                 return 1;
             }
-            // първото-четно, второто-нечетно -> не разменям числата
+         
             if (first % 2 == 0 && second % 2 != 0) {
                 return -1;
             }
 
-            // първото-четно, второто-четно
-            // първото-нечетно, второто-нечетно
-            // Default Comparator Implementation за числа: Подрежда числата в нарастващ ред
-            // първото > второто = 1
-            // първото < второто = -1
-            // първото == второто = 0
+
             return first.compareTo(second);
         };
 
