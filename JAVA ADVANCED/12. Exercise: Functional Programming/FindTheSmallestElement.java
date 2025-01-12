4cc58225eb6447bb16a30d66acbd10be42aebc7d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 12:42:40 +0100

    Update FindTheSmallestElement.java

--- JAVA ADVANCED/12. Exercise: Functional Programming/FindTheSmallestElement.java
+++ JAVA ADVANCED/12. Exercise: Functional Programming/FindTheSmallestElement.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
 
-public class _07_FindTheSmallestElement {
+public class FindTheSmallestElement {
 
     public static void main(String[] args) {
 
@@ -14,13 +14,11 @@ public class _07_FindTheSmallestElement {
                 .map(Integer::parseInt)
                 .toList();
 
-        // Function: аргумент -> извършва се действие с този аргумент и връща резултат
         Function<List<Integer>, Integer> function = listOfNumbers -> Collections.min(listOfNumbers);
 
         Integer minValue = function.apply(numbers);
 
-        // 5 6 7 1 8 5 1 3
-        //System.out.println(numbers.indexOf(minValue)); // получавам първата повява за този елемент
-        System.out.println(numbers.lastIndexOf(minValue)); // получавам последната повява за този елемент
+       
+        System.out.println(numbers.lastIndexOf(minValue)); 
     }
 }
