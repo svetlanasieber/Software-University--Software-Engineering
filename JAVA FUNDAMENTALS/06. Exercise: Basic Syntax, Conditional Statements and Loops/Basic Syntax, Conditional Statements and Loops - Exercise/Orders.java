a1cd45c3a49adbf9e2865c396eb0125b346e7ada
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 21:50:24 +0100

    Update Orders.java

--- JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Orders.java
+++ JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Orders.java
@@ -1,1 +1,30 @@
+package Exercise06_BasicSyntax;
+
+import java.util.Scanner;
+
+public class Orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      
+        int ordersCount = Integer.parseInt(scanner.nextLine());
+        double total = 0;
+
+        for (int i = 1; i <= ordersCount; i++) {
+
+            double pricePerCapsule = Double.parseDouble(scanner.nextLine());
+            int days = Integer.parseInt(scanner.nextLine());
+            int capsuleCount = Integer.parseInt(scanner.nextLine());
+
+            //((daysInMonth * capsulesCount) * pricePerCapsule)
+            double price = (days * capsuleCount) * pricePerCapsule;
+            total += price;
+            System.out.printf("The price for the coffee is: $%.2f%n", price);
+        }
+
+        System.out.printf("Total: $%.2f%n", total);
+
+
+    }
+}
 
