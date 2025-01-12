d790e93de9371ecb88773c2f6f54fba1a3788d29
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 13:38:15 +0100

    Update Ages.java

--- JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Ages.java
+++ JAVA FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Ages.java
@@ -10,19 +10,13 @@ public class Ages {
 
         if (ages >= 0 && ages <= 2) {
             System.out.println("baby");
-        }
-
-        else if (ages >= 3 && ages <= 13) {
+        } else if (ages >= 3 && ages <= 13) {
             System.out.println("child");
-        }
-
-        else if (ages >= 14 && ages <= 19) {
+        } else if (ages >= 14 && ages <= 19) {
             System.out.println("teenager");
-        }
-        else if (ages >= 20 && ages <= 65) {
+        } else if (ages >= 20 && ages <= 65) {
             System.out.println("adult");
-        }
-        else if (ages >= 66) {
+        } else if (ages >= 66) {
             System.out.println("elder");
         }
     }
