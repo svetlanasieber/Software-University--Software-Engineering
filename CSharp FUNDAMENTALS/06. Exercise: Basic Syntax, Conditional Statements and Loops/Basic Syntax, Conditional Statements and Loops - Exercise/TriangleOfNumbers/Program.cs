bf37fb5998385a1fc0d886b854c84e7e11f36a69
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 22:47:49 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/TriangleOfNumbers/Program.cs
+++ CSharp FUNDAMENTALS/06. Exercise: Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/TriangleOfNumbers/Program.cs
@@ -1,1 +1,11 @@
+int n = int.Parse(Console.ReadLine());
 
+for (int row = 1; row <= n; row++)
+{
+    for (int count = 1; count <= row; count++)
+    {
+        Console.Write(row + " ");
+    }
+
+    Console.WriteLine();
+}
