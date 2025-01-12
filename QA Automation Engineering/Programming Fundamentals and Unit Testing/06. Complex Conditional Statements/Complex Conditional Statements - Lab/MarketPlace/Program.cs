316779665a4da871b38d8d98e7cd10a9d2a4b467
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-13 00:29:17 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/MarketPlace/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/MarketPlace/Program.cs
@@ -1,1 +1,49 @@
+using System;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string product = Console.ReadLine(); // "Banana", "Apple", "Kiwi"
+        string dayType = Console.ReadLine(); // "Weekday", "Weekend"
+
+        double price = 0;
+
+        if (product == "Banana")
+        {
+            if (dayType == "Weekday")
+            {
+                price = 2.50;
+            }
+            else
+            {
+                price = 2.70;
+            }
+        }
+        else if (product == "Apple")
+        {
+            if (dayType == "Weekday")
+            {
+                price = 1.30;
+            }
+            else
+            {
+                price = 1.60;
+            }
+        }
+        else if (product == "Kiwi")
+        {
+            if (dayType == "Weekday")
+            {
+                price = 2.20;
+            }
+            else
+            {
+                price = 3.00;
+            }
+        }
+
+       
+        Console.WriteLine($"{price:F2}");
+    }
+}
