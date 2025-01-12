233bb2359412330b9aafd1e290d4ee0bfbdc8512
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-12 23:20:06 +0100

    Update CustomComparator.java

--- JAVA ADVANCED/12. Exercise: Functional Programming/CustomComparator.java
+++ JAVA ADVANCED/12. Exercise: Functional Programming/CustomComparator.java
@@ -1,33 +1,36 @@
+package FunctionalProgramming;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class CustomComparator {
-
     public static void main(String[] args) {
-
+      
         Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
 
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .map(Integer::parseInt)
-                .toList();
-
-
-        Comparator<Integer> comparator = (first, second) -> {
-     
-            if (first % 2 != 0 && second % 2 == 0) {
-                return 1;
-            }
-         
+        Comparator<Integer> comparator = ((first, second) -> {
+           
             if (first % 2 == 0 && second % 2 != 0) {
                 return -1;
             }
+          
+            else if (first % 2 != 0 && second % 2 == 0) {
+                return 1;
+            }
+        
+            else {
+                return first.compareTo(second);
+         
+            }
 
-
-            return first.compareTo(second);
-        };
+        });
 
         numbers.stream().sorted(comparator).forEach(number -> System.out.print(number + " "));
+
+
     }
 }
