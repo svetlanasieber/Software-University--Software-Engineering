c580d6d0175d01b826d41f5542c638a0a3be222a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-14 07:34:45 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/05. Simple Conditional Statements/Lab/P09ValidTriangle/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/05. Simple Conditional Statements/Lab/P09ValidTriangle/Program.cs
@@ -1,1 +1,13 @@
+int sideA = int.Parse(Console.ReadLine());
+int sideB = int.Parse(Console.ReadLine());
+int sideC = int.Parse(Console.ReadLine());
+
+if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
+{
+    Console.WriteLine("Valid Triangle");
+}
+else
+{
+    Console.WriteLine("Invalid Triangle");
+}
 
