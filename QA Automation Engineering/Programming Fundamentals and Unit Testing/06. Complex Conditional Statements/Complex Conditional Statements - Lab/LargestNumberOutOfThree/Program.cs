4f903fbfe672e684660d52f2f826d8118d29660f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-14 15:42:53 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/LargestNumberOutOfThree/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/LargestNumberOutOfThree/Program.cs
@@ -1,1 +1,25 @@
 
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int biggestNumber = 0; 
+
+
+if (firstNumber >= secondNumber && firstNumber >= thirdNumber)
+{
+    biggestNumber = firstNumber;
+}
+
+else if (secondNumber >= firstNumber && secondNumber >= thirdNumber)
+{
+    biggestNumber = secondNumber;
+}
+
+else
+{
+    biggestNumber = thirdNumber;
+}
+
+
+Console.WriteLine(biggestNumber);
