68e56302b1b4a087641705fe9c098fc46180b468
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-14 15:48:34 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/VowelOrConsonant/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/VowelOrConsonant/Program.cs
@@ -1,1 +1,20 @@
+char letter = char.Parse(Console.ReadLine());
 
+switch (letter)
+{
+    case 'A':
+    case 'a':
+    case 'E':
+    case 'e':
+    case 'I':
+    case 'i':
+    case 'U':
+    case 'u':
+    case 'O':
+    case 'o':
+        Console.WriteLine("Vowel");
+        break;
+    default:
+        Console.WriteLine("Consonant");
+        break;
+}
