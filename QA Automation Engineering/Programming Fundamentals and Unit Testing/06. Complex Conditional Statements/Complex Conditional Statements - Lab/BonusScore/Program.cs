58d6397c63ff4d3b21c38fc066c343204559cf4a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-16 14:14:39 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/BonusScore/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/06. Complex Conditional Statements/Complex Conditional Statements - Lab/BonusScore/Program.cs
@@ -1,1 +1,16 @@
+int points = int.Parse(Console.ReadLine());
 
+if (points <= 3)
+{
+    points += 5;
+}
+else if (points >= 4 && points <= 6)
+{
+    points += 15;
+}
+else if (points >= 7 && points <= 9)
+{
+    points += 20;
+}
+
+Console.WriteLine(points);
