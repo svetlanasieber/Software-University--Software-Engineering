99bd3e526d1dabed6a4e4d02a163831b0bea9651
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-17 20:20:54 +0100

    Update ConcatNames.java

--- JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/ConcatNames.java
+++ JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/ConcatNames.java
@@ -1,1 +1,19 @@
+package Lab08_Data_Types_And_Variables;
+
+import java.util.Scanner;
+
+public class ConcatNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+
+
+        String output = firstName + delimiter + lastName;
+        System.out.println(output);
+        //System.out.println(firstName + delimiter + lastName);
+    }
+}
 
