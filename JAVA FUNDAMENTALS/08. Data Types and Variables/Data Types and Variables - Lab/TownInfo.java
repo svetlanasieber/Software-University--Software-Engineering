c3ae68ea5fa610cf9d47a6df82bdf994d605a619
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-17 20:06:27 +0100

    Update TownInfo.java

--- JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/TownInfo.java
+++ JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/TownInfo.java
@@ -1,1 +1,17 @@
+package Lab08_Data_Types_And_Variables;
+
+import java.util.Scanner;
+
+public class TownInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String townName = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        short area = Short.parseShort(scanner.nextLine());
+
+        System.out.printf("Town %s has population of %d and area %d square km.", townName, population, area);
+    }
+
+}
 
