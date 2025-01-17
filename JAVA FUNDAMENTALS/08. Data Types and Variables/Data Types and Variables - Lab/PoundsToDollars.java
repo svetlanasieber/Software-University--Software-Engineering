be876b75ed75de09fdbec843b6276f93d3c0daba
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-17 16:52:00 +0100

    Update PoundsToDollars.java

--- JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/PoundsToDollars.java
+++ JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/PoundsToDollars.java
@@ -1,1 +1,18 @@
+package Lab08_Data_Types_And_Variables;
+
+import java.util.Scanner;
+
+public class PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double pounds = Double.parseDouble(scanner.nextLine());
+
+        //usd = pounds * 1.36
+        double usd = pounds * 1.36;
+
+        System.out.printf("%.3f", usd);
+
+    }
+}
 
