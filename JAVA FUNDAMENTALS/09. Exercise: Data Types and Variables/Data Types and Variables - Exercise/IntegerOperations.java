866ada577ce0ced35f95b4dcad2f354129bb9cd5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-17 23:56:12 +0100

    Update IntegerOperations.java

--- JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/IntegerOperations.java
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/IntegerOperations.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        int thirdNumber = Integer.parseInt(scanner.nextLine());
+        int fourthNumber = Integer.parseInt(scanner.nextLine());
+
+        // ((n1 + n2) / n3) x n4
+        int result = ((firstNumber + secondNumber) / thirdNumber * fourthNumber);
+        System.out.println(result);
+
+    }
+}
 
