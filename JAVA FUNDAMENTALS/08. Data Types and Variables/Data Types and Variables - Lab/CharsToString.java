617e564f9d03a98ac2eb554e7167eda227b51308
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-17 20:45:53 +0100

    Update CharsToString.java

--- JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/CharsToString.java
+++ JAVA FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/CharsToString.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class CharsToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char firstSymbol = scanner.nextLine().charAt(0);
+        char secondSymbol = scanner.nextLine().charAt(0);
+        char thirdSymbol = scanner.nextLine().charAt(0);
+
+        //v1
+        System.out.printf("%c%c%c", firstSymbol, secondSymbol, thirdSymbol);
+
+        //v2
+
+    }
+}
 
