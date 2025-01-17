1476a9e0b29bdfe6640a55703fa523f5cba90fac
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-17 22:31:44 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/09. Exercise: Data Types and Variables/01.IntegerOperations/Program.cs
+++ CSharp FUNDAMENTALS/09. Exercise: Data Types and Variables/01.IntegerOperations/Program.cs
@@ -1,16 +1,7 @@
-﻿using System;
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int num4 = int.Parse(Console.ReadLine());
 
-namespace _01.IntegerOperations;
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        int num1 = int.Parse(Console.ReadLine());
-        int num2 = int.Parse(Console.ReadLine());
-        int num3 = int.Parse(Console.ReadLine());
-        int num4 = int.Parse(Console.ReadLine());
-
-        Console.WriteLine((num1 + num2) / num3 * num4);
-    }
-}
+Console.WriteLine((num1 + num2) / num3 * num4);
+  
