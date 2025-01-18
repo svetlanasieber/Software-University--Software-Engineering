0ea14f1ee83ff532dcbb4dd6fec9376fc03e05ac
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 14:16:13 +0100

    Update P02_SumDigits.java

--- JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/P02_SumDigits.java
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/P02_SumDigits.java
@@ -1,1 +1,23 @@
+package Exercise09_Data_Types_And_Variables;
+
+import java.util.Scanner;
+
+public class P02_SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0; 
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            sum += lastDigit; 
+            number /= 10; 
+        }
+
+       
+        System.out.println(sum);
+
+    }
+}
 
