d84c3ba6dbb5b925fa6e57e7fa2ebbf725035df9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 17:44:17 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/BiggestOfFiveNums/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/BiggestOfFiveNums/Program.cs
@@ -21,7 +21,7 @@ else if (num4 > num1 & num4 > num2 && num4 > num3 && num4 > num5)
 {
     Console.WriteLine(num4);
 }
-else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4)
+else
 {
     Console.WriteLine(num5);
 }
