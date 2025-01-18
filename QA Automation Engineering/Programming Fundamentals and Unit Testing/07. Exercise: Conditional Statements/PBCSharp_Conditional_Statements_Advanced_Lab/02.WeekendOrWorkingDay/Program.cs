b3bdb3e6485bd70678cc07dac9378d750757a084
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 21:23:48 +0100

    Create Program.cs

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/PBCSharp_Conditional_Statements_Advanced_Lab/02.WeekendOrWorkingDay/Program.cs
@@ -0,0 +1,26 @@
+using System.Runtime.CompilerServices;
+using System.Xml;
+
+string dayOfWeek = Console.ReadLine();
+
+string output = "";
+switch (dayOfWeek)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+ 
+        output = "Working day";
+        break;
+    case "Saturday":
+    case "Sunday":
+        output = "Weekend";
+        break;
+    default:
+        output = "Error";
+        break;
+        
+}
+Console.WriteLine(output);
