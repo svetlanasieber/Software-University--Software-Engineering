88aec8ed36f5ac83477489d266ca7ca66ed03878
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 21:53:18 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P08_FruitOrVegetable/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P08_FruitOrVegetable/Program.cs
@@ -1,1 +1,40 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+       
+        string[] fruits = { "banana", "apple", "kiwi", "cherry", "lemon", "grapes" };
+        string[] vegetables = { "tomato", "cucumber", "pepper", "carrot" };
+
+
+        string product = Console.ReadLine();
+
+        string result = "unknown";
+
+        for (int i = 0; i < fruits.Length; i++)
+        {
+            if (fruits[i] == product)
+            {
+                result = "fruit";
+                break; 
+            }
+        }
+
+
+        if (result == "unknown")
+        {
+            for (int i = 0; i < vegetables.Length; i++)
+            {
+                if (vegetables[i] == product)
+                {
+                    result = "vegetable";
+                    break; 
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
