b2743dfd82408b6f64a78bd9e69dca1b1ef8154f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 16:28:47 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/NumberType/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/NumberType/Program.cs
@@ -1,5 +1,9 @@
 int number = int.Parse(Console.ReadLine());
 
+//--> number == 0 -> zero
+//--> number > 0 -> positive
+//--> number < 0 -> negative
+
 if (number == 0)
 {
     Console.WriteLine("zero");
