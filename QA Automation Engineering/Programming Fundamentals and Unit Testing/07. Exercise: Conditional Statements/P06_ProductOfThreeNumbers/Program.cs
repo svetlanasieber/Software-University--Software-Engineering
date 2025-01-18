76e9858643057a7ef0ed8e37450afe9b6fdeaa82
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 22:07:35 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P06_ProductOfThreeNumbers/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P06_ProductOfThreeNumbers/Program.cs
@@ -1,1 +1,36 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+       
+        double num1 = double.Parse(Console.ReadLine());
+        double num2 = double.Parse(Console.ReadLine());
+        double num3 = double.Parse(Console.ReadLine());
+
+    
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("zero");
+        }
+        else
+        {
+          
+            int negativeCount = 0;
+
+            if (num1 < 0) negativeCount++;
+            if (num2 < 0) negativeCount++;
+            if (num3 < 0) negativeCount++;
+
+            if (negativeCount % 2 == 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
