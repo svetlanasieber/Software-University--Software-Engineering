166bd521f6f728dc8a0bc5f727786cf3db384ba3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 22:11:32 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P07_WorkingHours/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P07_WorkingHours/Program.cs
@@ -1,1 +1,11 @@
+int hour = int.Parse(Console.ReadLine());
+string dayOfWeek = Console.ReadLine();
 
+if (dayOfWeek == "Sunday" || hour < 10 || hour > 18)
+{
+    Console.WriteLine("closed");
+}
+else
+{
+    Console.WriteLine("open");
+}
