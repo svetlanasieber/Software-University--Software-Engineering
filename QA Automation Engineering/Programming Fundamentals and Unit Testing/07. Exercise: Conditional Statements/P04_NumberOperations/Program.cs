444f2077cee87de9512a786cacaa3fa0f29f2344
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 19:31:22 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P04_NumberOperations/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/07. Exercise: Conditional Statements/P04_NumberOperations/Program.cs
@@ -1,1 +1,26 @@
+double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+string mathOperator = Console.ReadLine();
+
+double result = 0;
+if (mathOperator == "+")
+{
+    result = firstNumber + secondNumber;
+}
+else if (mathOperator == "-")
+{
+    result = firstNumber - secondNumber;
+}
+else if (mathOperator == "*")
+{
+    result = firstNumber * secondNumber;
+}
+else if (mathOperator == "/")
+{
+    result = firstNumber / secondNumber;
+}
+
+
+Console.WriteLine($"{firstNumber} {mathOperator} {secondNumber} = {result:F2}");
+
 
