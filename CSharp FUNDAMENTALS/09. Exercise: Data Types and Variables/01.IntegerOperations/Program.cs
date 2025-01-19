059c855ac57b20d1f61a8c74ab222fb14510d6ea
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 18:33:17 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/09. Exercise: Data Types and Variables/01.IntegerOperations/Program.cs
+++ CSharp FUNDAMENTALS/09. Exercise: Data Types and Variables/01.IntegerOperations/Program.cs
@@ -1,1 +1,6 @@
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+int fourthNumber = int.Parse(Console.ReadLine());
 
+Console.WriteLine((firstNumber + secondNumber) / thirdNumber * fourthNumber);
