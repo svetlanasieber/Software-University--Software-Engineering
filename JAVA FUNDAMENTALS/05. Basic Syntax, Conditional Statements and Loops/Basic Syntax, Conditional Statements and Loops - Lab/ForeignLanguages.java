78273365b97bb3a73d66c3cabbc9391a3a158648
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 15:51:05 +0100

    Update ForeignLanguages.java

--- JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/ForeignLanguages.java
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/ForeignLanguages.java
@@ -1,1 +1,28 @@
+package Lab05_Basic_Syntax_Recap;
+
+import java.util.Scanner;
+
+public class ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "USA":
+            case "England":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+
+        }
+
+    }
+}
 
