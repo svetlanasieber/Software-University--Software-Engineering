489a40e7b59ade6ce824f225e04718f06aea102e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 13:11:03 +0100

    Delete CSharp FUNDAMENTALS/09. Exercise: Data Types and Variables directory

--- CSharp FUNDAMENTALS/09. Exercise: Data Types and Variables/01.IntegerOperations/Program.cs
+++
@@ -1,7 +0,0 @@
-int num1 = int.Parse(Console.ReadLine());
-int num2 = int.Parse(Console.ReadLine());
-int num3 = int.Parse(Console.ReadLine());
-int num4 = int.Parse(Console.ReadLine());
-
-Console.WriteLine((num1 + num2) / num3 * num4);
-  
