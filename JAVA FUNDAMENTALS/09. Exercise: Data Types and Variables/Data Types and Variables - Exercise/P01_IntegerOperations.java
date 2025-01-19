7e5b967ba4c2072217a5d75f8c5a301d4f9245d9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 16:36:59 +0100

    Create P01_IntegerOperations.java

---
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/P01_IntegerOperations.java
@@ -0,0 +1,19 @@
+package Exercise09_Data_Types_And_Variables;
+
+import java.util.Scanner;
+
+public class P01_IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        int thirdNumber = Integer.parseInt(scanner.nextLine());
+        int fourthNumber = Integer.parseInt(scanner.nextLine());
+
+        // ((n1 + n2) / n3) x n4
+        int result = ((firstNumber + secondNumber) / thirdNumber * fourthNumber);
+        System.out.println(result);
+
+    }
+}
