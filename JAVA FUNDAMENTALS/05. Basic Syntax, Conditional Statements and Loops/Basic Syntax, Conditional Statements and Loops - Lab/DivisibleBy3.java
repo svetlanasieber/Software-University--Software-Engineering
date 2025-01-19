f71e7c4fbc746a09d7bce1a4e96bbaae0613e6a2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 15:49:55 +0100

    Update DivisibleBy3.java

--- JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/DivisibleBy3.java
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/DivisibleBy3.java
@@ -1,1 +1,12 @@
+package Lab05_Basic_Syntax_Recap;
 
+public class DivisibleBy3 {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 100; number++) //number <= 99 {
+            if (number % 3 == 0) {
+                System.out.println(number);
+            }
+        }
+
+    }
