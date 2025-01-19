35987fe1c39354fd1d54c0acd5a04f235643e4b3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 23:51:27 +0100

    Update InfixТoPostfix.java

--- JAVA ADVANCED/04. Exercise: Stack and Queues/Exercise/InfixТoPostfix.java
+++ JAVA ADVANCED/04. Exercise: Stack and Queues/Exercise/InfixТoPostfix.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class _08_InfixТoPostfix {
+public class InfixТoPostfix {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
