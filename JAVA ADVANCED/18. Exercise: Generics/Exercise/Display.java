b45a00ed1b9feeca1b45a2bd734ca5192803d714
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-20 00:14:27 +0100

    Update Display.java

--- JAVA ADVANCED/18. Exercise: Generics/Exercise/Display.java
+++ JAVA ADVANCED/18. Exercise: Generics/Exercise/Display.java
@@ -1,1 +1,10 @@
+public class Display<E> {
+
+    public void display(E first, E second, E third) {
+        System.out.print(first + " ");
+        System.out.print(second + " ");
+        System.out.print(third + " ");
+        System.out.println();
+    }
+}
 
