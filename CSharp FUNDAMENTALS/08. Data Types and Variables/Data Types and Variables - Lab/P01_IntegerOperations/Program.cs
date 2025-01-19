eede11e1acd169144c85b22803f419af7eef01c9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 13:15:15 +0100

    Create Program.cs

---
+++ CSharp FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/P01_IntegerOperations/Program.cs
@@ -0,0 +1,8 @@
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+int number4 = int.Parse(Console.ReadLine());
+
+int firstResult = number1 + number2;
+int secondResult = firstResult * number3;
+Console.WriteLine(firstResult);
