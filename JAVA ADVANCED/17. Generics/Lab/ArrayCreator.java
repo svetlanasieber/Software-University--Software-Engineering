f95366ff8c0737a64b1c2bc75545e3ed7270fba3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-20 00:12:46 +0100

    Update ArrayCreator.java

--- JAVA ADVANCED/17. Generics/Lab/ArrayCreator.java
+++ JAVA ADVANCED/17. Generics/Lab/ArrayCreator.java
@@ -1,1 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator {
+
+    public static <T> T[] create(int length, T item) {
+        return create(item.getClass(), length, item);
+    }
+
+    public static <T> T[] create(Class<?> clazz, int length, T item) {
+        T[] arr = (T[]) Array.newInstance(clazz, length);
+        Arrays.fill(arr, item);
+        return arr;
+    }
+
+}
 
