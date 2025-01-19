480cbaa509f0c60bb7a28d13ba58cf19b11dfa45
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 17:41:57 +0100

    Create V02_SumDigits.java

---
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/V02_SumDigits.java
@@ -0,0 +1,21 @@
+package Exercise09_Data_Types_And_Variables;
+
+import java.util.Scanner;
+
+public class V02_SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        while (number > 0) {
+            int lastDigit = number % 10;
+            sum += lastDigit;
+            number /= 10;
+
+        }
+        System.out.println(sum);
+    }
+
+}
