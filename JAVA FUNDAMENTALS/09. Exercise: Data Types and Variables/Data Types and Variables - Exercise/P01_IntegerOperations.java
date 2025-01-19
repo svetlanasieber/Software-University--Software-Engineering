4ffc359bd72df677f1450ba77d615dbc5f2b3c39
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 18:05:08 +0100

    Update P01_IntegerOperations.java

--- JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/P01_IntegerOperations.java
+++ JAVA FUNDAMENTALS/09. Exercise: Data Types and Variables/Data Types and Variables - Exercise/P01_IntegerOperations.java
@@ -17,3 +17,27 @@ public class P01_IntegerOperations {
 
     }
 }
+
+/*
+
+package DataTypesAndVariables_Vik_Exercise;
+
+import java.util.Scanner;
+
+public class V01_IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        int thirdNumber = Integer.parseInt(scanner.nextLine());
+        int fourthNumber = Integer.parseInt(scanner.nextLine());
+
+        //((firstNumber + secondNumber) / thirdNumber) * fourthNumber)
+        System.out.println(((firstNumber + secondNumber) / thirdNumber) * fourthNumber);
+    }
+}
+
+
+
+*/
