83d506165a7cf6cbc9d03cc80b090ccb2d97e815
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 23:50:59 +0100

    Update BasicQueueOperations.java

--- JAVA ADVANCED/04. Exercise: Stack and Queues/Exercise/BasicQueueOperations.java
+++ JAVA ADVANCED/04. Exercise: Stack and Queues/Exercise/BasicQueueOperations.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class _04_BasicQueueOperations {
+public class BasicQueueOperations {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        //n-5 s-2 x-13
+      
         int[] input = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         int n = input[0];
