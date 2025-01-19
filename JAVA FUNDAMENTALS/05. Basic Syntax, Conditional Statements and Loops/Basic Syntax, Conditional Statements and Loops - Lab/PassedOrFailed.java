ac4d1e2dd53b2c460cbc8fb6d01336696b4bc868
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-19 15:52:20 +0100

    Update PassedOrFailed.java

--- JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/PassedOrFailed.java
+++ JAVA FUNDAMENTALS/05. Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/PassedOrFailed.java
@@ -1,1 +1,19 @@
+package Lab05_Basic_Syntax_Recap;
+
+import java.util.Scanner;
+
+public class PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if(grade >= 3.00) {
+            System.out.println("Passed!");
+        } else {
+            //grade < 3.00
+            System.out.println("Failed!");
+        }
+    }
+}
 
