06382168343eda6feac84d1b4b9fea81ce084b0a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-20 01:56:29 +0100

    Update Program.cs

--- CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/Stack_Lifo/Program.cs
+++ CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/Stack_Lifo/Program.cs
@@ -2,13 +2,13 @@ using System.Collections;
 
 Stack stack = new Stack();
 
-stack.Push(1); //dobavi chisloto 1
-stack.Push(2); //dobavi chisloto 2
-stack.Push(3); //dobavi chisloto 3
-stack.Push(4); //dobavi chisloto 4
+stack.Push(1); 
+stack.Push(2); 
+stack.Push(3); 
+stack.Push(4); 
 
-stack.Pop(); //izbavi 4
-stack.Pop(); //izbavi 3
+stack.Pop(); 
+stack.Pop(); 
 
-stack.Peek(); //vsi4ki 2
-stack.Peek(); //vsi4ki 1
+stack.Peek();
+stack.Peek();
