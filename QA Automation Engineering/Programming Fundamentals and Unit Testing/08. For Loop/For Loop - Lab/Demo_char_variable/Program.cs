257eb4b91c2fafcd21fdafb57ed54793277b798c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-20 01:46:06 +0100

    Create Program.cs

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/Demo_char_variable/Program.cs
@@ -0,0 +1,15 @@
+string name = "Desislava";
+
+
+char symbol = 'D';
+Console.WriteLine((int)symbol);
+
+int number = 68;
+Console.WriteLine((char)number);
+
+char letters = 'd';
+char letters2 = (char)97;
+int price = 'r';
+Console.WriteLine(letters);
+Console.WriteLine(letters2);
+Console.WriteLine(price);
