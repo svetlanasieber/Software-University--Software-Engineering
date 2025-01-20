a0f10379c96d91e435a421f8c871204d5cfe612a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-20 22:14:04 +0100

    Create Main.java

---
+++ JAVA OOP/04. Exercise: Working with Abstraction/CardsWithPower/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        CardRank rank = CardRank.valueOf(scanner.nextLine());
+     
+        CardSuit suit = CardSuit.valueOf(scanner.nextLine());
+
+        int cardPower = rank.getPower() + suit.getPower();
+
+        Card card = new Card(cardPower);
+
+        System.out.printf("Card name: %s of %s; Card power: %d", rank, suit, card.getPower());
+    }
+}
