aa75a8f54ff894d46ad37895d4cf5f9f2aec5057
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-21 20:29:54 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/06.NumberInRange/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/06.NumberInRange/Program.cs
@@ -1,1 +1,11 @@
+int n = int.Parse(Console.ReadLine()!);
 
+bool isInRange = n >= 1 && n <= 100;
+
+while (!isInRange)
+
+{
+    n = int.Parse(Console.ReadLine()!);
+    isInRange = n >= 1 &&  n <= 100;
+}
+Console.WriteLine(n);
