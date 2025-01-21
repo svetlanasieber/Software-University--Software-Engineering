d59f223de9d25ac99507d34a298091722ae3c790
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-21 14:25:14 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/02.FirstNNumbersSum/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/08. For Loop/For Loop - Lab/02.FirstNNumbersSum/Program.cs
@@ -1,1 +1,13 @@
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+
+for (int i = 1; i < number; i++)
+{
+    sum += i; //sum = sum + i;
+    Console.Write($"{i}+");
+}
+sum += number;
+Console.WriteLine($"{number}={sum}");
 
