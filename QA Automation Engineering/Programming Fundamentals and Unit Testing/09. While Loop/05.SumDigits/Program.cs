7d5b7b40bdbae70e053552199e36088e1a36da4e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-21 20:29:25 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/05.SumDigits/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/09. While Loop/05.SumDigits/Program.cs
@@ -1,1 +1,9 @@
+int number = int.Parse(Console.ReadLine()!);
 
+int sum = 0;
+while (number > 0) {
+    int lastDigit = number % 10;
+    sum += lastDigit; //sum = sum + lastDigit;
+    number /= 10; //number = number / 10;
+}
+Console.WriteLine(sum);
