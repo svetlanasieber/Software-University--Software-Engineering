1381c3de00ddeb1c6fbfb60faa7a831815c1c59f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-21 15:40:28 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/02.CommonElements/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/02.CommonElements/Program.cs
@@ -7,13 +7,13 @@ class Program
     {
         
         int[] firstArray = Console.ReadLine()
-                                  .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                  .Split(' ')
                                   .Select(int.Parse)
                                   .ToArray();
 
        
         int[] secondArray = Console.ReadLine()
-                                   .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                   .Split(' ')
                                    .Select(int.Parse)
                                    .ToArray();
 
@@ -22,3 +22,53 @@ class Program
         Console.WriteLine(string.Join(" ", commonElements));
     }
 }
+
+
+/* ver2
+int[] firstArray = Console.ReadLine()
+						  .Split(" ")
+						  .Select(int.Parse)
+						  .ToArray();
+
+int[] secondArray = Console.ReadLine()
+						  .Split(" ")
+						  .Select(int.Parse)
+						  .ToArray();
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+	int currentElement = firstArray[i];
+
+	for (int j = 0; j < secondArray.Length; j++)
+	{
+		if (currentElement == secondArray[j])
+		{
+			Console.Write(currentElement + " ");
+		}
+	}
+}
+*/
+
+/*
+
+/*ver3
+using System.Linq.Expressions;
+using System.Runtime.ExceptionServices;
+
+int[] array1 = Console.ReadLine()
+                      .Split(' ')
+                      .Select(int.Parse)
+                      .ToArray();
+
+
+int[] array2 = Console.ReadLine()
+                      .Split(' ')
+                      .Select(int.Parse)
+                      .ToArray();
+
+foreach (int element in array1)
+{
+    if (array2.Contains(element))
+        Console.Write(element + " ");
+}
+*/
