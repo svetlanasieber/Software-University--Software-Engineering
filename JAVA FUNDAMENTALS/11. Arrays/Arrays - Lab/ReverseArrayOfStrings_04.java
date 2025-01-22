46637f8b0ae73d5905f035960e6b44cc3a24d4b2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-22 20:01:24 +0100

    Update and rename ReverseArrayOfStrings_03.java to ReverseArrayOfStrings_04.java

---
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/ReverseArrayOfStrings_04.java
@@ -0,0 +1,20 @@
+package Others_Tasks;
+
+import java.util.Scanner;
+
+public class ReverseArrayOfStrings_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] texts = scanner.nextLine().split(" ");
+
+
+        for (int position = texts.length - 1; position >= 0; position--) {
+            System.out.print(texts[position] + " ");
+
+
+        }
+    }
+}
+
