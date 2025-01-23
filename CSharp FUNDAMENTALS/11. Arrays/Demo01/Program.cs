79043415ee1921df1bf9dce948d8292b0ce15a46
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-23 20:10:43 +0100

    Create Program.cs

---
+++ CSharp FUNDAMENTALS/11. Arrays/Demo01/Program.cs
@@ -0,0 +1,13 @@
+int[] numbers = new int[20];
+
+numbers[0] = 20;
+numbers[1] = 30;
+numbers[2] = 56;
+numbers[3] = 78;
+numbers[4] = 349;
+
+Console.WriteLine(numbers[0]);
+Console.WriteLine(numbers[1]);
+Console.WriteLine(numbers[2]);
+Console.WriteLine(numbers[3]);
+Console.WriteLine(numbers[4]);
