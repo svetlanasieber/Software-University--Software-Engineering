e88c5c5b6623c0c5f2c0190bbc8824d3c679f2b6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-23 19:18:05 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/06.SpecialNumber/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/06.SpecialNumber/Program.cs
@@ -1,1 +1,25 @@
+int num = int.Parse(Console.ReadLine()!);
 
+int temp = num;
+bool isSpecial = true;
+
+while (temp > 0)
+{
+    int lastDigit = temp % 10;
+    temp = temp / 10;
+
+    if (num % lastDigit != 0)
+    {
+        isSpecial = false;
+        break;
+    }
+}
+
+if (isSpecial)
+{
+    Console.WriteLine($"{num} is special");
+}
+else
+{
+    Console.WriteLine($"{num} is not special");
+}
