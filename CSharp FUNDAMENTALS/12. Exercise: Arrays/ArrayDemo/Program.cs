b36f252e261f1896cfb554e97be96f8906c0217e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-24 00:36:44 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/12. Exercise: Arrays/ArrayDemo/Program.cs
+++ CSharp FUNDAMENTALS/12. Exercise: Arrays/ArrayDemo/Program.cs
@@ -3,9 +3,25 @@
  numbers[1] = 2;
  numbers[2] = 3;
  
-
+ //Printing array direct -> Print type of the variable
+ //Benefit -> know the index of element, reverse reading
  for (int i = 0; i < numbers.Length; i++)
  {
      int currNum = numbers[i];
      Console.WriteLine($"Index {i} -> {currNum}");
+ }
+
+ Console.WriteLine("--------------------------------------");
+ //Reverse reading
+ for (int i = numbers.Length; i >= 0; i--)
+ {
+     int currNum = numbers[i];
+     Console.WriteLine($"Index {i} -> {currNum}");
+ }
+ Console.WriteLine("--------------------------------------");
+ 
+ //Benefit -> Less code, easy to use, no indexes
+ foreach (int num in numbers)
+ {
+     Console.WriteLine(num);
  }
