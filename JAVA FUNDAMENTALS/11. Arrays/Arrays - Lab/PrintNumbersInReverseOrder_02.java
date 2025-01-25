d00c1776f9103c027fee4b2e244beacca1a5bcfe
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-26 00:50:40 +0100

    Update PrintNumbersInReverseOrder_02.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/PrintNumbersInReverseOrder_02.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/PrintNumbersInReverseOrder_02.java
@@ -6,8 +6,6 @@ public class PrintNumReverse_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
-
         int count = Integer.parseInt(scanner.nextLine()); 
        
         int[] numbers = new int[count];
@@ -15,8 +13,6 @@ public class PrintNumReverse_02 {
         for (int position = 0; position <= numbers.length - 1; position++) {
             numbers[position] = Integer.parseInt(scanner.nextLine());
         }
-
-
 
         for (int position = numbers.length - 1; position >= 0; position--) {
             System.out.print(numbers[position] + " ");
