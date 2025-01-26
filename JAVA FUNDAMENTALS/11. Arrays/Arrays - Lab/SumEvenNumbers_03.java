5c5dbad3c9242c35bbe0bc4f158021f5b875fc3b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-26 01:58:27 +0100

    Update SumEvenNumbers_03.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/SumEvenNumbers_03.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/SumEvenNumbers_03.java
@@ -7,16 +7,17 @@ public class SumEvenNumbers_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         int [] numbers = Arrays.stream(scanner.nextLine()
                         .split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
+
         int sumEven = 0; 
         for (int number : numbers) {
-        
             if (number % 2 == 0) {
-           
+                sumEven += number;
             }
         }
 
