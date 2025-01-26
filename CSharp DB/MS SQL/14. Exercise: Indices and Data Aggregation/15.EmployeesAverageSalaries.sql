ef3bce596cb7d9d398aaea6dad387acc316f5cb5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-26 16:49:24 +0100

    Create 15.EmployeesAverageSalaries.sql

---
+++ CSharp DB/MS SQL/14. Exercise: Indices and Data Aggregation/15.EmployeesAverageSalaries.sql
@@ -0,0 +1,19 @@
+-- 15. Employees Average Salaries
+
+SELECT * INTO [TempTable]
+FROM Employees
+WHERE [Salary] > 30000
+
+DELETE
+FROM [TempTable]
+WHERE [ManagerID] = 42
+
+UPDATE TempTable
+SET [Salary] += 5000
+WHERE [DepartmentID] = 1
+
+SELECT
+    [DepartmentID]
+    , AVG([Salary]) AS [AverageSalary]
+FROM TempTable
+GROUP BY [DepartmentID]
