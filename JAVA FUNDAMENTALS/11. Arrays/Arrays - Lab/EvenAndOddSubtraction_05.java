5a33fcdfc5d0e93a53ee21fb7ad40ab08fbcb143
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-26 02:45:19 +0100

    Update EvenAndOddSubtraction_05.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/EvenAndOddSubtraction_05.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/EvenAndOddSubtraction_05.java
@@ -1,1 +1,29 @@
+package Lab11_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenAndOddSubtraction_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int [] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int number : numbers) {
+            if (number % 2 == 0){
+       
+                sumEven += number;
+            } else {
+          
+                sumOdd += number;
+            }
+        }
+
+        System.out.println(sumEven - sumOdd);
+
+    }
+}
 
