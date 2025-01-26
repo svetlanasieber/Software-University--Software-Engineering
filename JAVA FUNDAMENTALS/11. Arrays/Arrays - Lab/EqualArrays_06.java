0436c5a59d6c6b123687d6bffb0cb1a7897a8e2c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-26 02:46:46 +0100

    Update EqualArrays_06.java

--- JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/EqualArrays_06.java
+++ JAVA FUNDAMENTALS/11. Arrays/Arrays - Lab/EqualArrays_06.java
@@ -1,1 +1,43 @@
+package Lab11_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EqualArrays_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int [] firstArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int [] secondArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+
+        boolean isIdentical = true;
+       
+        int differentIndex = 0;
+        int sum = 0;
+
+        for (int position = 0; position <= firstArray.length - 1; position++) {
+            int elementFirstArray = firstArray[position]; 
+            int elementSecondArray = secondArray[position];
+
+            if (elementFirstArray == elementSecondArray) {
+                sum += elementFirstArray;
+            } else {
+            
+                isIdentical = false;
+                differentIndex = position;
+                break;
+            }
+        }
+
+        if (isIdentical) {
+            System.out.printf("Arrays are identical. Sum: %d", sum);
+        }
+     
+        else {
+            System.out.printf("Arrays are not identical. Found difference at %d index.", differentIndex);
+        }
+    }
+}
 
