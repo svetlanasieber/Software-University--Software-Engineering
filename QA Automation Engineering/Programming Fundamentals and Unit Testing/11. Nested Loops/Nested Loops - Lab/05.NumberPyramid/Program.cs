978822c00715f89897ec08967b2554f81311f9f3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:12:00 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/05.NumberPyramid/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/05.NumberPyramid/Program.cs
@@ -1,1 +1,21 @@
+int n = int.Parse(Console.ReadLine());
+int currentNum = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(currentNum + " ");
+        currentNum++;
+        if (currentNum > n)
+        {
+            break;
+        }
+    }
+    Console.WriteLine();
+    if (currentNum > n)
+    {
+        break;
+    }
+}
 
