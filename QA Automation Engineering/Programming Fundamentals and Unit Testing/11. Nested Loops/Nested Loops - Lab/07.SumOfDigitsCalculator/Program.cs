bfd66351fb08178cc46e869b430914a5d3cf4932
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:16:14 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/07.SumOfDigitsCalculator/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/07.SumOfDigitsCalculator/Program.cs
@@ -2,24 +2,23 @@ string input = Console.ReadLine();
 
 while (input != "End")
 {
-  
+
     int number = int.Parse(input); 
     int sumDigits = 0; 
 
+
     while (number > 0) 
     {
-       
+        
         int lastDigit = number % 10;
-    
+     
         sumDigits += lastDigit;
-    
+
         number /= 10;
     }
 
-
     Console.WriteLine("Sum of digits = " + sumDigits);
 
-  
     input = Console.ReadLine();
 }
 
