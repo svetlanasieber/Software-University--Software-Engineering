6f19fee75abac187d6f894f7105d9791def59e4c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:18:19 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/08.PrimeNumbers/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/08.PrimeNumbers/Program.cs
@@ -1,1 +1,25 @@
+int startNumber = int.Parse(Console.ReadLine());
+int endNumber = int.Parse(Console.ReadLine());
+
+
+for (int number = startNumber; number <= endNumber; number++)
+{
+  
+    int count = 0; 
+    for (int divisor = 1; divisor <= number; divisor++)
+    {
+        if (number % divisor == 0)
+        {
+           
+            count++;
+        }
+    }
+ 
+    if (count == 2)
+    {
+        
+        Console.Write(number + " ");
+    }
+}
+
 
