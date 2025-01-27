e30ebeacbb9041f34a5959c807c1828e5d6e6507
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-28 00:31:39 +0100

    Add files via upload

---
+++ JAVA WEB/SPRING Fundamentals/09. Exercise: Spring MVC with Thymeleaf Advanced/smart-wallet-application/src/main/java/app/web/IndexController.java
@@ -0,0 +1,88 @@
+package app.web;
+
+import app.user.model.User;
+import app.user.service.UserService;
+import app.web.dto.LoginRequest;
+import app.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Controller
+public class IndexController {
+
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getIndexPage() {
+
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView getLoginPage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        modelAndView.addObject("loginRequest", new LoginRequest());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid LoginRequest loginRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()){
+            return "login";
+        }
+
+        userService.login(loginRequest);
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegisterPage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerNewUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("register");
+        }
+
+        userService.register(registerRequest);
+
+        return new ModelAndView("redirect:/home");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getHomePage() {
+
+        User user = userService.getById(UUID.fromString("0a9b6944-9702-424e-b7c7-c8e82a1b5f3f"));
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+}
