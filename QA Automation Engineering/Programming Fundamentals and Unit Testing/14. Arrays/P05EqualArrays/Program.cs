988fc470728b00fd8b57a7ecabe8b4ced04a4665
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:21:40 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/14. Arrays/P05EqualArrays/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/14. Arrays/P05EqualArrays/Program.cs
@@ -1,1 +1,48 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] array1 = Console.ReadLine()
+                              .Split(' ')
+                              .Select(int.Parse)
+                              .ToArray();
+
+        
+        int[] array2 = Console.ReadLine()
+                              .Split(' ')
+                              .Select(int.Parse)
+                              .ToArray();
+
+        
+        if (array1.Length != array2.Length)
+        {
+            Console.WriteLine("Arrays are not identical.");
+            return;
+        }
+
+        bool areIdentical = true;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i] != array2[i])
+            {
+                areIdentical = false;
+                break;
+            }
+        }
+
+      
+        if (areIdentical)
+        {
+            Console.WriteLine("Arrays are identical.");
+        }
+        else
+        {
+            Console.WriteLine("Arrays are not identical.");
+        }
+    }
+}
 
