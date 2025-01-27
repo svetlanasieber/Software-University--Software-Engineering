c6c25001058b6f8ae86165f2c27edb82c703dc72
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 18:06:49 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/07.SpecialBonus/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/10. Exercise: Loops/Loops - Exercise/07.SpecialBonus/Program.cs
@@ -1,1 +1,14 @@
+int stopNumber = int.Parse(Console.ReadLine());
 
+int currentNum = int.Parse(Console.ReadLine());
+
+int previousNum = 0;
+
+while (currentNum != stopNumber)
+{
+	previousNum = currentNum;
+
+	currentNum = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(previousNum * 1.2);
