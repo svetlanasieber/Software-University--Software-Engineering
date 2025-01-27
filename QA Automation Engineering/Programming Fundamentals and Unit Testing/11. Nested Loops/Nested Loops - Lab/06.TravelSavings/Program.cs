641179bb6b9c5106e77ddc94be690de4de008976
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:12:45 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/06.TravelSavings/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/06.TravelSavings/Program.cs
@@ -1,1 +1,14 @@
+string destination = Console.ReadLine();
+while (destination != "End")
+{
+    double neededSum = double.Parse(Console.ReadLine());
+    double collectedSum = 0;
+    while (collectedSum < neededSum)
+    {
+        collectedSum += double.Parse(Console.ReadLine());
+        Console.WriteLine($"Collected: {collectedSum:F2}");
+    }
+    Console.WriteLine($"Going to {destination}!");
 
+    destination = Console.ReadLine();
+}
