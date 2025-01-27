d36f4c00006f59bb2c10f6234ded47c7a78149e9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:10:54 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/04.Building/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/04.Building/Program.cs
@@ -1,1 +1,29 @@
+int floors = int.Parse(Console.ReadLine());
+int estates = int.Parse(Console.ReadLine());
 
+
+for (int floor = floors; floor >= 1; floor--)
+{
+   
+    for (int estate = 0; estate < estates; estate++)
+    {
+        
+        if (floor == floors)
+        {
+           
+            Console.Write($"L{floor}{estate} ");
+        }
+        else if (floor % 2 == 0)
+        {
+           
+            Console.Write($"O{floor}{estate} ");
+        }
+        else
+        {
+            
+            Console.Write($"A{floor}{estate} ");
+        }
+    }
+
+    Console.WriteLine(); 
+}
