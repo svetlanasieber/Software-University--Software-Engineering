172fdf192a1238a2dc4abd3a784b87ab44417637
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-27 23:09:43 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/03.TriangleOfStars/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/11. Nested Loops/Nested Loops - Lab/03.TriangleOfStars/Program.cs
@@ -1,1 +1,11 @@
+int size = int.Parse(Console.ReadLine());
 
+
+for (int row = 1; row <= size; row++)
+{
+  
+    for (int count = 1; count <= row; count++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine(); 
