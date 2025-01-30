6421823d1641c9c65d4a558aacd67318a2e70b2d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-30 02:48:11 +0100

    Create Grades.java

---
+++ JAVA FUNDAMENTALS/14. Methods/Methods - Lab/Grades.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        printGradeName(grade);
+
+    }
+
+    public static void printGradeName(double grade) {
+        if (grade >= 2 && grade < 3) {
+            System.out.println("Fail");
+        } else if (grade >= 3 && grade < 3.5) {
+            System.out.println("Poor");
+        } else if (grade >= 3.5 && grade < 4.5) {
+            System.out.println("Good");
+        } else if (grade >= 4.5 && grade < 5.5) {
+            System.out.println("Very good");
+        } else {
+            System.out.println("Excellent");
+        }
+    }
+}
