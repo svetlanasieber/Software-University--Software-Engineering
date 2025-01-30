6aed62b10ec2a3c1a8db2126bc6f2273805cc7f2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-30 03:03:29 +0100

    Create Program.cs

---
+++ CSharp FUNDAMENTALS/14. Methods/Methods - Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,10 @@
+int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+
+PrintRectangleArea(width, length);
+
+static void PrintRectangleArea(int width, int length)
+{
+    int area = width * length;
+    Console.WriteLine(area);
+}
