9548047eed4e790846041fac4d039c8807ee8536
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 21:12:23 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/14. Arrays/P06CommonElements/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/14. Arrays/P06CommonElements/Program.cs
@@ -1,1 +1,48 @@
+/*
+
+using System.Linq.Expressions;
+using System.Runtime.ExceptionServices;
+
+int[] array1 = Console.ReadLine()
+                      .Split(' ')
+                      .Select(int.Parse)
+                      .ToArray();
+
+
+int[] array2 = Console.ReadLine()
+                      .Split(' ')
+                      .Select(int.Parse)
+                      .ToArray();
+
+foreach (int element in array1)
+{
+    if (array2.Contains(element))
+        Console.Write(element + " ");
+}
+*/
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] firstArray = Console.ReadLine()
+                                  .Split(' ')
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+
+        int[] secondArray = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+
+        int[] commonElements = firstArray.Intersect(secondArray).ToArray();
+        Console.WriteLine(string.Join(" ", commonElements));
+    }
+}
 
