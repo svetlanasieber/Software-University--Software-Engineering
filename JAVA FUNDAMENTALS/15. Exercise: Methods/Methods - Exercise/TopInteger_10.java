830ef5b88e5f5c45202b0c4b56ee1fd456e75dae
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 20:56:18 +0100

    Update TopInteger_10.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/TopInteger_10.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/TopInteger_10.java
@@ -8,30 +8,26 @@ public class TopInteger_10 {
 
         int n = Integer.parseInt(scanner.nextLine());
         for (int number = 1; number <= n; number++) {
-            //проверка дали е топ
-            //1. сумата от цифрите да се дели на 8 -> checkSumDigitsIsDivisibleBy8
+
             boolean isValidSumDigits = checkSumDigitsIsDivisibleBy8(number);
-            //2. съдържа поне една нечетна цифра
+       
             boolean isContainsOddDigit = checkContainsOddDigits(number);
 
             if (isValidSumDigits && isContainsOddDigit) {
-                //топ число
+          
                 System.out.println(number);
             }
         }
     }
 
-    //метод, който проверява дали сумата от цифрите на числото се дели на 8
-    //true -> сумата се дели на 8
-    //false -> сумата не се дели на 8
     public static boolean checkSumDigitsIsDivisibleBy8 (int number) {
-        int sum = 0; //сума от цифрите
+        int sum = 0;
         while (number > 0) {
-            int lastDigit = number % 10; //последна цифра
-            sum += lastDigit; //сумираме последна цифра
-            number /= 10; //премахваме последната цифра
+            int lastDigit = number % 10;
+            sum += lastDigit;
+            number /= 10;
         }
-        //сумата от цифрите на числото
+
         if (sum % 8 == 0) {
             return true;
         } else {
@@ -39,23 +35,16 @@ public class TopInteger_10 {
         }
     }
 
-    //метод, който проверява дали имаме поне една нечетна цифра в числото
-    //true -> ако имаме поне една нечетна цифра
-    //false -> ако нямаме нито една нечетна цифра
     public static boolean checkContainsOddDigits (int number) {
-        //4365
+
         while (number > 0) {
-            int lastDigit = number % 10; // последна цифра
+            int lastDigit = number % 10; 
             if (lastDigit % 2 != 0) {
-                //нечетна цифра -> поне една нечетна
                 return true;
             } else {
-                //четна цифра -> премахваме
                 number /= 10;
             }
         }
-
-        //проверили всички цифри и никоя не е била нечетна
         return false;
     }
 }
