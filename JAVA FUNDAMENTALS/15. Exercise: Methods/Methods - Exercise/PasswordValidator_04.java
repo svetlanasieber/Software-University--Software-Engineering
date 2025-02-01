f1aef3f618d0fdea73c48d9a8f0a3468d12fcc2c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 20:47:03 +0100

    Update PasswordValidator_04.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/PasswordValidator_04.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/PasswordValidator_04.java
@@ -13,71 +13,51 @@ public class PasswordValidator_04 {
         }
 
         boolean isValidContent = isValidContent(password);
-        //isValidContent = true -> валидно съдържание
-        //isValidContent = false -> невалидно съдържание
+
         if (!isValidContent) {
             System.out.println("Password must consist only of letters and digits");
         }
 
-        //3. проверим дали брой на цифрите е валиден
         boolean isValidCount = isValidCountDigits(password);
-        //isValidCount = true -> валиден брой
-        //isValidCount = false -> невалиден брой
+
         if (!isValidCount) {
             System.out.println("Password must have at least 2 digits");
         }
 
-        //4. проверяваме дали цялостно е валидна паролата
         if (isValidLength && isValidContent && isValidCount) {
             System.out.println("Password is valid");
         }
 
     }
 
-    //метод, който проверява дали ми е валидна дължината на паролата
-    //true -> ако дължината на паролата е валидна
-    //false -> ако дължината на паролата не е валидна
     public static boolean isValidLength (String password) {
-        //валидна дължина: [6; 10]
-        //невалидна дължина: < 6 или > 10
+
         return password.length() >= 6 && password.length() <= 10;
     }
 
-    //метод, който проверява дали ми е валидно съдържанието на паролата
-    //true -> ако съдържанието е валидно
-    //false -> ако съдържанието не е валидно
     public static boolean isValidContent (String password) {
-        //password = "logIn".toCharArray() -> ['l', 'o', 'g', 'I', 'n']
-        //валидно съдържание: букви или цифри
-        //невалидно съдържание: нещо различно от буква или цифра
+ 
         for (char symbol : password.toCharArray()) {
             if (!Character.isLetterOrDigit(symbol)) {
-                //isLetterOrDigit
-                //true ако символа е буква или цифра
-                //false ако символа не е нито буква, нито цифра
+
                 return false;
             }
         }
-        //обходили всички символи
+
         return true;
     }
 
-    //метод, който проверява дали е валиден броя на цифрите в паролата
-    //true -> ако броят е валиден >= 2
-    //false -> ако броят е невалиден < 2
+
     public static boolean isValidCountDigits (String password) {
-        int countDigits = 0; //брой на цифрите в паролата
-        //password = "Pesho123".toCharArray()
+        int countDigits = 0; 
+
         for (char symbol :  password.toCharArray()) {
             if (Character.isDigit(symbol)) {
-                //isDigit
-                //true -> ако символа е цифра
-                //false -> ако символа не е цифра
+
                 countDigits++;
             }
         }
-        //true -> брой >= 2
-        //false -> брой < 2
+
         return countDigits >= 2;
     }
 }
