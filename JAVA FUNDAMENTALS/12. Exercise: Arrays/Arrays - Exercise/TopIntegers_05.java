f5305d5fbadbda8be98c884fd93947e2a7d06a29
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 20:32:32 +0100

    Update TopIntegers_05.java

--- JAVA FUNDAMENTALS/12. Exercise: Arrays/Arrays - Exercise/TopIntegers_05.java
+++ JAVA FUNDAMENTALS/12. Exercise: Arrays/Arrays - Exercise/TopIntegers_05.java
@@ -13,31 +13,24 @@ public class TopIntegers_05 {
 
         for (int position = 0; position <= numbers.length - 1; position++) {
 
-            //винаги последното число е топ -> отпечатваме го
             if (position == numbers.length - 1) {
-                //последното число
                 System.out.print(numbers[position]);
                 break;
             }
 
-            //останалите числа
+           
             int currentNumber = numbers[position];
-            //проверка дали е топ число
+       
             boolean isTop = true;
-            //isTop = true -> числото ми е топ
-            //isTop = false -> числото ми не е топ
 
-            //обхождаме всички позиции след моята
             for (int nextPosition = position + 1; nextPosition <= numbers.length - 1; nextPosition++) {
                 int nextNumber = numbers[nextPosition]; //числа, след моето
                 if (currentNumber <= nextNumber) {
-                    //не е топ
                     isTop = false;
                     break;
                 }
             }
 
-            //знам дали числото е топ или не
             if (isTop) {
                 System.out.print(currentNumber + " ");
             }
