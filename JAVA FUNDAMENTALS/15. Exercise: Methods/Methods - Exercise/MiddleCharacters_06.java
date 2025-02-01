7eac9d72bd41831ca6d7023b30819360a98a1b57
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 01:55:11 +0100

    Update MiddleCharacters_06.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/MiddleCharacters_06.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/MiddleCharacters_06.java
@@ -1,5 +1,3 @@
-package Methods_Exercise;
-
 import java.util.Scanner;
 
 public class MiddleCharacters_06 {
@@ -11,24 +9,20 @@ public class MiddleCharacters_06 {
 
     }
 
-    //метод, който принтира средния символ
     public static void printMiddleCharacter (String text) {
-        int length = text.length(); //дължина = брой символи
-        //нечетна дължина -> 1 среден символ
+        int length = text.length();
+     
         if (length % 2 != 0) {
-            //"aString" 7 символа -> 'r' 3 позиция
-            //"Pesho" 5 символа -> 's' 2 позиция
-            //"Pes" 3 символа -> 'е' 1 позиция
+
             int positionMiddleSymbol = length / 2;
-            //позиция на средния символ при нечетна дължина
+
             System.out.println(text.charAt(positionMiddleSymbol));
         }
-        //четна дължина -> 2 средни символа
+    
         else {
-            //"tables" 6 символа -> b (2 позиция) и l (3 позиция)
-            //"Desi" 4 символа -> е (1 позиция) и s (2 позиция)
-            int positionFirstMiddleCharacter = length / 2 - 1; //позиция на първия среден символ
-            int positionSecondMiddleCharacter = length / 2; //позиция на втория среден символ
+ 
+            int positionFirstMiddleCharacter = length / 2 - 1;
+            int positionSecondMiddleCharacter = length / 2; 
 
             System.out.print(text.charAt(positionFirstMiddleCharacter));
             System.out.print(text.charAt(positionSecondMiddleCharacter));
