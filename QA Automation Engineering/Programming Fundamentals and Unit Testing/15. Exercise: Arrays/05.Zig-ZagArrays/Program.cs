4777669c0ed20c53d8bf417201034711f679becc
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 21:28:41 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/05.Zig-ZagArrays/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/05.Zig-ZagArrays/Program.cs
@@ -5,26 +5,25 @@ int[] secondArray = new int[n];
 
 for (int i = 0; i < n; i++)
 {
-	int[] rowData = Console.ReadLine()
-							.Split()
-							.Select(int.Parse)
-							.ToArray();
+    int[] rowData = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
 
-	int firstNumber = rowData[0];
-	int secondNumber = rowData[1];
+    int firstNumber = rowData[0];
+    int secondNumber = rowData[1];
 
-	if (i % 2 == 0)
-	{
-		firstArray[i] = firstNumber;
-		secondArray[i] = secondNumber;
-	}
-	else
-	{
-		firstArray[i] = secondNumber;
-		secondArray[i] = firstNumber;
-	}
+    if (i % 2 == 0)
+    {
+        firstArray[i] = firstNumber;
+        secondArray[i] = secondNumber;
+    }
+    else
+    {
+        firstArray[i] = secondNumber;
+        secondArray[i] = firstNumber;
+    }
 }
 
 Console.WriteLine(string.Join(" ", firstArray));
 Console.WriteLine(string.Join(" ", secondArray));
-
