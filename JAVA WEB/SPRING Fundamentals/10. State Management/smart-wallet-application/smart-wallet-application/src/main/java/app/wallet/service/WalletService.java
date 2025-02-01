471e4aa93c7a7f38ee23eff25902b21c5c46fdfa
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-02 00:53:31 +0100

    Update WalletService.java

--- JAVA WEB/SPRING Fundamentals/10. State Management/smart-wallet-application/smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
+++ JAVA WEB/SPRING Fundamentals/10. State Management/smart-wallet-application/smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
@@ -107,11 +107,6 @@ public class WalletService {
                     "Invalid criteria for transfer");
         }
 
-        // Money Transfer
-        // Ivan -> Gosho | 20 EUR
-        // Ivan -20.00 EUR
-        // Gosho +20.00 EUR
-
         Transaction withdrawal = charge(sender, senderWallet.getId(), transferRequest.getAmount(), transferDescription);
         if (withdrawal.getStatus() == TransactionStatus.FAILED) {
             return withdrawal;
