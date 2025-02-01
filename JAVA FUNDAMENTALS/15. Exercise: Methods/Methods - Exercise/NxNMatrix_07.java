2a574ab3c26bc935497df8714f38d59798cb1027
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 01:55:49 +0100

    Update NxNMatrix_07.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/NxNMatrix_07.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/NxNMatrix_07.java
@@ -1,23 +1,18 @@
-package Methods_Exercise;
-
 import java.util.Scanner;
 
 public class NxNMatrix_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine()); // брой редове = брой колони
+        int n = Integer.parseInt(scanner.nextLine()); 
 
         printMatrix(n);
 
-
-
-
     }
-    //метод, който отпечатва матрица с редове = колони = n
+ 
     private static void printMatrix (int n) {
-        for (int row = 1; row <= n; row++) { //всички редове
-            for (int col = 1; col <= n; col++) { //всички колони
+        for (int row = 1; row <= n; row++) { 
+            for (int col = 1; col <= n; col++) {
                 System.out.print(n + " ");
             }
             System.out.println();
