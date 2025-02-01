cdc78d9606585a3685eccb40afa955f101645212
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 21:13:43 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/03.CondenseArrayToNumber/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/03.CondenseArrayToNumber/Program.cs
@@ -1,1 +1,19 @@
+int[] numbers = Console.ReadLine()
+						  .Split(" ")
+						  .Select(int.Parse)
+						  .ToArray();
 
+while (numbers.Length > 1)
+{
+	int[] condensedArray = new int[numbers.Length - 1];
+
+	for (int i = 0; i < numbers.Length - 1; i++)
+	{
+		int currentSum = numbers[i] + numbers[i + 1];
+		condensedArray[i] = currentSum;
+	}
+
+	numbers = condensedArray;
+}
+
+Console.WriteLine(numbers[0]);
