fdb4972bbc6a25de010823ab39ee89a861644749
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 21:15:15 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/04.MagicSum/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/04.MagicSum/Program.cs
@@ -1,1 +1,25 @@
+int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
 
+int controlNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    int currentElement = numbers[i];
+
+    if (currentElement > controlNumber)
+        continue;
+
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        int nextRightElement = numbers[j];
+
+        if (currentElement + nextRightElement == controlNumber)
+        {
+            Console.WriteLine(currentElement + " " + nextRightElement);
+            break;
+        }
+    }
+}
