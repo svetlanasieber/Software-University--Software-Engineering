c08288a2b05a6a77e9240a32bb153359a73999ef
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 21:30:00 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/07.TopIntegers/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/07.TopIntegers/Program.cs
@@ -1,1 +1,32 @@
+int[] numbersArray = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
 
+
+for (int i = 0; i < numbersArray.Length - 1; i++)  
+{
+
+    bool isGreater = true;
+
+    int currentElement = numbersArray[i]; 
+
+    for (int j = i + 1; j < numbersArray.Length; j++)
+    {
+    
+        int nextRightElement = numbersArray[j]; 
+
+        if (nextRightElement >= currentElement)
+        {
+            isGreater = false;
+            break;
+        }
+    }
+
+    if (isGreater)
+    {
+        Console.Write(currentElement + " ");
+    }
+}
+
+Console.Write(numbersArray[numbersArray.Length - 1]);
