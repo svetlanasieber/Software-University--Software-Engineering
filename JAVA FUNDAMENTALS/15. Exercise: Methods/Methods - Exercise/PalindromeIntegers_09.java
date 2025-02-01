086fa897ccbfd51cb8002d6fbe6c83c585c07224
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-01 01:56:42 +0100

    Update PalindromeIntegers_09.java

--- JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/PalindromeIntegers_09.java
+++ JAVA FUNDAMENTALS/15. Exercise: Methods/Methods - Exercise/PalindromeIntegers_09.java
@@ -1,18 +1,13 @@
-package Methods_Exercise;
-
 import java.util.Scanner;
 
 public class PalindromeIntegers_09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //повтаряме: четем число -> проверка да ли е палиндром
-
-        String input = scanner.nextLine(); //входни данни -> тескт: "123", "323", "bob", "ivan"
+        String input = scanner.nextLine(); 
 
         while (!input.equals("END")) {
 
-            //проверка дали е палиндром
             if (isPalindrome(input)) {
                 System.out.println("true");
             } else {
@@ -23,13 +18,9 @@ public class PalindromeIntegers_09 {
         }
     }
 
-    //метод, който проверява дали дадена дума е палиндром
-    //true -> ако текстът е палинром
-    //false -> ако текстът не е палиндром
     public static boolean isPalindrome (String text) {
-        //палиндром: text == reversedText
-        //String text - текста прочетен от ляво към дясно
-        String reversedText = ""; //текста прочетен от дясно към ляво
+  
+        String reversedText = "";
 
         for (int position = text.length() - 1; position >= 0 ; position--) {
             reversedText += text.charAt(position);
