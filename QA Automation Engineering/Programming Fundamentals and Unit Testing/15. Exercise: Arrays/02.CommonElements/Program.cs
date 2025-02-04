4df05cf30452c7784a9b83992f13c76c30ad11f0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-04 18:07:08 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/02.CommonElements/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/02.CommonElements/Program.cs
@@ -1,74 +1,14 @@
-using System;
-using System.Linq;
-
-class Program
-{
-    static void Main()
-    {
-        
-        int[] firstArray = Console.ReadLine()
-                                  .Split(' ')
-                                  .Select(int.Parse)
-                                  .ToArray();
-
-       
-        int[] secondArray = Console.ReadLine()
-                                   .Split(' ')
-                                   .Select(int.Parse)
-                                   .ToArray();
-
-       
-        int[] commonElements = firstArray.Intersect(secondArray).ToArray();
-        Console.WriteLine(string.Join(" ", commonElements));
-    }
-}
-
-
-/* ver2
 int[] firstArray = Console.ReadLine()
-						  .Split(" ")
-						  .Select(int.Parse)
-						  .ToArray();
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
 
 int[] secondArray = Console.ReadLine()
-						  .Split(" ")
-						  .Select(int.Parse)
-						  .ToArray();
-
-for (int i = 0; i < firstArray.Length; i++)
-{
-	int currentElement = firstArray[i];
-
-	for (int j = 0; j < secondArray.Length; j++)
-	{
-		if (currentElement == secondArray[j])
-		{
-			Console.Write(currentElement + " ");
-		}
-	}
-}
-*/
-
-/*
-
-/*ver3
-using System.Linq.Expressions;
-using System.Runtime.ExceptionServices;
-
-int[] array1 = Console.ReadLine()
-                      .Split(' ')
-                      .Select(int.Parse)
-                      .ToArray();
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
 
 
-int[] array2 = Console.ReadLine()
-                      .Split(' ')
-                      .Select(int.Parse)
-                      .ToArray();
-
-foreach (int element in array1)
-{
-    if (array2.Contains(element))
-        Console.Write(element + " ");
-}
-*/
+int[] commonElements = firstArray.Intersect(secondArray).ToArray();
+Console.WriteLine(string.Join(" ", commonElements));
