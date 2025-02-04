4aea7f82fc0a86b9456fd7a22e3bbfa1d8cacbd8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-04 01:16:26 +0100

    Update RemoveNegativesAndReverse_06.java

--- JAVA FUNDAMENTALS/17. List/List - Lab/RemoveNegativesAndReverse_06.java
+++ JAVA FUNDAMENTALS/17. List/List - Lab/RemoveNegativesAndReverse_06.java
@@ -7,35 +7,21 @@ public class RemoveNegativesAndReverse_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
-                                .map(Integer::parseInt).collect(Collectors.toList()); //всички числа
+                                .map(Integer::parseInt).collect(Collectors.toList()); 
 
-        //numbers = {10, -5, 7, 9, -33, 50}
-        
-        List<Integer> positiveNumbers = new ArrayList<>(); //положителните числа
+        List<Integer> positiveNumbers = new ArrayList<>();
 
-        for (int number : numbers) { //всяко едно число от първоначалния списък
+        for (int number : numbers) { 
             if (number > 0) {
-                //положително число
                 positiveNumbers.add(number);
             }
         }
-        //positiveNumbers -> имаме само положителните числа от първоначалния списък
-        //positiveNumbers = {10, 7, 9, 50}
-        //reverse (обръщаме на обратно)
-        Collections.reverse(positiveNumbers);
-        //positiveNumbers = {50, 9, 7, 10}
 
-        //празен списък
+        Collections.reverse(positiveNumbers);
+
         if (positiveNumbers.isEmpty()) {
-            //празен списък
             System.out.println("empty");
         } else {
-            //има елементи в списъка
-            /*System.out.println(positiveNumbers.toString()           //"[50, 10, 9, 7]"
-                                .replace("[", "")   //"50, 10, 9, 7]"
-                                .replace("]", "")   //"50, 10, 9, 7"
-                                .replaceAll(",", "") //"50 10 9 7"
-            );*/
 
             for (int number : positiveNumbers) {
                 System.out.print(number + " ");
