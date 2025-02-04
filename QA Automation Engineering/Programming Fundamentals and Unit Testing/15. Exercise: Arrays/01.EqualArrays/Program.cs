416c49c5b1f7252673a5b6aaed8f04378b603673
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-04 17:56:25 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/01.EqualArrays/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/15. Exercise: Arrays/01.EqualArrays/Program.cs
@@ -1,46 +1,37 @@
-using System;
-using System.Linq;
+int[] array1 = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
 
-class Program
+
+int[] array2 = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+
+if (array1.Length != array2.Length)
 {
-    static void Main()
+    Console.WriteLine("Arrays are not identical.");
+    return;
+}
+
+bool areIdentical = true;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i] != array2[i])
     {
-       
-        int[] array1 = Console.ReadLine()
-                              .Split(' ')
-                              .Select(int.Parse)
-                              .ToArray();
-
-      
-        int[] array2 = Console.ReadLine()
-                              .Split(' ')
-                              .Select(int.Parse)
-                              .ToArray();
-
-       
-        if (array1.Length != array2.Length)
-        {
-            Console.WriteLine("Arrays are not identical.");
-            return;
-        }
-
-        bool areIdentical = true;
-        for (int i = 0; i < array1.Length; i++)
-        {
-            if (array1[i] != array2[i])
-            {
-                areIdentical = false;
-                break;
-            }
-        }
-
-        if (areIdentical)
-        {
-            Console.WriteLine("Arrays are identical.");
-        }
-        else
-        {
-            Console.WriteLine("Arrays are not identical.");
-        }
+        areIdentical = false;
+        break;
     }
+}
+
+
+if (areIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
 }
