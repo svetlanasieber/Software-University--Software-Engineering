95d32f96ccb280732abcf936db56ec8586586c24
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-04 01:15:23 +0100

    Update ListOfProducts_05.java

--- JAVA FUNDAMENTALS/17. List/List - Lab/ListOfProducts_05.java
+++ JAVA FUNDAMENTALS/17. List/List - Lab/ListOfProducts_05.java
@@ -8,22 +8,17 @@ import java.util.Scanner;
 public class ListOfProducts_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int countProducts = Integer.parseInt(scanner.nextLine()); //брой продуктите
-        List<String> products = new ArrayList<>(); //списък с продуктите
+        int countProducts = Integer.parseInt(scanner.nextLine()); 
+        List<String> products = new ArrayList<>(); 
 
         for (int count = 1; count <= countProducts; count++) {
             String productName = scanner.nextLine();
             products.add(productName);
         }
 
-        //products = {"Potatoes", "Tomatoes", "Onions", "Apples"}
-        //sort -> сортира в ascending order (нарастващ ред)
-        //списък с числа.sort -> нарастващ ред от най-малкото към най-голямото
-        //списък с текстове.sort -> нарастващ ред от A към Z
         Collections.sort(products);
-        //products = {"Apples", "Onions", "Potatoes", "Tomato"}
 
-        int number = 1; //пореден номер на продукта
+        int number = 1; 
         for (String product : products) {
             System.out.println(number + "." + product);
             number++;
