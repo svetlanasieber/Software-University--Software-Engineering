4c215bd13103760c42432504ef7b5247d872c6eb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-06 21:38:57 +0100

    Add files via upload

---
+++ Python Introduction for Data Science/09. Functions/05.Orders.py
@@ -0,0 +1,16 @@
+def total_price_of_order(order_type, order_quantity):
+    total_price = None
+    if order_type == "coffee":
+        total_price = order_quantity * 1.50
+    elif order_type == "coke":
+        total_price = order_quantity * 1.40
+    elif order_type == "water":
+        total_price = order_quantity * 1.00
+    elif order_type == "snacks":
+        total_price = order_quantity * 2.00
+    return total_price
+
+
+product = input()
+quantity = int(input())
+print(f"{total_price_of_order(product, quantity):.2f}")
