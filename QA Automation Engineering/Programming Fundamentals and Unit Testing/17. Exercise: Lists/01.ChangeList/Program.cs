f266659b9b5699015d24a48ce410578da237caab
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-10 19:24:29 +0100

    Create Program.cs

---
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/17. Exercise: Lists/01.ChangeList/Program.cs
@@ -0,0 +1,26 @@
+List<int> list = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToList();
+
+string commands;
+while ((commands = Console.ReadLine()) != "end")
+{
+    string[] arguments = commands.Split();
+    int index;
+
+    switch (arguments[0])
+    {
+        case "Insert":
+            int item = int.Parse(arguments[1]);
+            index = int.Parse(arguments[2]);
+            list.Insert(index, item);
+            break;
+        case "Delete":
+            item = int.Parse(arguments[1]);
+            list.Remove(item);
+            break;
+    }
+}
+
+Console.WriteLine(string.Join(" ", list));
