5c441e50bdb65fcd69eba995d21bd8b0841c5c1a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-12 14:33:34 +0100

    Update ChangeList_02.java

--- JAVA FUNDAMENTALS/18. Exercise: List/Lists - Exercise/ChangeList_02.java
+++ JAVA FUNDAMENTALS/18. Exercise: List/Lists - Exercise/ChangeList_02.java
@@ -13,20 +13,19 @@ public class ChangeList_02 {
         List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
                 .map(Integer::parseInt).collect(Collectors.toList());
 
-        //numbers = {1, 2, 3, 4, 5, 5, 5, 6}
+
         String command = scanner.nextLine();
         while (!command.equals("end")) {
             if (command.contains("Delete")) {
-                //command = "Delete 5".split(" ") -> ["Delete", "5"]
-                int numberForDelete = Integer.parseInt(command.split(" ")[1]); //"5" -> 5
+                int numberForDelete = Integer.parseInt(command.split(" ")[1]); 
                 numbers.removeAll(List.of(numberForDelete));
                 //numbers = {1, 2, 3, 4, 6}
             } else if (command.contains("Insert")) {
-                //command = "Insert 34 2".split(" ") -> ["Insert", "34", "2"]
-                int numberForInsertion = Integer.parseInt(command.split(" ")[1]); //"34" -> 34
-                int position = Integer.parseInt(command.split(" ")[2]); //"2" -> 2
+              
+                int numberForInsertion = Integer.parseInt(command.split(" ")[1]); 
+                int position = Integer.parseInt(command.split(" ")[2]); 
                 numbers.add(position, numberForInsertion);
-                //numbers = {1, 2, 34, 3, 4, 5, 5, 5, 6}
+      
             }
 
             command = scanner.nextLine();
