cf655f114867ed26cc0ee291449b438a7c78c877
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-12 22:37:57 +0100

    Add files via upload

---
+++ JAVA WEB/SPRING Fundamentals/PaintingsCollectors-App/src/main/java/com/paintingscollectors/painting/model/Painting.java
@@ -0,0 +1,38 @@
+package com.paintingscollectors.painting.model;
+
+import com.paintingscollectors.user.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Painting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Style style;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    private int votes;
+
+    @ManyToOne
+    private User owner;
+}
---
+++ JAVA WEB/SPRING Fundamentals/PaintingsCollectors-App/src/main/java/com/paintingscollectors/user/model/User.java
@@ -0,0 +1,36 @@
+package com.paintingscollectors.user.model;
+
+import com.paintingscollectors.painting.model.FavouritePainting;
+import com.paintingscollectors.painting.model.Painting;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    private List<Painting> paintings;
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    private List<FavouritePainting> favouritePaintings;
+}
