059b42eeb5f1cba21e7c9e12fd68cc2cab2fb122
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-12 14:36:52 +0100

    Update Train_01.java

--- JAVA FUNDAMENTALS/18. Exercise: List/Lists - Exercise/Train_01.java
+++ JAVA FUNDAMENTALS/18. Exercise: List/Lists - Exercise/Train_01.java
@@ -13,32 +13,26 @@ public class Train_01 {
         List<Integer> wagons = Arrays.stream(scanner.nextLine().split(" "))
                 .map(Integer::parseInt).collect(Collectors.toList());
 
-        //{32, 54, 21, 12, 4, 0, 23}
 
-        int maxCapacity = Integer.parseInt(scanner.nextLine()); //макс брой хора във всеки вагон
 
-        //повтаряме: въвеждаме команда
-        //стоп: команда == "end"
-        //продължаваме: команда != "end"
+        int maxCapacity = Integer.parseInt(scanner.nextLine()); 
+
         String command = scanner.nextLine();
         while (!command.equals("end")) {
             if (command.contains("Add")) {
-                //1. command = "Add 12".split(" ") -> ["Add", "12"]
-                //добавяме числото в края на списъка
+
                 int count = Integer.parseInt(command.split(" ")[1]);
                 wagons.add(count);
-                //{32, 54, 21, 12, 4, 0, 23, 12}
+            
             } else {
-                //2. command = "23"
-                int passengers = Integer.parseInt(command); //бр. хората, които да добавим в някой вагон
-                //"23" -> 23
-                //maxCapacity = 75
-                //{32, 54, 21, 12, 4, 0, 23, 12}
+              
+                int passengers = Integer.parseInt(command);
+
                 for (int wagon = 0; wagon <= wagons.size() - 1; wagon++) {
                     int currentPassengersInWagon = wagons.get(wagon);
-                    //проверка: мога ли да кача хората в текущия вагон
+
                     if (currentPassengersInWagon + passengers <= maxCapacity) {
-                        //качвам хората в текущия вагон
+
                         wagons.set(wagon, currentPassengersInWagon + passengers);
                         break;
                     }
