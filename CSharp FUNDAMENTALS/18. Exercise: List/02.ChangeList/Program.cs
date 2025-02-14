4be8509b767c23bd2950d35fee07035672838dc4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-14 01:01:36 +0100

    Update Program.cs

--- CSharp FUNDAMENTALS/18. Exercise: List/02.ChangeList/Program.cs
+++ CSharp FUNDAMENTALS/18. Exercise: List/02.ChangeList/Program.cs
@@ -1,1 +1,23 @@
+List <int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
 
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    List<string> commandParts = command.Split(" ").ToList();
+    int element = int.Parse(commandParts[1]);
+
+    if (command.StartsWith("Delete"))
+    {
+        numbers.RemoveAll(number => number == element);
+    }
+    else if (command.StartsWith("Insert"))
+    {
+        int index = int.Parse(commandParts[2]);
+        numbers.Insert(index, element);
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
