ccff23ce55ccbd98b02549f05651a8fe090ea4a6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-15 03:14:59 +0100

    Update Demo_01.java

--- JAVA FUNDAMENTALS/25. Associative Arrays/Demo_docs/Demo_01.java
+++ JAVA FUNDAMENTALS/25. Associative Arrays/Demo_docs/Demo_01.java
@@ -6,9 +6,7 @@ public class Demo_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
         Map<String, Double> studentsMap = new HashMap<>();
-
      
         studentsMap.put("Ivan", 5.60);
         studentsMap.put("Petya", 4.30);
@@ -16,18 +14,13 @@ public class Demo_01 {
 
         System.out.println(studentsMap.size()); 
 
-     
         studentsMap.remove("Petya"); 
         studentsMap.remove("Georgi", 3.40); 
-
-       
+   
         System.out.println(studentsMap.isEmpty());
-
-
         System.out.println(studentsMap.containsKey("Desi"));
         System.out.println(studentsMap.containsValue(5.40));
 
-    
         studentsMap.clear();
     }
 }
