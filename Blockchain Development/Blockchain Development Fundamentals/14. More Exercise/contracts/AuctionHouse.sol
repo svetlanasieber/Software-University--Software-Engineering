6b6f08359d20eb5cb029e634bbc0d0e66e7933c5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-21 00:57:28 +0100

    Add files via upload

---
+++ Blockchain Development/Blockchain Development Fundamentals/14. More Exercise/contracts/AuctionHouse.sol
@@ -0,0 +1,229 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+struct Auction {
+    uint256 tokenId;
+    address nftAddress;
+    uint256 minPrice;
+    uint256 startTime;
+    uint256 endTime;
+    uint256 minBidIncr;
+    uint256 timeExtensionWindow;
+    uint256 timeExtensionIncr;
+    address seller;
+    bool nftClaimed;
+    bool rewardClaimed;
+}
+
+error CannotBeZero();
+error InvalidStartTime();
+error InvalidEndTime(string argument);
+error InsufficientBid();
+error NotInBidPeriod();
+error NotValidAuction();
+error AuctionUnfinished();
+error NotClaimer();
+error AlreadyClaimed();
+error HighestBidder();
+error NoWinner();
+error NotSeller();
+
+contract AuctionHouse is Ownable {
+    uint256 MIN_AUCTION_DURATION = 1 days;
+    uint256 MAX_AUCTION_DURATION = 60 days;
+
+    uint256 private _nextAuctionId;
+
+    mapping(uint256 => Auction) public auctions;
+    mapping(uint256 auctionId => mapping(address bidder => uint256 bid))
+        public bids;
+    mapping(uint256 auctionId => address highestBidder) public highestBidders;
+
+    event AuctionCreated(uint256 indexed auctionId);
+    event NewBid(
+        uint256 indexed auctionId,
+        address indexed bidder,
+        uint256 bid
+    );
+    event NftClaimed(uint256 indexed auctionId);
+    event BidClaimed(
+        uint256 indexed auctionId,
+        address indexed bidder,
+        uint256 bid
+    );
+    event RewardClaimed(uint256 indexed auctionId, uint256 reward, uint256 fee);
+
+    constructor() Ownable(msg.sender) {}
+
+    function createAuction(
+        uint256 tokenId,
+        address tokenAddress,
+        uint256 minPrice,
+        uint256 startTime,
+        uint256 endTime,
+        uint256 minBidIncr,
+        uint256 timeExtensionWindow,
+        uint256 timeExtensionIncr
+    ) external {
+        if (minPrice == 0) {
+            revert CannotBeZero();
+        }
+
+        if (startTime < block.timestamp) {
+            revert InvalidStartTime();
+        }
+
+        if (endTime < startTime + MIN_AUCTION_DURATION) {
+            revert InvalidEndTime("TOO_LOW");
+        }
+
+        if (endTime > startTime + MAX_AUCTION_DURATION) {
+            revert InvalidEndTime("TOO_HIGH");
+        }
+
+        uint256 auctionId = _nextAuctionId++;
+        auctions[auctionId] = Auction({
+            tokenId: tokenId,
+            nftAddress: tokenAddress,
+            minPrice: minPrice,
+            startTime: startTime,
+            endTime: endTime,
+            minBidIncr: minBidIncr,
+            timeExtensionWindow: timeExtensionWindow,
+            timeExtensionIncr: timeExtensionIncr,
+            seller: msg.sender,
+            nftClaimed: false,
+            rewardClaimed: false
+        });
+
+        // @note The NFT needs allowance
+        IERC721(tokenAddress).transferFrom(msg.sender, address(this), tokenId);
+
+        emit AuctionCreated(auctionId);
+    }
+
+    function bid(uint256 auctionId) external payable {
+        Auction storage auction = auctions[auctionId];
+
+        if (auction.nftAddress == address(0)) {
+            revert NotValidAuction();
+        }
+
+        if (
+            block.timestamp < auction.startTime ||
+            block.timestamp > auction.endTime
+        ) {
+            revert NotInBidPeriod();
+        }
+
+        uint256 newBid = bids[auctionId][msg.sender] + msg.value;
+        if (
+            newBid < auction.minPrice ||
+            (highestBidders[auctionId] != address(0) &&
+                newBid <
+                bids[auctionId][highestBidders[auctionId]] + auction.minBidIncr)
+        ) {
+            revert InsufficientBid();
+        }
+
+        if (block.timestamp > auction.endTime - auction.timeExtensionWindow) {
+            auction.endTime += auction.timeExtensionIncr;
+        }
+
+        bids[auctionId][msg.sender] = newBid;
+        highestBidders[auctionId] = msg.sender;
+
+        emit NewBid(auctionId, msg.sender, newBid);
+    }
+
+    function claimNFT(uint256 auctionId) external auctionFinished(auctionId) {
+        Auction storage auction = auctions[auctionId];
+
+        if (auction.nftClaimed) {
+            revert AlreadyClaimed();
+        }
+
+        address highestBidder = highestBidders[auctionId];
+        if (
+            msg.sender != highestBidder &&
+            highestBidders[auctionId] != address(0) &&
+            msg.sender != auction.seller
+        ) {
+            revert NotClaimer();
+        }
+
+        auction.nftClaimed = true;
+
+        address receiver = msg.sender == highestBidder
+            ? highestBidder
+            : auction.seller;
+        IERC721(auction.nftAddress).safeTransferFrom(
+            address(this),
+            receiver,
+            auction.tokenId
+        );
+
+        emit NftClaimed(auctionId);
+    }
+
+    function claimBid(uint256 auctionId) external {
+        uint256 userBid = bids[auctionId][msg.sender];
+
+        if (highestBidders[auctionId] == msg.sender) {
+            revert HighestBidder();
+        }
+
+        if (bids[auctionId][msg.sender] != 0) {
+            bids[auctionId][msg.sender] = 0;
+            _transferETH(payable(msg.sender), userBid);
+        }
+
+        emit BidClaimed(auctionId, msg.sender, userBid);
+    }
+
+    function claimReward(
+        uint256 auctionId
+    ) external auctionFinished(auctionId) {
+        uint256 reward = bids[auctionId][highestBidders[auctionId]];
+        if (reward == 0) {
+            revert NoWinner();
+        }
+
+        Auction storage auction = auctions[auctionId];
+        if (auction.rewardClaimed) {
+            revert AlreadyClaimed();
+        }
+
+        if (msg.sender != auction.seller) {
+            revert NotSeller();
+        }
+
+        auction.rewardClaimed = true;
+
+        uint256 fee = (reward / 100);
+        if (fee > 0) {
+            _transferETH(payable(owner()), fee);
+        }
+
+        _transferETH(payable(msg.sender), reward - fee);
+
+        emit RewardClaimed(auctionId, reward, fee);
+    }
+
+    function _transferETH(address payable to, uint256 amount) private {
+        (bool sent, ) = to.call{value: amount}("");
+
+        require(sent, "Failed to send Ether");
+    }
+
+    modifier auctionFinished(uint256 auctionId) {
+        if (block.timestamp <= auctions[auctionId].endTime) {
+            revert AuctionUnfinished();
+        }
+
+        _;
+    }
+}
---
+++ Blockchain Development/Blockchain Development Fundamentals/14. More Exercise/test/AuctionHouse.js
@@ -0,0 +1,185 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Crowdsale", function () {
+  const ONE_WEEK = 7 * 24 * 60 * 60;
+  const TOKEN_ID = 0;
+
+  async function initialDeployFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [seller, bidder] = await ethers.getSigners();
+
+    const CustomTokenFactory = await ethers.getContractFactory("NFT");
+    const nftContract = await CustomTokenFactory.deploy(seller.address);
+
+    const AuctionHouseFactory = await ethers.getContractFactory("AuctionHouse");
+    const auctionContract = await AuctionHouseFactory.deploy();
+
+    return {
+      nftContract,
+      auctionContract,
+      seller,
+      bidder,
+    };
+  }
+
+  describe("Create Auction", function () {
+    it("Should revert if min price < 0 ", async function () {
+      const { auctionContract, seller, nftContract } = await loadFixture(
+        initialDeployFixture
+      );
+
+      await expect(
+        auctionContract
+          .connect(seller)
+          .createAuction(
+            TOKEN_ID,
+            await nftContract.getAddress(),
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+          )
+      ).to.be.revertedWithCustomError(auctionContract, "CannotBeZero");
+    });
+    it("Should revert if start time < block.timestamp ", async function () {
+      const { auctionContract, seller, nftContract } = await loadFixture(
+        initialDeployFixture
+      );
+
+      const latest = await time.latest();
+      const next = latest + 2;
+      time.setNextBlockTimestamp(next);
+
+      await expect(
+        auctionContract
+          .connect(seller)
+          .createAuction(
+            TOKEN_ID,
+            await nftContract.getAddress(),
+            1,
+            next - 1,
+            0,
+            0,
+            0,
+            0
+          )
+      ).to.be.revertedWithCustomError(auctionContract, "InvalidStartTime");
+    });
+    it("Should revert if end time too low ", async function () {
+      const { auctionContract, seller, nftContract } = await loadFixture(
+        initialDeployFixture
+      );
+
+      const latest = await time.latest();
+      const next = latest + 2;
+      time.setNextBlockTimestamp(next);
+      const ONE_DAY = 24 * 60 * 60;
+
+      await expect(
+        auctionContract
+          .connect(seller)
+          .createAuction(
+            TOKEN_ID,
+            await nftContract.getAddress(),
+            1,
+            next,
+            next + ONE_DAY - 1,
+            0,
+            0,
+            0
+          )
+      )
+        .to.be.revertedWithCustomError(auctionContract, "InvalidEndTime")
+        .withArgs("TOO_LOW");
+    });
+    it("Should revert if end time too high ", async function () {
+      const { auctionContract, seller, nftContract } = await loadFixture(
+        initialDeployFixture
+      );
+
+      const latest = await time.latest();
+      const next = latest + 2;
+      time.setNextBlockTimestamp(next);
+      const SIXTY_DAY = 24 * 60 * 60 * 60;
+
+      await expect(
+        auctionContract
+          .connect(seller)
+          .createAuction(
+            TOKEN_ID,
+            await nftContract.getAddress(),
+            1,
+            next,
+            next + SIXTY_DAY + 1,
+            0,
+            0,
+            0
+          )
+      )
+        .to.be.revertedWithCustomError(auctionContract, "InvalidEndTime")
+        .withArgs("TOO_HIGH");
+    });
+
+    it("Should properly transfer NFT", async function () {
+      const { auctionContract, seller, nftContract } = await loadFixture(
+        initialDeployFixture
+      );
+
+      const nftContractAddress = await nftContract.getAddress();
+      const auctionContractAddress = await auctionContract.getAddress();
+
+      await nftContract
+        .connect(seller)
+        .approve(auctionContractAddress, TOKEN_ID);
+
+      expect(await nftContract.ownerOf(TOKEN_ID)).to.equal(seller.address);
+
+      const latest = await time.latest();
+      const next = latest + 2;
+      const SIXTY_DAY = 24 * 60 * 60 * 60;
+      time.setNextBlockTimestamp(next);
+
+      await expect(
+        auctionContract
+          .connect(seller)
+          .createAuction(
+            TOKEN_ID,
+            nftContractAddress,
+            1,
+            next,
+            next + SIXTY_DAY,
+            1,
+            1,
+            1
+          )
+      )
+        .to.emit(auctionContract, "AuctionCreated")
+        .withArgs(0);
+
+      expect(await nftContract.ownerOf(TOKEN_ID)).to.equal(
+        auctionContractAddress
+      );
+
+      const auction = await auctionContract.auctions(0);
+      expect(auction.tokenId).to.equal(TOKEN_ID);
+      expect(auction.nftAddress).to.equal(nftContractAddress);
+      expect(auction.minPrice).to.equal(1);
+      expect(auction.startTime).to.equal(next);
+      expect(auction.endTime).to.equal(next + SIXTY_DAY);
+      expect(auction.minBidIncr).to.equal(1);
+      expect(auction.timeExtensionWindow).to.equal(1);
+      expect(auction.timeExtensionIncr).to.equal(1);
+      expect(auction.seller).to.equal(seller.address);
+      expect(auction.nftClaimed).to.equal(false);
+      expect(auction.rewardClaimed).to.equal(false);
+    });
+  });
+});
