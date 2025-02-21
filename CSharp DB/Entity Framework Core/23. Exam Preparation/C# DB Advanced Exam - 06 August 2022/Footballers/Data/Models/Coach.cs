a59b68ad3b0a3b2c54c2a3505052fb6b92054fba
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-21 04:23:18 +0100

    Add files via upload

---
+++ CSharp DB/Entity Framework Core/23. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Footballers.Data.Models
+{
+    public class Coach
+    {
+        public Coach()
+        {
+            Footballers = new HashSet<Footballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Nationality { get; set; } = null!;
+
+        public virtual ICollection<Footballer> Footballers { get; set; } = null!;
+    }
+}
---
+++ CSharp DB/Entity Framework Core/23. Exam Preparation/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Team.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Footballers.Data.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            TeamsFootballers = new HashSet<TeamFootballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        [RegularExpression(@"[a-zA-z.\-\d\s]+$")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public int Trophies { get; set; }
+
+        public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; } = null!;
+    }
+}
