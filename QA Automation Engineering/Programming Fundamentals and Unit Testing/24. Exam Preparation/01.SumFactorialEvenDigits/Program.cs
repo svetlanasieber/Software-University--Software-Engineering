ebfab8862c9d69ab0153fc76333dd96e6ca9f7e6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-25 17:59:48 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Fundamentals and Unit Testing/24. Exam Preparation/01.SumFactorialEvenDigits/Program.cs
+++ QA Automation Engineering/Programming Fundamentals and Unit Testing/24. Exam Preparation/01.SumFactorialEvenDigits/Program.cs
@@ -1,1 +1,32 @@
+string number = Console.ReadLine(); 
 
+int sumFactorials = 0;
+
+foreach (var digit in number)
+{
+   
+    int currentDigit = int.Parse(digit.ToString());
+
+    if (currentDigit % 2 == 0)
+    {
+        sumFactorials += Factorial(currentDigit);
+    }
+}
+
+Console.WriteLine(sumFactorials);
+
+
+static int Factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+
+    int factorial = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        factorial *= i;
+    }
+
+    return factorial;
+}
