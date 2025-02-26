241544e1ed3814b17be05147d95292a1221627c2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-26 19:32:54 +0100

    Create CountRealNumbers_01.java

---
+++ JAVA FUNDAMENTALS/25. Associative Arrays/CountRealNumbers_01.java
@@ -0,0 +1,30 @@
+package Maps_AssocArr_Lab;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class CountRealNumbers_01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        TreeMap<Double, Integer> numbersCount = new TreeMap<>();
+
+        double[] nums = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToDouble(Double::parseDouble).toArray();
+
+        for (double num : nums) {
+            if (numbersCount.containsKey(num)){
+                numbersCount.put(num,numbersCount.get(num)+1);
+            }else {
+                numbersCount.put(num, 1);
+            }
+        }
+
+        for (Map.Entry<Double, Integer> kvp : numbersCount.entrySet()) {
+            System.out.printf("%.0f -> %d%n", kvp.getKey(),kvp.getValue());
+        }
+    }
+}
