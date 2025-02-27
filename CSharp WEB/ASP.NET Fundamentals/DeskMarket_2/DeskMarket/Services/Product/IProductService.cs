9be434f7342c05957bee2dfe91699b74ae4c2267
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-27 18:34:39 +0100

    Add files via upload

---
+++ CSharp WEB/ASP.NET Fundamentals/DeskMarket_2/DeskMarket/Services/Product/IProductService.cs
@@ -0,0 +1,27 @@
+﻿namespace DeskMarket.Services.Product
+{
+    using DeskMarket.Services.Product.Models;
+
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductServiceModel>> GetAllAsync();
+
+        Task<IEnumerable<ProductServiceModel>> GetCartAsync(string userId);
+
+        Task<ProductServiceModel?> GetModelByIdAsync(int id);
+
+        Task<ProductDetailsModel?> GetDetailsModelAsync(string? userId, int id);
+
+        Task<bool> ProductAlreadyAddedAsync(string userId, int productId);
+
+        Task AddToCartAsync(string userId, int id);
+
+        Task RemoveFromCartAsync(string userId, int id);
+
+        Task CreateAsync(ProductServiceModel model);
+
+        Task EditAsync(ProductServiceModel model);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ CSharp WEB/ASP.NET Fundamentals/DeskMarket_2/DeskMarket/Services/Product/Models/ProductServiceModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DeskMarket.Services.Product.Models
+{
+    public class ProductServiceModel
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; init; } = null!;
+
+        public string Description { get; init; } = null!;
+
+        public decimal Price { get; init; }
+
+        public string? ImageUrl { get; init; }
+
+        public string SellerId { get; set; } = null!;
+
+        public DateTime AddedOn { get; init; }
+
+        public int CategoryId { get; init; }
+
+        public bool IsDeleted { get; init; }
+    }
+}
