faf308333ec55218c44cae96e9e1f427d85f922d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-02 11:11:15 +0100

    Create Program.cs

---
+++ CSharp Basics/While Loop - Exercise/OldBooks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OldLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedBook = Console.ReadLine();
+            string input = Console.ReadLine();
+            int booksChecked = 0;
+
+            while (input != "No More Books")
+            {
+                if (input == searchedBook)
+                {
+                    Console.WriteLine($"You checked {booksChecked} books and found it.");
+                    return;
+                }
+
+                booksChecked++;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("The book you search is not here!");
+            Console.WriteLine($"You checked {booksChecked} books.");
+        }
+    }
+}
