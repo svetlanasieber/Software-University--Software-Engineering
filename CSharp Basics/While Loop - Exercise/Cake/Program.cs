a160307856c85b156c672e8cae8a3883cf46281a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-02 11:14:27 +0100

    Create Program.cs

---
+++ CSharp Basics/While Loop - Exercise/Cake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            int totalPieces = width * length;
+
+            while (input != "STOP")
+            {
+                int piecesTaken = int.Parse(input);
+                totalPieces -= piecesTaken;
+
+                if (totalPieces <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(totalPieces)} pieces more.");
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{totalPieces} pieces are left.");
+        }
+    }
+}
