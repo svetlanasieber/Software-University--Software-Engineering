fb3fa6047d735f834821db66d85cb9c6bed33ceb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-02 11:12:14 +0100

    Create Program.cs

---
+++ CSharp Basics/While Loop - Exercise/ExamPreparation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxBadGrades = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int badGradesCount = 0;
+            int gradesSum = 0;
+            int gradesCount = 0;
+            string problemName = "";
+
+            while (input != "Enough")
+            {
+                problemName = input;
+                int grade = int.Parse(Console.ReadLine());
+
+                if (grade <= 4)
+                {
+                    badGradesCount++;
+                }
+
+                if (badGradesCount == maxBadGrades)
+                {
+                    Console.WriteLine($"You need a break, {badGradesCount} poor grades.");
+                    return;
+                }
+
+                gradesCount++;
+                gradesSum += grade;
+                input = Console.ReadLine();
+            }
+
+            double avgScore = (double)gradesSum / gradesCount;
+            Console.WriteLine($"Average score: {avgScore:f2}");
+            Console.WriteLine($"Number of problems: {gradesCount}");
+            Console.WriteLine($"Last problem: {problemName}");
+        }
+    }
+}
