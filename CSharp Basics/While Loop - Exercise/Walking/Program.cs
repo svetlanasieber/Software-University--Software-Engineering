437a671350c427a17a5508f0d1ce88acceccc9e0
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-02 11:13:23 +0100

    Create Program.cs

---
+++ CSharp Basics/While Loop - Exercise/Walking/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Walk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int totalSteps = 0;
+
+            while (input != "Going home")
+            {
+                int steps = int.Parse(input);
+                totalSteps += steps;
+
+                if (totalSteps >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{totalSteps - 10000} steps over the goal!");
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int stepsToHome = int.Parse(Console.ReadLine());
+            totalSteps += stepsToHome;
+
+            if (totalSteps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{totalSteps - 10000} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - totalSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
