8edadd04031adf605b6f9c34a09be3f1810c47b4
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 22:58:54 +0100

    Update Main.java

--- JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Students/Main.java
+++ JAVA FUNDAMENTALS/23. Exercise: Objects and Classes/Students/Main.java
@@ -11,8 +11,8 @@ public class Main {
 
         for (int count = 1; count <= countStudents; count++) {
             String personalData = scanner.nextLine();
-            //"Lakia Eason 3.90".split(" ") -> ["Lakia", "Eason", "3.90"]
-            String[] splitData = personalData.split(" "); //["Lakia", "Eason", "3.90"]
+    
+            String[] splitData = personalData.split(" "); 
             String firstName = splitData[0];
             String lastName = splitData[1];
             double grade = Double.parseDouble(splitData[2]);
@@ -21,22 +21,13 @@ public class Main {
             studentList.add(student);
         }
 
-        //лист със студенти
 
-        //сортираме
-        //ascending order -> нарастващ ред
-        //descending order -> намаляващ ред
         studentList
-                .sort(Comparator.comparingDouble(Student::getGrade) //ascending order
-                .reversed()); //descending order
+                .sort(Comparator.comparingDouble(Student::getGrade) 
+                .reversed()); 
 
-        //отпечатаме
-        //"{first name} {last name}: {grade}"
         for (Student student : studentList) {
-            //начин 1: отпечатване на обект -> достъпваме всички полета и ги отпечатваме
-            //System.out.printf("%s %s: %.2f", student.getFirstName(), student.getLastName(), student.getGrade());
 
-            //начин 2
             System.out.println(student.toString());
         }
     }
