c8f1b087214e11b5fd486dfd8c34283229f0c71d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 22:54:16 +0100

    Update Main.java

--- JAVA FUNDAMENTALS/22. Objects and Classes/Students_02/Main.java
+++ JAVA FUNDAMENTALS/22. Objects and Classes/Students_02/Main.java
@@ -10,27 +10,25 @@ public class Main {
 
         String input = scanner.nextLine();
 
-        //Създаваме един празен списък с ученици
+
         List<Student_02> students = new ArrayList<>();
 
         while (!input.equals("end")){
 
             String[] currentData = input.split(" ");
 
-            //прочитаме конкретните елементи от масива
             String firstName = currentData[0];
             String lastName = currentData[1];
             int age = Integer.parseInt(currentData[2]);
             String homeTown = currentData[3];
 
-            //вземаме обект от списъка с ученици
             Student_02 currentStudent = getExistingStudent(students, firstName, lastName);
 
             if(currentStudent != null){
                 currentStudent.setAge(age);
                 currentStudent.setHometown(homeTown);
             }else {
-                //към списъка с ученици добавяме нашия текущ ученик
+                
                 currentStudent = new Student_02(firstName, lastName, age, homeTown);
                 students.add(currentStudent);
             }
