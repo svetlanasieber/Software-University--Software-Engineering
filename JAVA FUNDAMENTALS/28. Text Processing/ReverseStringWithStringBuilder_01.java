0afb24a04091c896579442a0fe8f7eacb3acb9de
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:08:49 +0100

    Update ReverseStringWithStringBuilder_01.java

--- JAVA FUNDAMENTALS/28. Text Processing/ReverseStringWithStringBuilder_01.java
+++ JAVA FUNDAMENTALS/28. Text Processing/ReverseStringWithStringBuilder_01.java
@@ -6,16 +6,13 @@ public class ReverseStringWithStringBuilder_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine(); //входни данни -> дума или "end"
+        String input = scanner.nextLine(); 
 
         while (!input.equals("end")) {
-            //input -> дума
-            //input = "hello"
+
             StringBuilder sb = new StringBuilder(input);
             sb.reverse();
 
-            //входяща дума -> input
-            //обърната дума -> reverseWord
             System.out.println(input + " = " + sb.toString());
 
             input = scanner.nextLine();
