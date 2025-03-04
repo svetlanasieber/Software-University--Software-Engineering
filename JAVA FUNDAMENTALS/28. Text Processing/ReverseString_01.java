27f251fb236efe294be5b37febe2f6750de97bea
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:09:38 +0100

    Update ReverseString_01.java

--- JAVA FUNDAMENTALS/28. Text Processing/ReverseString_01.java
+++ JAVA FUNDAMENTALS/28. Text Processing/ReverseString_01.java
@@ -6,20 +6,17 @@ public class ReverseString_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine(); //входни данни -> дума или "end"
+        String input = scanner.nextLine();
         while (!input.equals("end")) {
-            //input -> дума
-            //input = "hello"
-            String reverseWord = ""; //обърнатата дума
+
+            String reverseWord = ""; 
             for (int position = input.length() - 1; position >= 0; position--) {
                 char currentSymbol = input.charAt(position);
                 reverseWord = reverseWord + currentSymbol;
-                //reverseWord += currentSymbol;
+              
             }
 
-            //входяща дума -> input
-            //обърната дума -> reverseWord
-            System.out.println(input + " = " + reverseWord);
+             System.out.println(input + " = " + reverseWord);
 
             input = scanner.nextLine();
         }
