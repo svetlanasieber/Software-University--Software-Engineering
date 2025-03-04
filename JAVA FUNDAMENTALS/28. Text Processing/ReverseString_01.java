f3d85a70f361375278093046aec93b845550bd38
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:10:55 +0100

    Update ReverseString_01.java

--- JAVA FUNDAMENTALS/28. Text Processing/ReverseString_01.java
+++ JAVA FUNDAMENTALS/28. Text Processing/ReverseString_01.java
@@ -24,3 +24,33 @@ public class ReverseString_01 {
 
     }
 }
+
+/*
+
+package textProcessing;
+
+import java.util.Scanner;
+
+public class ReverseString_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        while (!input.equals("end")) {
+            String reverseWord = "";
+            for (int position = input.length() - 1; position >= 0; position--) {
+                char currentSymbol = input.charAt(position);
+                reverseWord = reverseWord + currentSymbol;
+           
+            }
+            System.out.println(input + " = " + reverseWord);
+
+            input = scanner.nextLine();
+        }
+
+
+    }
+}
+
+
+*/
