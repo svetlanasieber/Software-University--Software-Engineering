c9e6d4162ec50d1827d4444eb61580b99f3d52dd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:19:20 +0100

    Update Furniture_01.java

--- JAVA FUNDAMENTALS/32. Exercise: Regular Expressions/Furniture_01.java
+++ JAVA FUNDAMENTALS/32. Exercise: Regular Expressions/Furniture_01.java
@@ -9,41 +9,34 @@ import java.util.regex.Pattern;
 public class Furniture_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String regex = ">>(?<furnitureName>[A-Za-z]+)<<(?<price>[0-9]+\\.?[0-9]*)!(?<quantity>[0-9]+)"; //текст на шаблона
-        Pattern pattern = Pattern.compile(regex); //шаблон
+        String regex = ">>(?<furnitureName>[A-Za-z]+)<<(?<price>[0-9]+\\.?[0-9]*)!(?<quantity>[0-9]+)"; 
+        Pattern pattern = Pattern.compile(regex); 
 
-        List<String> furniture = new ArrayList<>(); //списък със закупените мебели
-        double totalPrice = 0; //общо изхарчена сума за закупените мебели
+        List<String> furniture = new ArrayList<>();
+        double totalPrice = 0; 
 
-        String input = scanner.nextLine(); //входни данни
+        String input = scanner.nextLine(); 
         while (!input.equals("Purchase")) {
-            //input = ">>Sofa<<312.23!3"
-            //проверка дали входните данни отговарят на шаблона
+
             Matcher matcher = pattern.matcher(input);
-            //matcher -> съвкупност от текстовете, които отговарят на шаблон
-            //matcher = [">>(?<furnitureName>Sofa)<<(?<price>312.23)!(?<quantity>3)"]
-            //find
-            //true -> input отговарят на шаблона
-            //false -> input не отговарят на шаблона
+
             if (matcher.find()) {
-                //input отговаря на шаблона -> валидни входни данни -> информация за закупена мебел
-                //matcher.find() = ">>(?<furnitureName>Sofa)<<(?<price>312.23)!(?<quantity>3)"
-                String furnitureName = matcher.group("furnitureName");//мебел -> "Sofa"
-                double price = Double.parseDouble(matcher.group("price"));//ед. цена -> "312.23" -> 312.23
-                int quantity =  Integer.parseInt(matcher.group("quantity"));//количество -> "3" -> 3
+     
+                String furnitureName = matcher.group("furnitureName");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity =  Integer.parseInt(matcher.group("quantity"));
 
                 furniture.add(furnitureName);
-                double pricePerFurniture = price * quantity; //сума за текуща мебел
+                double pricePerFurniture = price * quantity; 
                 totalPrice += pricePerFurniture;
             }
             input = scanner.nextLine();
         }
 
-        //списък със закупени мебели
+       
         System.out.println("Bought furniture:");
         furniture.forEach(System.out::println);
 
-        //обща сума за мебели
         System.out.printf("Total money spend: %.2f", totalPrice);
     }
 }
