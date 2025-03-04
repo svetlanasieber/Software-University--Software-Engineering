d3d5a355bc6b669c375bdc51780149971e56e06a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:18:34 +0100

    Update ExtractEmails_05.java

--- JAVA FUNDAMENTALS/32. Exercise: Regular Expressions/ExtractEmails_05.java
+++ JAVA FUNDAMENTALS/32. Exercise: Regular Expressions/ExtractEmails_05.java
@@ -8,19 +8,14 @@ public class ExtractEmails_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        //да намерим валидните имейли
-        //{user}@{host}
-        //desislava.topuzakova@softuni.bg
+
 
         String regexUser = "[A-Za-z0-9]+[\\.\\-\\_]?[A-Za-z0-9]+";
         String regexHost = "[A-Za-z]+\\-?[A-Za-z]+(\\.[A-Za-z]+\\-?[A-Za-z]+)+";
-        String regexEmail = regexUser + "@" + regexHost; //текст на шаблона
-        Pattern pattern = Pattern.compile(regexEmail); //шаблон
+        String regexEmail = regexUser + "@" + regexHost; 
+        Pattern pattern = Pattern.compile(regexEmail); 
 
         Matcher matcher = pattern.matcher(text);
-        //text = "Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information."
-        //matcher -> съвкупност от всички текстове, които ми отговарят на шаблона -> всички валидни имейли
-        //matcher -> ["s.miller@mit.edu", "j.hopking@york.ac.uk"]
 
         while (matcher.find()) {
             System.out.println(matcher.group());
