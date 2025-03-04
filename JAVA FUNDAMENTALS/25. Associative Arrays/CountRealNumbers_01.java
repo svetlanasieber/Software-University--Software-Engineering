3eda4a20b27775d035fac2cd11e936b52c36967d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:01:30 +0100

    Update CountRealNumbers_01.java

--- JAVA FUNDAMENTALS/25. Associative Arrays/CountRealNumbers_01.java
+++ JAVA FUNDAMENTALS/25. Associative Arrays/CountRealNumbers_01.java
@@ -1,5 +1,3 @@
-package maps;
-
 import java.util.*;
 
 public class CountRealNumbers_01 {
@@ -7,34 +5,24 @@ public class CountRealNumbers_01 {
         Scanner scanner = new Scanner(System.in);
 
         double [] numbers = Arrays.stream(scanner.nextLine().split(" "))
-                                    .mapToDouble(Double::parseDouble).toArray();
-        //numbers = [8, 2, 2, 8, 2]
 
-        //запис / двойка (pair): число -> бр. срещанията
         Map<Double, Integer> numbersCountMap = new TreeMap<>();
 
         for (double number : numbers) {
-            //1. не сме го срещали до момента -> нямаме го записано
+            
             if (!numbersCountMap.containsKey(number)) {
-                //първото срещане на числото
+               
                 numbersCountMap.put(number, 1);
             }
-            //2. вече да сме го срещали -> има го записано
+         
             else {
-                int currentCount = numbersCountMap.get(number);//текущия брой на срещанията
+                int currentCount = numbersCountMap.get(number);
                 numbersCountMap.put(number, currentCount + 1);
             }
         }
 
-        //принтиране на записите в map
-        //numbersCountMap:
-        //2 -> 3
-        //8 -> 2
-        //запис: key(число) -> value(бр. срещанията)
 
         for (Map.Entry<Double, Integer> pair : numbersCountMap.entrySet()) {
-            //всеки един запис е съхранен в pair
-            //pair: key (число)   value (бр. срещания)
             System.out.printf("%.0f -> %d%n", pair.getKey(), pair.getValue());
         }
 
