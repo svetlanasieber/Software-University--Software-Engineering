4adae15829f7dba33bd99bd500f74b1d4466589d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:08:21 +0100

    Update Digits_letters_Others_05.java

--- JAVA FUNDAMENTALS/28. Text Processing/Digits_letters_Others_05.java
+++ JAVA FUNDAMENTALS/28. Text Processing/Digits_letters_Others_05.java
@@ -6,24 +6,24 @@ public class Digits_letters_Others_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String text = scanner.nextLine(); //"Agd#53Dfg^&4F53"
+        String text = scanner.nextLine(); 
 
-        //цифри
+     
         StringBuilder digits = new StringBuilder();
-        //букви
+      
         StringBuilder letters = new StringBuilder();
-        //други символи
+    
         StringBuilder others = new StringBuilder();
 
         for (char symbol : text.toCharArray()) {
             if (Character.isDigit(symbol)) {
-                //символът ми е цифра
+               
                 digits.append(symbol);
             } else if (Character.isLetter(symbol)) {
-                //символът ми е буква
+               
                 letters.append(symbol);
             } else {
-                //символът ми е друг (различен от буква или цифра)
+                
                 others.append(symbol);
             }
         }
