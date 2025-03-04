8ee138ff86a35982e08f8960a550e4e36d0c79ce
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:19:48 +0100

    Update NetherRealms_04.java

--- JAVA FUNDAMENTALS/32. Exercise: Regular Expressions/NetherRealms_04.java
+++ JAVA FUNDAMENTALS/32. Exercise: Regular Expressions/NetherRealms_04.java
@@ -14,7 +14,6 @@ public class NetherRealms_04 {
             int health = getHealth(demonName);
             double damage = getDamage(demonName);
 
-            //модификации на damage
             for (char symbol : demonName.toCharArray()) {
                 if (symbol == '*') {
                     damage *= 2;
@@ -23,31 +22,29 @@ public class NetherRealms_04 {
                 }
             }
 
-            //отпечатваме за всеки демон
+     
             System.out.printf("%s - %d health, %.2f damage%n", demonName, health, damage);
         }
     }
 
-    //изчислява и връща damage
+
     private static double getDamage(String demonName) {
         double damage = 0;
-        //всички дробни и цели числа, които са положителни и отрицателни
+      
         String regexNumbers = "[-]?[0-9]+\\.?[0-9]*";
         Pattern pattern = Pattern.compile(regexNumbers);
         Matcher matcher = pattern.matcher(demonName);
-        //matcher -> съвкупност от всички тесктове, които ми отговарят на шаблона
-        //demonName = "M3ph-0.5s-0.5t0.0**"
-        //matcher = ["3", "-0.5", "-0.5", "0.0"]
+
         while (matcher.find()) {
             damage += Double.parseDouble(matcher.group());
         }
         return damage;
     }
 
-    //изчислява и връща health
+ 
     private static int getHealth(String demonName) {
         int health = 0;
-        //всички символи без: [0-9], +, -, ., *, /
+     
         for (char symbol : demonName.toCharArray()) {
             if (!Character.isDigit(symbol) && symbol != '+' && symbol != '-' && symbol != '*' && symbol != '.' && symbol != '/') {
                 health += (int)(symbol);
