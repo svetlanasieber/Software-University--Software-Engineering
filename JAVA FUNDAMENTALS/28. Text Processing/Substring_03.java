710de6ecce0f2048bbedd69a7c99603d12cd14c1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-04 23:09:54 +0100

    Update Substring_03.java

--- JAVA FUNDAMENTALS/28. Text Processing/Substring_03.java
+++ JAVA FUNDAMENTALS/28. Text Processing/Substring_03.java
@@ -10,15 +10,12 @@ public class Substring_03 {
         String secondText = scanner.nextLine();
 
         int index = secondText.indexOf(firstText);
-        //firstText = "ice"
-        //secondText = "kgb"
-        //ако firstText се съдържа в secondText -> на коя позиция се намира
-        //ако firstText НЕ СЕ СЪДЪРЖА в secondText -> -1
+
 
         while (index != -1) {
-            //firstText го има secondText
+          
             secondText = secondText.replace(firstText, "");
-            //след премахването -> търсим дали имаме тази дума
+
             index = secondText.indexOf(firstText);
         }
 
