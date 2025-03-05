8422411f150721a2aae923506b71ca4151f8a589
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-05 17:09:36 +0100

    Update CustomList.java

--- JAVA ADVANCED/18. Exercise: Generics/Exercise/CustomList/CustomList.java
+++ JAVA ADVANCED/18. Exercise: Generics/Exercise/CustomList/CustomList.java
@@ -36,8 +36,7 @@ public class CustomList<T extends Comparable<T>> {
     public int countGreaterValues(T elementToCompare) {
         int count = 0;
         for (T element : this.elements) {
-            // Сравняваме дали стойността от списъка е по-голяма от стойността, с която сравнявам
-            // първото > второто = 1 или повече
+
             int comparingResult = element.compareTo(elementToCompare);
             if (comparingResult > 0) {
                 count++;
