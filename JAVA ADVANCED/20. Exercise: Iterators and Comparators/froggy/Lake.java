b4c347342d937c6fa1ee927931a1701384acedfd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-05 17:15:20 +0100

    Update Lake.java

--- JAVA ADVANCED/20. Exercise: Iterators and Comparators/froggy/Lake.java
+++ JAVA ADVANCED/20. Exercise: Iterators and Comparators/froggy/Lake.java
@@ -3,7 +3,7 @@ package froggy;
 import java.util.Iterator;
 import java.util.List;
 
-// Iterable<тип на елемента, който ще обхождам в това езеро>
+
 public class Lake implements Iterable<Integer> {
 
     private List<Integer> numbers;
@@ -12,7 +12,7 @@ public class Lake implements Iterable<Integer> {
         this.numbers = numbers;
     }
 
-    // Това е нашата Жаба
+   
     @Override
     public Iterator<Integer> iterator() {
 
@@ -21,7 +21,7 @@ public class Lake implements Iterable<Integer> {
             private int evenIndex = 0;
             private int oddIndex = 1;
 
-            // Ако не съм стигнал до края с един от двата индекса -> все още имаме числа в езерото
+           
             @Override
             public boolean hasNext() {
                 return evenIndex < numbers.size() || oddIndex < numbers.size();
