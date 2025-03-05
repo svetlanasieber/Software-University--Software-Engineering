0e6e1bf52b13923b184f0cec7d309b32b7b80f75
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-05 01:49:34 +0100

    Add files via upload

---
+++ CSharp DB/Entity Framework Core/09. LINQ/SongsAboveDuration/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicHub.Data;
+
+public static class Configuration
+{
+    public static string ConnectionString =
+        @"Server=DESKTOP-S4MI5RU\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+}
---
+++ CSharp DB/Entity Framework Core/09. LINQ/SongsAboveDuration/Data/Models/Song.cs
@@ -0,0 +1,52 @@
+﻿namespace MusicHub.Data.Models;
+
+using MusicHub.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Song
+{
+    public Song()
+    {
+        this.SongPerformers = new HashSet<SongPerformer>();
+    }
+
+
+    [Key]
+    public int Id { get; set; }
+
+
+    //In EF <= 3.1.x we use [Required] attribute
+    //In EF >=6.x everything is required and we add "?" to be nullable
+    [MaxLength(ValidationConstants.SongNameMaxLength)]
+    public string Name { get; set; } = null!;//this is required
+
+
+    //TimeSpan datatype is required by default!
+    //In the DB this will be stored as BIGINT <=> Ticks count
+    public TimeSpan Duration { get; set; }//required by default
+
+    public DateTime CreatedOn { get; set; }//required by default
+
+
+    public Genre Genre { get; set; }//required by default 
+
+
+    [ForeignKey(nameof(Album))]
+    public int? AlbumId { get; set; }//not required, nullable int
+    public virtual Album? Album { get; set; }
+
+
+    [ForeignKey(nameof(Writer))]
+    public int WriterId { get; set; }
+    public virtual Writer Writer { get; set; } = null!;//required
+
+
+    public decimal Price { get; set; }
+
+
+    public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+
+
+}
+
