88343b8b15dc2e4aebb56d6edaee0580910f9fca
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 01:56:54 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P04TextFilter/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P04TextFilter/Program.cs
@@ -1,1 +1,14 @@
+string[] badWords = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+
+
+foreach (string badWord in badWords)
+{
+    string replacement = new string('*', badWord.Length);
+    text = text.Replace(badWord, replacement);
+}
+
+Console.WriteLine(text);
+
+
 
