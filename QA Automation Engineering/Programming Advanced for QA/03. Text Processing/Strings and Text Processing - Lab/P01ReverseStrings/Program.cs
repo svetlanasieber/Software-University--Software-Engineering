b29fdfd4887715a423b41e24d8254b41f9af2453
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 01:58:53 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P01ReverseStrings/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P01ReverseStrings/Program.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Linq;
+
+namespace 01Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string text = Console.ReadLine();
+
+                if (text == "end")
+                {
+                    break;
+                }
+				
+                string reversedText = null;
+
+                for(int i = text.Length-1; i >= 0;i--)
+                {
+                    reversedText+=text[i];
+                }
+
+                Console.WriteLine($"{text} = {reversedText}");
+            }
+        }
+    }
+}
 
