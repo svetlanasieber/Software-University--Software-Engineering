c1fc45943c91551315e0b093820f42862e071a34
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 02:01:19 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P05DigitsLettersAndOther/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P05DigitsLettersAndOther/Program.cs
@@ -1,1 +1,32 @@
+using System.Text;
+
+string text = Console.ReadLine(); 
+
+StringBuilder sbDigits = new StringBuilder();
+
+StringBuilder sbLetters = new StringBuilder();
+
+StringBuilder sbOthers = new StringBuilder();
+
+foreach(char symbol in text)
+{
+  
+    if (char.IsLetter(symbol))
+    {
+        sbLetters.Append(symbol);
+    }
+    else if (char.IsDigit(symbol))
+    {
+        sbDigits.Append(symbol);
+    }
+    else
+    {
+        sbOthers.Append(symbol);
+    }
+}
+
+
+Console.WriteLine(sbDigits);
+Console.WriteLine(sbLetters);
+Console.WriteLine(sbOthers);
 
