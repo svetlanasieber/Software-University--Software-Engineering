aa8bd9febd1f3b22dc6bbd7273a886f001ab924f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 18:52:25 +0100

    Add files via upload

---
+++ CSharp DB/Entity Framework Core/08. Exercise: Entity Relations/P02_FootballBetting.Common/ApplicationCommonConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace P02_FootballBetting.Common
+{
+    public static class ApplicationCommonConfiguration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-H3STVF4\SQLEXPRESS;Database=FootballBetting2025;Trusted_Connection=True;";
+    }
+}
---
+++ CSharp DB/Entity Framework Core/08. Exercise: Entity Relations/P02_FootballBetting/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace P02_FootballBetting
+{
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            // Main application (Console, Web, Android, iOS, Desktop)
+            Console.WriteLine("Db Creation Started...");
+
+            try
+            {
+                using FootballBettingContext dbContext = new FootballBettingContext();
+
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                Console.WriteLine("Db Creation was successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Db Creation failed!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
