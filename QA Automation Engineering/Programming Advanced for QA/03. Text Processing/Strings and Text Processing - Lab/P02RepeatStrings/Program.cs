3e46f03896b997f1bce09673842d1689e0ea33cb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 01:59:44 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P02RepeatStrings/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P02RepeatStrings/Program.cs
@@ -1,1 +1,15 @@
+string[] words = Console.ReadLine().Split(" ");
+string result = ""; 
+
+foreach (string word in words)
+{
+    int length = word.Length;
+
+    for (int count = 1; count <= length; count++)
+    {
+        result += word;
+    }
+}
+
+Console.WriteLine(result);
 
