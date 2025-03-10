76ba17ab9caf85023b51bf2a778f10f4635fc4b8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 02:00:31 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P03Substring/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P03Substring/Program.cs
@@ -1,1 +1,15 @@
+string wordToRemove = Console.ReadLine();
+string sentence = Console.ReadLine(); 
+
+while (sentence.Contains(wordToRemove))
+{
+    int positionWord = sentence.IndexOf(wordToRemove);
+    sentence = sentence.Remove(positionWord, wordToRemove.Length);
+}
+
+Console.WriteLine(sentence);
+
+
+
+
 
