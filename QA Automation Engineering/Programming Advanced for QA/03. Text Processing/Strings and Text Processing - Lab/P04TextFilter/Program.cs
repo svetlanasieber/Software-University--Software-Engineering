4d8e706135bd7ffccd7aa54492c835b7beb37053
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-10 01:57:53 +0100

    Update Program.cs

--- QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P04TextFilter/Program.cs
+++ QA Automation Engineering/Programming Advanced for QA/03. Text Processing/Strings and Text Processing - Lab/P04TextFilter/Program.cs
@@ -11,4 +11,37 @@ foreach (string badWord in badWords)
 Console.WriteLine(text);
 
 
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ");
+
+            string text = Console.ReadLine();
+
+            while (true)
+            {
+                for (int i = 0; i < bannedWords.Length; i++)
+                {
+                    while (text.Contains(bannedWords[i]))
+                    {
+                        string word = new string('*', bannedWords[i].Length);
+                        text = text.Replace(bannedWords[i], word);
+                    }
+                }
+				
+                Console.WriteLine(text);
+                break;
+            }
+        }
+    }
+}*/
+
 
