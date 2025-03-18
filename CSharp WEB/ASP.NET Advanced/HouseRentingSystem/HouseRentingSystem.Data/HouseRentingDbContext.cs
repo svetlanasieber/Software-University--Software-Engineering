c2469eb38faf9a86a89ee96e0bc21e30681d98bd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-18 19:57:29 +0100

    Add files via upload

---
+++ CSharp WEB/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Data/HouseRentingDbContext.cs
@@ -0,0 +1,41 @@
+﻿// ReSharper disable VirtualMemberCallInConstructor
+namespace HouseRentingSystem.Data
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using Configurations;
+    using Models;
+
+    public class HouseRentingDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        private readonly bool seedDb;
+
+        public HouseRentingDbContext(DbContextOptions<HouseRentingDbContext> options, bool seedDb = true)
+            : base(options)
+        {
+            this.seedDb = seedDb;
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<House> Houses { get; set; } = null!;
+
+        public DbSet<Agent> Agents { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+            builder.ApplyConfiguration(new HouseEntityConfiguration());
+
+            if (this.seedDb)
+            {
+                builder.ApplyConfiguration(new CategoryEntityConfiguration());
+                builder.ApplyConfiguration(new SeedHousesEntityConfiguration());
+            }
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
