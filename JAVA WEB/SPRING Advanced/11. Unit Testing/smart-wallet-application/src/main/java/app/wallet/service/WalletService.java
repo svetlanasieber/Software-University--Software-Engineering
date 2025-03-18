64f81816dfec328a3eff116cfdec7227b3dd65d9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-18 20:35:00 +0100

    Update WalletService.java

--- JAVA WEB/SPRING Advanced/11. Unit Testing/smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
+++ JAVA WEB/SPRING Advanced/11. Unit Testing/smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
@@ -49,10 +49,6 @@ public class WalletService {
         List<Wallet> allUserWallets = walletRepository.findAllByOwnerUsername(user.getUsername());
         Subscription activeSubscription = user.getSubscriptions().get(0);
 
-        // Pesho
-        // Subs: Premium
-        // Wallets: 2
-
         boolean isDefaultPlanAndMaxWalletsUnlocked = activeSubscription.getType() == SubscriptionType.DEFAULT && allUserWallets.size() == 1;
         boolean isPremiumPlanAndMaxWalletsUnlocked = activeSubscription.getType() == SubscriptionType.PREMIUM && allUserWallets.size() == 2;
         boolean isUltimatePlanAndMaxWalletsUnlocked = activeSubscription.getType() == SubscriptionType.ULTIMATE && allUserWallets.size() == 3;
@@ -147,10 +143,7 @@ public class WalletService {
                     "Invalid criteria for transfer");
         }
 
-        // Money Transfer
-        // Ivan -> Gosho | 20 EUR
-        // Ivan -20.00 EUR
-        // Gosho +20.00 EUR
+
 
         Transaction withdrawal = charge(sender, senderWallet.getId(), transferRequest.getAmount(), transferDescription);
         if (withdrawal.getStatus() == TransactionStatus.FAILED) {
@@ -210,7 +203,7 @@ public class WalletService {
 
         walletRepository.save(wallet);
 
-        // Успешно плащане
+        
         System.out.printf("Thread [%s]: Code in WalletService.class\n", Thread.currentThread().getName());
         PaymentNotificationEvent event = PaymentNotificationEvent.builder()
                 .userId(user.getId())
@@ -218,9 +211,7 @@ public class WalletService {
                 .email(user.getEmail())
                 .amount(amount)
                 .build();
-        // Ако искате да публикувате евент, просто разкоментирайте реда по-долу
-        // eventPublisher.publishEvent(event);
-
+   
         return transactionService.createNewTransaction(user,
                 wallet.getId().toString(),
                 SMART_WALLET_LTD,
