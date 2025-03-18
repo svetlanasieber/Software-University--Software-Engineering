e9662b46aadb1154276c18ea7beac5cd2b3ad31b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-18 19:41:15 +0100

    Add files via upload

---
+++ CSharp WEB/ASP.NET Fundamentals/CinemaApp/CinemaApp.Common/ApplicationConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace CinemaApp.Common
+{
+    public static class ApplicationConstants
+    {
+        public const int ReleaseYear = 2024;
+        public const string NoImageUrl = "/images/1.jpg";
+        public const string IsManagerCookieName = "IsManager";
+
+        public const string AdminRoleName = "Admin";
+
+        public const string ErrorMessage = "ErrorMessage";
+        public const string WarningMessage = "WarningMessage";
+        public const string SuccessMessage = "SuccessMessage";
+        public const string InfoMessage = "InfoMessage";
+    }
+}
