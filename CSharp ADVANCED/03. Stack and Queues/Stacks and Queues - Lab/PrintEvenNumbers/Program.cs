f6a271fb664a9bea3ff1856431dc6fbb4b01042e
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-26 01:05:22 +0100

    Update Program.cs

--- CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/PrintEvenNumbers/Program.cs
+++ CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/PrintEvenNumbers/Program.cs
@@ -1,1 +1,19 @@
+Queue<int> numbers = new(
+    Console.ReadLine()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse));
 
+while (numbers.Count > 0)
+{
+    int number = numbers.Dequeue();
+
+    if (number % 2 == 0)
+    {
+        Console.Write(number);
+
+        if (numbers.Count > 0)
+        {
+            Console.Write(", ");
+        }
+    }
+}
