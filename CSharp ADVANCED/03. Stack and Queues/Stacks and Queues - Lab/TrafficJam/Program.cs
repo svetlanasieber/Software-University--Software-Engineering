f09c1c4aef6d2b1f9f70af1b26d6f6d05767242b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-26 01:00:53 +0100

    Update Program.cs

--- CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/TrafficJam/Program.cs
+++ CSharp ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/TrafficJam/Program.cs
@@ -1,1 +1,26 @@
+using System;
+using System.Collections.Generic;
 
+internal class Program
+{
+    static void Main()
+    {
+        int carsPassingOnGreen = int.Parse(Console.ReadLine());
+        int totalCarsPassed = 0;
+        var carsQueue = new Queue<string>();
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            if (input == "green")
+                for (int i = 0; i < carsPassingOnGreen; i++)
+                {
+                    if (carsQueue.Count == 0) break;
+                    Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                    totalCarsPassed++;
+                }
+            else
+                carsQueue.Enqueue(input);
+        }
+        Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
+    }
+}
