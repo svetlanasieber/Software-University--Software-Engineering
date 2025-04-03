9fde670ab0686b1bbe5898f2babc9495ed197693
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 01:01:24 +0300

    Update PreparedMain.java

--- JAVA DB_/Spring Data/03. Java DB Apps Introduction/03_Connect/src/PreparedMain.java
+++ JAVA DB_/Spring Data/03. Java DB Apps Introduction/03_Connect/src/PreparedMain.java
@@ -8,10 +8,6 @@ public class PreparedMain {
         String user = "";
         String pass = "";
 
-        //jdbc connection string
-        //from demo: Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/soft_uni", props);
-        // String url = String.format("jdbc:mysql://%s:%s/dbName", host, port);
-
         Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/soft_uni", user, pass);
 
