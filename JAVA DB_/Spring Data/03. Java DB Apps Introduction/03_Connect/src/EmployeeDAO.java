d53fb27eb0fdd8f60550c174c60a438114b7e089
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 01:32:11 +0300

    Create EmployeeDAO.java

---
+++ JAVA DB_/Spring Data/03. Java DB Apps Introduction/03_Connect/src/EmployeeDAO.java
@@ -0,0 +1,9 @@
+public class EmployeeDAO {
+    public static void main(String[] args) {
+
+        //findById();
+        //findByFirstName();
+        //insert();
+        //hireIntoProject();
+    }
+}
