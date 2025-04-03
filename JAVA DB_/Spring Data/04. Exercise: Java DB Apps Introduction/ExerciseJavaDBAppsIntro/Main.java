ceedb9b94b74cb34680187f9ff1a7b31fcb58df6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-04 02:53:19 +0300

    Update Main.java

--- JAVA DB_/Spring Data/04. Exercise: Java DB Apps Introduction/ExerciseJavaDBAppsIntro/Main.java
+++ JAVA DB_/Spring Data/04. Exercise: Java DB Apps Introduction/ExerciseJavaDBAppsIntro/Main.java
@@ -1,1 +1,23 @@
+import java.sql.*;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "");
+        properties.setProperty("password", "");
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/minions_db", properties);
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name FROM villains");
+
+        while (resultSet.next()) {
+            String name = resultSet.getString("name");
+            System.out.printf("%s%n", name);
+        }
+            System.out.println();
+        }
+    }
 
