1ab1c8db96b704e3999f98b5ae962da3ca7b0c1d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-13 23:21:52 +0200

    Create Student.cs

---
+++ QA Automation Engineering/Programming Advanced for QA/10. Exercise: Objects and Classes/Students/Student.cs
@@ -0,0 +1,17 @@
+public class Student
+{
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+
+    public Student (string firstName, string lastName, double grade)
+    {
+
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+}
+
