d9fbd4c9fffae2c726d7d67ec08ae85067182521
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-23 18:30:27 +0200

    Delete JAVA ADVANCED/19. Iterators and Comparators directory

--- JAVA ADVANCED/19. Iterators and Comparators/BookComparatorByYearAscending.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class BookComparatorByYearAscending implements Comparator<Book> {
-    @Override
-    public int compare(Book f, Book s) {
-        return f.getYear() - s.getYear();
-    }
-}
