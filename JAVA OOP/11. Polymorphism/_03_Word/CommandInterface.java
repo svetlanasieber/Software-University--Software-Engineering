79a046b4b88895f9ee4b5f65528fdf5f2c27b215
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-23 18:01:40 +0200

    Delete JAVA OOP/11. Polymorphism/_03_Word directory

--- JAVA OOP/11. Polymorphism/_03_Word/CommandInterface.java
+++
@@ -1,7 +0,0 @@
-package _03_Word;
-
-public interface CommandInterface {
-    void init();
-
-    void handleInput(String input);
-}
--- JAVA OOP/11. Polymorphism/_03_Word/TextTransform.java
+++
@@ -1,5 +0,0 @@
-package _03_Word;
-
-public interface TextTransform {
-    void invokeOn(StringBuilder text, int startIndex, int endIndex);
-}
