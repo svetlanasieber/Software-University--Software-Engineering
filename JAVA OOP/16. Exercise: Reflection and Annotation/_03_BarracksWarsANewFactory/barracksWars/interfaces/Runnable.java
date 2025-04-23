e114981ed49f38dca8e52a85323489ad193df3e3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-23 18:03:53 +0200

    Delete JAVA OOP/16. Exercise: Reflection and Annotation/_03_BarracksWarsANewFactory directory

--- JAVA OOP/16. Exercise: Reflection and Annotation/_03_BarracksWarsANewFactory/barracksWars/interfaces/Runnable.java
+++
@@ -1,7 +0,0 @@
-package barracksWars.interfaces;
-
-import java.lang.reflect.InvocationTargetException;
-
-public interface Runnable {
-	void run() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
-}
