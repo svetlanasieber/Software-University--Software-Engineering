86274dbbfac21edc9b8859c8b39eff10e55a8210
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-23 18:02:24 +0200

    Delete JAVA OOP/13. SOLID/p02_OpenClosedPrinciple directory

--- JAVA OOP/13. SOLID/p02_OpenClosedPrinciple/p02_DrawingShape/interfaces/Shape.java
+++
@@ -1,5 +0,0 @@
-package _01_Lab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces;
-
-public interface Shape {
-
-}
--- JAVA OOP/13. SOLID/p02_OpenClosedPrinciple/p03_ShoppingCart/OnlineOrder.java
+++
@@ -1,8 +0,0 @@
-package _01_Lab.p02_OpenClosedPrinciple.p03_ShoppingCart;
-
-public class OnlineOrder extends Order {
-
-    protected OnlineOrder(Cart cart) {
-        super(cart);
-    }
-}
