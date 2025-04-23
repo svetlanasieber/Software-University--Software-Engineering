9628f714b7480a4c9be8789f9c0d2a9a9e00b1e6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-23 18:33:19 +0200

    Delete JAVA OOP/11. Polymorphism/vehicles directory

--- JAVA OOP/11. Polymorphism/vehicles/Car.java
+++
@@ -1,8 +0,0 @@
-package vehicles;
-
-public class Car extends AbstractVehicle{
-    private static final double SUMMER_FUEL_CONSUMPTION = 0.9;
-    public Car(double fuelQuantity, double fuelConsumption) {
-        super(fuelQuantity, fuelConsumption + SUMMER_FUEL_CONSUMPTION);
-    }
-}
--- JAVA OOP/11. Polymorphism/vehicles/Vehicle.java
+++
@@ -1,7 +0,0 @@
-package vehicles;
-
-public interface Vehicle {
-    String drive(double distance);
-     void refuel(double liters);
-
-}
