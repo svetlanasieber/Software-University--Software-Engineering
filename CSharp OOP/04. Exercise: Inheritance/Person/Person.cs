f21a5148ef85662f7b1b22c14a8de012b0fbfc12
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-04 02:22:21 +0200

    Add files via upload

---
+++ CSharp OOP/04. Exercise: Inheritance/Person/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Person;
+
+public class Person
+{
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name { get; }
+
+    public int Age { get; }
+
+    public override string ToString() => $"{this.GetType().Name} -> Name: {this.Name}, Age: {this.Age}";
+}
---
+++ CSharp OOP/04. Exercise: Inheritance/Person/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Person;
+
+using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        if(age < 0)
+        {
+            Console.WriteLine("Age must be positive!");
+            return;
+        }
+
+        Person person;
+        if (age > 15) person = new Person(name, age);
+        else person = new Child(name, age);
+
+        Console.WriteLine(person);
+    }
+}
