6532f8f5bfcb81fb3697e3b1539a8131d236f44d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-04 02:35:59 +0200

    Add files via upload

---
+++ CSharp FUNDAMENTALS/08. Data Types and Variables/Data Types and Variables - Lab/02.PoundsToDollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.PoundsToDollars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        decimal pounds = decimal.Parse(Console.ReadLine());
+        decimal rate = 1.31m;
+        decimal result = pounds * rate;
+
+        Console.WriteLine($"{result:f3}");
+    }
+}
