b4f0d8ec4fab53dac8efae717017fc3cd9cba021
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-09 17:02:00 +0200

    Add files via upload

---
+++ CSharp Basics/02-Conditional_Statements-lab/02-GreaterNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Max(num1, num2));
+        }
+    }
+}
