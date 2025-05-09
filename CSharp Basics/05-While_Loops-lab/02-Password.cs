c46f46f1f18dbb2bfe2030db6467a4fa7c65f872
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-09 17:03:18 +0200

    Add files via upload

---
+++ CSharp Basics/05-While_Loops-lab/02-Password.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string pass = Console.ReadLine();
+
+            string passGuess = Console.ReadLine();
+
+            while (passGuess != pass)
+            {
+                passGuess = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {username}!");
+        }
+    }
+}
---
+++ CSharp Basics/05-While_Loops-lab/08-Graduation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int badGradesCount = 0;
+            int year = 1;
+            double gradesSum = 0.0;
+
+            while (year <= 12)
+            {
+                double curGrade = double.Parse(Console.ReadLine());
+                gradesSum += curGrade;
+
+                if (curGrade < 4)
+                {
+                    badGradesCount++;
+                }
+                else
+                {
+                    year++;
+                }
+
+                if (badGradesCount > 1)
+                {
+                    Console.WriteLine($"{name} has been excluded at {year} grade");
+                    return;
+                }
+
+            }
+
+            Console.WriteLine($"{name} graduated. Average grade: {(gradesSum / 12):f2}");
+        }
+    }
+}
