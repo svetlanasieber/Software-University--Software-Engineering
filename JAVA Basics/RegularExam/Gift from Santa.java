cb317e7abba12c5f6714623b66f95eba1c72501c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-11 02:06:23 +0200

    Update Gift from Santa.java

--- JAVA Basics/RegularExam/Gift from Santa.java
+++ JAVA Basics/RegularExam/Gift from Santa.java
@@ -4,13 +4,11 @@ public class GiftsFromSanta_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
-  
         int N = Integer.parseInt(scanner.nextLine());
         int M = Integer.parseInt(scanner.nextLine());
         int S = Integer.parseInt(scanner.nextLine());
 
-        // Iterating from M to N
+
         for (int i = M; i >= N; i--) {
             if (i % 2 == 0 && i % 3 == 0) {
                 if (i == S) {
