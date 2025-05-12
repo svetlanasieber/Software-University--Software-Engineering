df9ca02807358cb8398f879865f9e6f6c145d1b7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:10:07 +0200

    Update Mammal.cs

--- QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Mammal.cs
+++ QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Mammal.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 namespace Zoo
 {
-	//описва всеки един бозайник
+
 	public class Mammal : Animal
 	{
-		//наследено поле: name
-		//наследено пропърти: Name
 
-		//конструктор
+		
 		public Mammal (string name) : base(name)
 		{
-			//нов празен обект
-			//name = null
 
 		}
 	}
