371df64c0361b472ee5814fa5f462a32063cc0f7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:09:29 +0200

    Update Gorilla.cs

--- QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Gorilla.cs
+++ QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Gorilla.cs
@@ -3,13 +3,11 @@ namespace Zoo
 {
 	public class Gorilla : Mammal
 	{
-		//наследено поле: name
-		//наследено пропърти: Name
+
 
 		public Gorilla (string name) : base(name)
 		{
-			//нов празен обект
-			//name = null
+
 		}
 	}
 }
