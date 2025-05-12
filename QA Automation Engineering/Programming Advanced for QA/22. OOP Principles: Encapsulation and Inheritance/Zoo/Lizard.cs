ba8bf9d2bfd6c639071a6078298b5f55547c27bc
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:09:40 +0200

    Update Lizard.cs

--- QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Lizard.cs
+++ QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Lizard.cs
@@ -3,13 +3,11 @@ namespace Zoo
 {
 	public class Lizard : Reptile
 	{
-        //наследено поле: name
-        //наследено пропърти: Name
+
 
         public Lizard(string name) : base(name)
         {
-            //нов празен обект
-            //name = null
+
         }
     }
 }
