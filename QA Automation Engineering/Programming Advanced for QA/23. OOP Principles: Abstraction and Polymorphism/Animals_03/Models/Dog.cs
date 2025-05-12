28b5626a80812f505e848a0fd3f94f4e87090403
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:05:06 +0200

    Update Dog.cs

--- QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Animals_03/Models/Dog.cs
+++ QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Animals_03/Models/Dog.cs
@@ -3,17 +3,11 @@ namespace Animals.Models
 {
     public class Dog : Animal
     {
-        //наследени полета: name, favouriteFood
 
-        //конструктор
         public Dog(string name, string favouriteFood) : base(name, favouriteFood)
         {
-            //нов празен обект
-            //name = null
-            //favouriteFood = null
         }
 
-        //наследени методи: abstract ExplainSelf
         public override void ExplainSelf()
         {
             Console.WriteLine($"I am {Name} and my favourite food is {FavouriteFood}");
