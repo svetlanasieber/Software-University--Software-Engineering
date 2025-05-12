4b4b64b578eb783b47c526e82cfdbfee3946574d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:04:01 +0200

    Add files via upload

---
+++ QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Animals_03/Models/Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Animals.Models
+{
+    //клас Cat наследява клас Animal
+    public class Cat : Animal
+    {
+
+        //наследени полета: name, favouriteFood
+
+        //конструктор
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+            //нов празен обект
+            //name = null
+            //favouriteFood = null
+        }
+
+        //наследени методи: abstract ExplainSelf
+        public override void ExplainSelf()
+        {
+            Console.WriteLine($"I am {Name} and my favourite food is {FavouriteFood}");
+            Console.WriteLine("MEEOW");
+        }
+    }
+}
+
---
+++ QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Animals_03/Models/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Animals.Models
+{
+    public class Dog : Animal
+    {
+        //наследени полета: name, favouriteFood
+
+        //конструктор
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+            //нов празен обект
+            //name = null
+            //favouriteFood = null
+        }
+
+        //наследени методи: abstract ExplainSelf
+        public override void ExplainSelf()
+        {
+            Console.WriteLine($"I am {Name} and my favourite food is {FavouriteFood}");
+            Console.WriteLine("BORK");
+        }
+    }
+}
+
