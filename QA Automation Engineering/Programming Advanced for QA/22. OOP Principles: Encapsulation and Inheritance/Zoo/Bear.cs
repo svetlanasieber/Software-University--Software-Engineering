bdfa86808e875b86f4b055e5aa0240690832664a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:09:18 +0200

    Update Bear.cs

--- QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Bear.cs
+++ QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Bear.cs
@@ -3,13 +3,11 @@ namespace Zoo
 {
 	public class Bear : Mammal
 	{
-        //наследено поле: name
-        //наследено пропърти: Name
+
 
         public Bear(string name) : base(name)
         {
-            //нов празен обект
-            //name = null
+
         }
     }
 }
