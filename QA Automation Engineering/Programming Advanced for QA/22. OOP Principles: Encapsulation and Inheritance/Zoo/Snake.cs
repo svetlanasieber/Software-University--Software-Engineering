46da8b674334e6605031a90aed0302b10ce8e98f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:10:33 +0200

    Update Snake.cs

--- QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Snake.cs
+++ QA Automation Engineering/Programming Advanced for QA/22. OOP Principles: Encapsulation and Inheritance/Zoo/Snake.cs
@@ -3,13 +3,11 @@ namespace Zoo
 {
 	public class Snake : Reptile
 	{
-        //наследено поле: name
-        //наследено пропърти: Name
+
 
         public Snake(string name) : base(name)
         {
-            //нов празен обект
-            //name = null
+
         }
     }
 }
