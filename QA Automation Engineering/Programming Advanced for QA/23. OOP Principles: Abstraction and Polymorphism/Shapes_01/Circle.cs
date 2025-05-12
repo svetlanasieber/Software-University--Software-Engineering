3ec818eaf796b58cc99e924607ab397619ad3f6d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:06:52 +0200

    Add files via upload

---
+++ QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Shapes_01/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Shapes
+{
+    //наследяване на клас - достъп до всички полета и методи
+    //имплементиране на interface - възможност да изпълним всички методи в interface
+    //описва всеки един кръг (геометрична фигура)
+
+
+    //клас Circle импелементира интерфейса IDrawable
+    //-> кръгът ще има възможността да се чертае
+    //-> кръгът да изпълни всички методи свързани с чертаенето
+    public class Circle : IDrawable
+    {
+        //характерстики -> радиус
+        private int radius;
+        public int Radius { get; set; }
+
+        //конструктор
+        public Circle (int radius)
+        {
+            this.Radius = radius;
+        }
+
+        //действия
+        public void Draw()
+        {
+            //дваме детайли как точно се чертае кръг
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Shapes_01/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Shapes
+{
+    //описва всеки един правоъгълник (геометрична фигура)
+    public class Rectangle : IDrawable
+    {
+        //характеристики
+        private int height;
+        private int width;
+
+        public int Height { get; set;}
+        public int Width { get; set; }
+
+        //конструктор
+        public Rectangle(int height, int width)
+        {
+            //нов празен обект
+            this.Height = height;
+            this.Width = width;
+        }
+
+        //действия
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; i++)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; i++)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
+
