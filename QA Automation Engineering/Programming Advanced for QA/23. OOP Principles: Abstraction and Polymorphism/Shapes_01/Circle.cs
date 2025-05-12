123362f31285e49159547f480f0791510fa2703f
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-12 22:07:31 +0200

    Update Circle.cs

--- QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Shapes_01/Circle.cs
+++ QA Automation Engineering/Programming Advanced for QA/23. OOP Principles: Abstraction and Polymorphism/Shapes_01/Circle.cs
@@ -1,30 +1,23 @@
 ﻿using System;
 namespace Shapes
 {
-    //наследяване на клас - достъп до всички полета и методи
-    //имплементиране на interface - възможност да изпълним всички методи в interface
-    //описва всеки един кръг (геометрична фигура)
 
-
-    //клас Circle импелементира интерфейса IDrawable
-    //-> кръгът ще има възможността да се чертае
-    //-> кръгът да изпълни всички методи свързани с чертаенето
     public class Circle : IDrawable
     {
-        //характерстики -> радиус
+       
         private int radius;
         public int Radius { get; set; }
 
-        //конструктор
+       
         public Circle (int radius)
         {
             this.Radius = radius;
         }
 
-        //действия
+    
         public void Draw()
         {
-            //дваме детайли как точно се чертае кръг
+          
             double rIn = this.radius - 0.4;
             double rOut = this.radius + 0.4;
             for (double y = this.radius; y >= -this.radius; --y)
