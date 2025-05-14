3d82796b9ac37f5c8140ac3feff30155638d0950
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-14 22:16:38 +0200

    Update MatchingBrackets_04.java

--- JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/MatchingBrackets_04.java
+++ JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/MatchingBrackets_04.java
@@ -1,1 +1,25 @@
+package stack_and_queues_lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class MatchingBrackets_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for(int position = 0; position <= expression.length() - 1; position++) {
+            char currentSymbol = expression.charAt(position);
+            if (currentSymbol == '(') {
+                stack.push(position);
+            } else if (currentSymbol == ')') {
+                int positionOpenBracket = stack.pop();
+                System.out.println(expression.substring(positionOpenBracket, position + 1));
+            }
+        }
+    }
+}
 
