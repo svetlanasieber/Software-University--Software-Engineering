02d891f0e99e49fa0031cd24600a27ee642a038c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-14 20:42:07 +0200

    Update BrowserHistory_01.java

--- JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/BrowserHistory_01.java
+++ JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/BrowserHistory_01.java
@@ -1,1 +1,33 @@
+package stack_and_queues_lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistory_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String currentURL = "";
+        ArrayDeque<String> browserHistory = new ArrayDeque<String>();
+
+        String command = scanner.nextLine();
+        while (!command.equals("Home")) {
+            if (command.equals("back")) {
+
+                if (browserHistory.size() <= 1) {
+                    System.out.println("no previous URLs");
+                }
+                else {
+                    browserHistory.pop(); 
+                    currentURL = browserHistory.peek();
+                    System.out.println(currentURL);
+                }
+            } else {
+                currentURL = command;
+                System.out.println(currentURL);
+                browserHistory.push(currentURL);
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
 
