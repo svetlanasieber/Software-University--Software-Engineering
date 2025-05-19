e11831f32d38d28ecbca3ee4c7bedd76c7054d28
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 01:21:27 +0200

    Update SumMatrixElements_04.java

--- JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/SumMatrixElements_04.java
+++ JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/SumMatrixElements_04.java
@@ -1,28 +1,46 @@
+package multidimensional_arrays_Lab;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumMatrixElements_04 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        int[] dimension = Arrays.stream(scanner.nextLine().split(", "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        String dimensions = scanner.nextLine();
+        int rows = Integer.parseInt(dimensions.split(", ")[0]);
+        int cols = Integer.parseInt(dimensions.split(", ")[1]);
 
-        int rows = dimension[0];
-        int cols = dimension[1];
-        int totalSum = 0;
+        int[][] matrix = new int[rows][cols];
 
-        for (int i = 0; i < rows; i++) {
-            int sum = Arrays.stream(scanner.nextLine().split(", "))
-                    .mapToInt(Integer::parseInt)
-                    .sum();
-            totalSum += sum;
+        fillMatrix(matrix, scanner);
 
-        }
+        int sum = getSumMatrixElements(matrix);
+
         System.out.println(rows);
         System.out.println(cols);
-        System.out.println(totalSum);
+        System.out.println(sum);
+
+    }
+
+    private static void fillMatrix(int[][] matrix, Scanner scanner) {
+        for (int row = 0; row <= matrix.length - 1; row++) {
+            matrix[row] = Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt).toArray();
+        }
+    }
+
+    private static int getSumMatrixElements(int[][] matrix) {
+        int sum = 0; 
+
+        for (int row = 0; row <= matrix.length -1 ; row++) {
+            for (int col = 0; col <= matrix[0].length -1 ; col++) {
+                int currentElement = matrix[row][col];
+                sum += currentElement;
+            }
+        }
+
+        return sum;
+
     }
 }
