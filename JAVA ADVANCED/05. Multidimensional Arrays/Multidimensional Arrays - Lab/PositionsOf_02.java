30af54c404c7a1c9f6ec1262e5f8e5b83e8c6784
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 00:37:02 +0200

    Update PositionsOf_02.java

--- JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/PositionsOf_02.java
+++ JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/PositionsOf_02.java
@@ -1,1 +1,46 @@
+package multidimensional_arrays_Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class PositionsOf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String matrixDimension = scanner.nextLine();
+
+        int rows = Integer.parseInt(matrixDimension.split(" ")[0]);
+        int cols = Integer.parseInt(matrixDimension.split(" ")[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        fillMatrix(matrix, scanner);
+        
+        int number = Integer.parseInt(scanner.nextLine());
+        
+        boolean isFound = false;
+        for (int row = 0; row <= matrix.length -1; row++) {
+            for (int col = 0; col <= matrix[0].length -1; col++) {
+                int currentElement = matrix[row][col];
+                if (currentElement == number) {
+                    System.out.println(row + " " + col);
+                    isFound = true;
+                }
+            }
+        }
+        
+        if (!isFound) {
+            System.out.println("not found");
+        }
+        
+    }
+
+    private static void fillMatrix(int[][] matrix, Scanner scanner) {
+        for (int row = 0; row <= matrix.length - 1; row++) {
+            matrix[row] = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+        }
+    }
+}
 
