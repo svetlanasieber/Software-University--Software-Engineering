d1e0ae218ed4642ee84135b767c19a6712f32060
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-19 23:44:48 +0200

    Update SimpleCalculator_02.java

--- JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/SimpleCalculator_02.java
+++ JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/SimpleCalculator_02.java
@@ -1,1 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
 
+public class _SimpleCalculator_02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputArr = scanner.nextLine().split(" ");
+
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+
+        for (int i = 0; i < inputArr.length; i++) {
+            String symbol = inputArr[i];
+            if (Character.isDigit(symbol.charAt(0))) {
+                numbers.push(Integer.parseInt(symbol));
+            } else {
+                int rightDigit = Integer.parseInt(inputArr[++i]);
+                int leftDigit = numbers.getFirst();
+                numbers.push(rightDigit);
+
+                int result = symbol.equals("+")
+                        ? leftDigit + rightDigit 
+                        : leftDigit - rightDigit; 
+
+                numbers.push(result);
+            }
+        }
+
+        System.out.println(numbers.peek());
+    }
+}
