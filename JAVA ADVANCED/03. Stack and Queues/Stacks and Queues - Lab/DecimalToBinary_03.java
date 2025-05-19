3811443c4f91c51ab33fa7bebcf82f8c3446a776
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-19 23:45:16 +0200

    Update DecimalToBinary_03.java

--- JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/DecimalToBinary_03.java
+++ JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/DecimalToBinary_03.java
@@ -1,1 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
 
+public class DecimalToBinary_03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(Integer.toBinaryString(number));
+
+    }
+}
