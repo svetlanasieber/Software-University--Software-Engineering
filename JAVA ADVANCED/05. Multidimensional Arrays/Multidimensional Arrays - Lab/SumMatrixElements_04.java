c790ba269b6e34eabc70abee52bb08ee49105681
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-19 16:24:55 +0200

    Update SumMatrixElements_04.java

--- JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/SumMatrixElements_04.java
+++ JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/SumMatrixElements_04.java
@@ -1,1 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class SumMatrixElements_04 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dimension = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int rows = dimension[0];
+        int cols = dimension[1];
+        int totalSum = 0;
+
+        for (int i = 0; i < rows; i++) {
+            int sum = Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+            totalSum += sum;
+
+        }
+        System.out.println(rows);
+        System.out.println(cols);
+        System.out.println(totalSum);
+    }
+}
