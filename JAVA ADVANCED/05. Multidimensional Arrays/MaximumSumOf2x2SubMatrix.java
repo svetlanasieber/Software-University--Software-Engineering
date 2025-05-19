d0c716af5bc4a77205e401328291412d2d3e0a92
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 01:25:24 +0200

    Update MaximumSumOf2x2SubMatrix.java

--- JAVA ADVANCED/05. Multidimensional Arrays/MaximumSumOf2x2SubMatrix.java
+++ JAVA ADVANCED/05. Multidimensional Arrays/MaximumSumOf2x2SubMatrix.java
@@ -1,1 +1,78 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaximumSumOf2X2SubMatrix {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matrix = readMatrix(scanner);
+
+        int[][] maxSubMatrix = getMaxSumSubMatrix2By2(matrix);
+
+        printMatrix(maxSubMatrix);
+
+        System.out.println(getElementsSum(maxSubMatrix));
+
+    }
+    public static int[][] readMatrix(Scanner scanner) {
+        int[] rowsAndCols = readArray(scanner.nextLine());
+        int rows = rowsAndCols[0];
+        int cols = rowsAndCols[1];
+        int[][] matrix = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            matrix[row] = readArray(scanner.nextLine());
+        }
+        return matrix;
+    }
+
+    public static int[] readArray(String line) {
+        return Arrays.stream(line.split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    public static  int[][] getMaxSumSubMatrix2By2(int[][] matrix) {
+        int maxSum = 0;
+        int bestRow = 0;
+        int bestCol = 0;
+
+        for (int row = 0; row < matrix.length - 1; row++) {
+            for (int col = 0; col < matrix[row].length - 1; col++) {
+                int sum =
+                        matrix[row][col] +
+                                matrix[row][col + 1] +
+                                matrix[row + 1][col] +
+                                matrix[row + 1][col + 1];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    bestRow = row;
+                    bestCol = col;
+                }
+            }
+        }
+        return new int[][] {
+                {matrix[bestRow][bestCol], matrix[bestRow][bestCol + 1]},
+                {matrix[bestRow + 1][bestCol], matrix[bestRow + 1][bestCol + 1]}
+        };
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int[] arr : matrix) {
+            for (int element : arr) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+    private static int getElementsSum(int[][] matrix) {
+        int sum = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                sum += matrix[row][col];
+            }
+        }
+        return sum;
+    }
+}
 
