62256a84ea5c275496bf693b5731729a9a75b1d5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 19:44:16 +0200

    Update ParkingLot.java

--- JAVA ADVANCED/07. Sets and Dictionaries Advanced/Sets And Maps Advanced - Lab/ParkingLot.java
+++ JAVA ADVANCED/07. Sets and Dictionaries Advanced/Sets And Maps Advanced - Lab/ParkingLot.java
@@ -1,1 +1,39 @@
+package sets_and_maps_advanced_lab;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> carNumbers = new LinkedHashSet<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("END")) {
+
+            String direction = input.split(", ")[0];
+            String carNumber = input.split(", ")[1];
+
+            if (direction.equals("IN")) {
+                carNumbers.add(carNumber);
+            } else {
+                carNumbers.remove(carNumber);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        if (carNumbers.isEmpty()) {
+            System.out.println("Parking Lot is Empty");
+        } else {
+            for (String carNumber : carNumbers) {
+                System.out.println(carNumber);
+            }
+        }
+    }
+}
 
