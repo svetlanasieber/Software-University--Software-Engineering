d33af1dc493f72a7aff1ae66c3ff1877f0c7df2b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 21:56:42 +0200

    Update ReverseNumbersWithAStack.java

--- JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/ReverseNumbersWithAStack.java
+++ JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/ReverseNumbersWithAStack.java
@@ -1,1 +1,24 @@
+package stack_and_queues_exercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] tokens = scanner.nextLine().split("\\s+");
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        for (String number : tokens) {
+            stack.push(number);
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
+}
 
