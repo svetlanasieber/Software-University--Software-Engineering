2d861fe3fd7b71f85297ee93a397294aced620a6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 14:30:12 +0200

    Update MaximumSumOf2x2Submatrix.java

--- JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/MaximumSumOf2x2Submatrix.java
+++ JAVA ADVANCED/05. Multidimensional Arrays/Multidimensional Arrays - Lab/MaximumSumOf2x2Submatrix.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MaximumSumOfSubmatrix_05 {
+public class MaximumSumOfSubmatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
