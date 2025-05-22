da6c9054d814ae4b0c1dce1ed7c2a05f7551404b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-23 01:02:27 +0200

    Update CountSymbols_04.java

--- JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/CountSymbols_04.java
+++ JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/CountSymbols_04.java
@@ -1,1 +1,30 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
+public class CountSymbols_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Map<Character, Integer> result = new TreeMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if(!result.containsKey(currentChar)){
+                result.put(currentChar, 1);
+            }else {
+                int currentCount = result.get(currentChar);
+                currentCount++;
+                result.put(currentChar, currentCount);
+            }
+
+        }
+
+        result.entrySet().stream().forEach(e -> System.out.printf("%s: %d time/s%n", e.getKey(), e.getValue()));
+
+
+    }
+}
