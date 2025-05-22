0d3b4f21346602b6dedd7b27fa8b9f241e7751e1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-23 01:47:57 +0200

    Update FixEmails_06.java

--- JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/FixEmails_06.java
+++ JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/FixEmails_06.java
@@ -1,1 +1,30 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FixEmails_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        String email = "";
+
+        Map<String, String> collection = new LinkedHashMap<>();
+
+        while (!command.equals("stop")) {
+
+            email = scanner.nextLine();
+
+            if (!email.endsWith("uk") && !email.endsWith("us") && !email.endsWith("com")) {
+
+                collection.put(command, email);
+            }
+
+            command = scanner.nextLine();
+        }
+
+        collection.forEach((key, value) -> System.out.printf("%s -> %s%n", key, value));
+
+    }
+}
 
