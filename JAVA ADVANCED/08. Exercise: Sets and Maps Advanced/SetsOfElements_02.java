b72bd269bed8cf1f44a91d68bb89d6452c9edcbd
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-23 00:01:25 +0200

    Update SetsOfElements_02.java

--- JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/SetsOfElements_02.java
+++ JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/SetsOfElements_02.java
@@ -1,1 +1,34 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class SetsOfElements_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int countElementsFirstSet = Integer.parseInt(input.split("\\s+")[0]);
+        int countElementsSecondSet = Integer.parseInt(input.split("\\s+")[1]);
+
+        Set<Integer> firstSet = new LinkedHashSet<>();
+        Set<Integer> secondSet = new LinkedHashSet<>();
+
+        fillSet(countElementsFirstSet, scanner, firstSet);
+        fillSet(countElementsSecondSet, scanner, secondSet);
+
+
+        for (int numberFromFirstSet : firstSet) {
+            if (secondSet.contains(numberFromFirstSet)) {
+                System.out.print(numberFromFirstSet + " ");
+            }
+        }
+    }
+
+    private static void fillSet(int countElements, Scanner scanner, Set<Integer> secondSet) {
+        for (int i = 1; i <= countElements; i++) {
+            int currentNumber = Integer.parseInt(scanner.nextLine());
+            secondSet.add(currentNumber);
+        }
+    }
+}
