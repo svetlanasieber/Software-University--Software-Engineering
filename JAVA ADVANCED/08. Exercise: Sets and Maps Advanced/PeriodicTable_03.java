fc0d3432fc41b9376eeda362a64ab4df88daf1c7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-23 00:24:39 +0200

    Update PeriodicTable_03.java

--- JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/PeriodicTable_03.java
+++ JAVA ADVANCED/08. Exercise: Sets and Maps Advanced/PeriodicTable_03.java
@@ -1,1 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PeriodicTable_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countChemicalCompounds = Integer.parseInt(scanner.nextLine());
+
+        Set<String> chemicalElements = new TreeSet<>();
+
+        for (int i = 1; i <= countChemicalCompounds; i++) {
+
+            //String[] currentChemicalCompound = scanner.nextLine().split("\\s+ ");
+            chemicalElements.addAll(Arrays.asList(scanner.nextLine().split("\\s+")));
+        }
+
+        chemicalElements.forEach(e -> System.out.printf("%s ", e));
+
+    }
+}
 
