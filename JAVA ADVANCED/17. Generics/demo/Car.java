6963dcc095ad133f5c226bc73fc8195dd33787d9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-06-07 00:22:25 +0200

    Create Car.java

---
+++ JAVA ADVANCED/17. Generics/demo/Car.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class Car {
+
+    private String brand;
+    private String color;
+    private int year;
+
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
