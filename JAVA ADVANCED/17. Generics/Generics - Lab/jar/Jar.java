5c3d384d44d755ed0099864d9fb0656a6843e0ee
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-06-06 23:21:41 +0200

    Update Jar.java

--- JAVA ADVANCED/17. Generics/Generics - Lab/jar/Jar.java
+++ JAVA ADVANCED/17. Generics/Generics - Lab/jar/Jar.java
@@ -2,16 +2,12 @@ package jar;
 
 import java.util.ArrayDeque;
 
-// Generic parameter - очаква да бъде заменен с някакъв тип данни
 public class Jar<T> {
 
-    //При създаване на обект от този клас, полетата се инициализират със стойност null
     private ArrayDeque<T> elements;
 
-
-    //конструктор
     public Jar() {
-        //създавам един празен стек срещу това поле
+  
         this.elements = new ArrayDeque<>();
 
     }
