0facf3c1384324357d805fae919a3c43b1a0e450
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-06-18 01:55:55 +0200

    Update Main.java

--- JAVA DB_/Spring Data/03. Java DB Apps Introduction/03_Custom_Demo/src/Main.java
+++ JAVA DB_/Spring Data/03. Java DB Apps Introduction/03_Custom_Demo/src/Main.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        // Connect to SQL Server
+
         String jdbc = "jdbc:mysql://localhost:3306/soft_uni";
         String username = "root";
         String password = "12345";
@@ -20,12 +20,12 @@ public class Main {
         Connection connection =
             DriverManager.getConnection(jdbc, properties);
 
-        // Execute query
+       
         PreparedStatement preparedStatement = connection
             .prepareStatement("SELECT * FROM employees WHERE salary > 100000 LIMIT 10");
         ResultSet resultSet = preparedStatement.executeQuery();
 
-        // Parse result
+    
         while (resultSet.next()) {
             String firstName = resultSet.getString("first_name");
             String jobTitle = resultSet.getString("job_title");
