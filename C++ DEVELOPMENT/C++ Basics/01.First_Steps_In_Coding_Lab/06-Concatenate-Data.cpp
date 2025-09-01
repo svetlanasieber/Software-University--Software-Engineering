69468a14c5fe0eb99e7222644336514c564de249
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-01 23:04:39 +0200

    Add files via upload

---
+++ C++ DEVELOPMENT/C++ Basics/01.First_Steps_In_Coding_Lab/06-Concatenate-Data.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//1. въвеждаме: име, фамилия, възраст, град
+	//2. отпечатваме: "You are <firstName> <lastName>, a <age>-years old person from <town>."
+
+	string name;
+	cin >> name;
+
+	string lastName;
+	cin >> lastName;
+
+	int age;
+	cin >> age;
+
+	string town;
+	cin >> town;
+
+	cout << "You are " << name << " " << lastName << ", a " << age << "-years old person from " << town << ".";
+}
