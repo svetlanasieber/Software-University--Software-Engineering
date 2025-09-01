f98f2a168b8321fd0b3a49383a8a759a4e551e81
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-01 23:04:18 +0200

    Add files via upload

---
+++ C++ DEVELOPMENT/C++ Basics/01.First_Steps_In_Coding_Lab/04-Inches-to-Cm.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//1. въвеждаме инчове
+	//2. преобразуваме инчове в см
+	//3. отпечатваме см
+
+	double inches;
+	cin >> inches;
+
+	double cm = inches * 2.54;
+
+	cout << cm << endl;
+
+	return 0;
+}
