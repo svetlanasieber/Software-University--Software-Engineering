caef5276e5d5d8164f60d9e8f35f5b35c64106df
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-01 23:05:20 +0200

    Add files via upload

---
+++ C++ DEVELOPMENT/C++ Basics/08-Pet-Shop.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//1. въвеждаме брой храна за кучета и брой храна за котки
+	//2. изчисляваме обща сума = сума за кучета + сума котки
+	//3. отпечатваме: "{крайната сума} lv."
+
+	int countDogs;
+	cin >> countDogs;
+
+	int countCats;
+	cin >> countCats;
+
+	double sum = (countDogs * 2.50) + (countCats * 4);
+
+	cout << sum << " lv.";
+}
