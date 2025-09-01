1f3024530194ba7e7ef9275ebc2cb17138d9672f
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-01 23:03:14 +0200

    Add files via upload

---
+++ C++ DEVELOPMENT/C++ Basics/01.First_Steps_In_Coding_Lab/05-Greeting-by-name.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//1. въвежда име
+	//2. отпечатва "Hello, <name>!"
+
+	string name;
+	cin >> name;
+
+	cout << "Hello, " << name << "!" << endl;
+
+	return 0;
+}
