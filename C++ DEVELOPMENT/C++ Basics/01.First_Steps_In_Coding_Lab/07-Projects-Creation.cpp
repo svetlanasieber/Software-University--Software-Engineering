9e411aee4e2d86bbcf9c643ee140b0e63635a222
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-01 23:05:02 +0200

    Add files via upload

---
+++ C++ DEVELOPMENT/C++ Basics/01.First_Steps_In_Coding_Lab/07-Projects-Creation.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+
+	//1. въвеждаме име на архитект и брой проекти
+	//2. изчисляваме време за изпълнение
+	//3. отпечатваме: "The architect {името на архитекта} will need {необходими часове} hours to complete {брой на проектите} project/s."
+
+	string architectName;
+	cin >> architectName;
+
+	int countProjects;
+	cin >> countProjects;
+
+	int time = countProjects * 3;
+
+	cout << "The architect " << architectName << " will need " << time << " hours to complete " << countProjects << " project/s.";
+}
