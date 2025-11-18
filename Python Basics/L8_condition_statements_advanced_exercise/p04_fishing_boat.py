147b9785360d9a8e125988d1dbef5ef51ae883e9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-18 02:10:05 +0100

    Remove extra whitespace and clean up code

--- Python Basics/L8_condition_statements_advanced_exercise/p04_fishing_boat.py
+++ Python Basics/L8_condition_statements_advanced_exercise/p04_fishing_boat.py
@@ -5,7 +5,7 @@ number_of_fishermans = int(input())
 boat_rent = 0
 if season == "Spring":
     boat_rent = 3000
-elif season == "Summer" or season == "Autumn": # да се изписва винаги променливата с нещо си, или променливата равно нещо си.
+elif season == "Summer" or season == "Autumn":
     boat_rent = 4200
 elif season == "Winter":
     boat_rent = 2600
@@ -18,7 +18,7 @@ else: #number_of_fishermans >= 12
     boat_rent *= 0.75
 
 
-if number_of_fishermans % 2 == 0 and season != "Autumn": # трябва и от двете страни условията да са ни True
+if number_of_fishermans % 2 == 0 and season != "Autumn":
     boat_rent *= 0.95
 
 
@@ -27,6 +27,7 @@ if budget >= boat_rent:
     print(f"Yes! You have {difference:.2f} leva left.")
 else:
     print(f"Not enough money! You need {difference:.2f} leva.")
+
 
 
 
