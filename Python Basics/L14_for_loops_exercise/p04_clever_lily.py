2d5191f80e03f38aa034f219eb107bd689abd0fd
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-18 02:14:37 +0100

    Refactor code formatting in p04_clever_lily.py

--- Python Basics/L14_for_loops_exercise/p04_clever_lily.py
+++ Python Basics/L14_for_loops_exercise/p04_clever_lily.py
@@ -9,19 +9,20 @@ toys_count = 0
 
 
 for i in range(1, age + 1):
-    if i % 2 == 0:  # Even year, she receives money
-        total_money += money_gift  # Add money gift to total
-        money_gift += 10  # Increase the money gift for the next even year
-        total_money -= 1  # Brother takes 1 unit of money
-    else:  # Odd year, she receives a toy
+    if i % 2 == 0: 
+        total_money += money_gift  
+        money_gift += 10 
+        total_money -= 1 
+    else:  
         toys_count += 1
 
-# Add money from selling toys
+
 total_money += toys_count * toy_price
 
-# Check if she can buy the washing machine
+
 if total_money >= washing_machine_price:
     print(f"Yes! {total_money - washing_machine_price:.2f}")
 else:
     print(f"No! {washing_machine_price - total_money:.2f}")
+
 
