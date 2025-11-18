2d218f132e553f9229e863d61c59d21c6cd0245d
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-18 02:10:21 +0100

    Update p04_fishing_boat.py

--- Python Basics/L8_condition_statements_advanced_exercise/p04_fishing_boat.py
+++ Python Basics/L8_condition_statements_advanced_exercise/p04_fishing_boat.py
@@ -11,9 +11,9 @@ elif season == "Winter":
     boat_rent = 2600
 
 if number_of_fishermans <= 6:
-    boat_rent *= 0.9 # това е отстъпката от 10%
+    boat_rent *= 0.9
 elif number_of_fishermans <= 11:
-    boat_rent *= 0.85 # това е отстъпката от 15%
+    boat_rent *= 0.85 
 else: #number_of_fishermans >= 12
     boat_rent *= 0.75
 
@@ -27,6 +27,7 @@ if budget >= boat_rent:
     print(f"Yes! You have {difference:.2f} leva left.")
 else:
     print(f"Not enough money! You need {difference:.2f} leva.")
+
 
 
 
