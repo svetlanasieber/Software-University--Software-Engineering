14531304c0b5e0a81156132fe6afde39e756efa6
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-12-05 16:15:53 +0100

    Implement printer queue functionality

--- JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/PrinterQueue_05.java
+++ JAVA ADVANCED/03. Stack and Queues/Stacks and Queues - Lab/PrinterQueue_05.java
@@ -1,1 +1,36 @@
+package stack_queue;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class PrinterQueue_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("print")) {
+          
+            if (input.equals("cancel")) {
+              
+                if (printerQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled " + printerQueue.poll());
+                }
+            } else {
+               
+                printerQueue.offer(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+
+        while (!printerQueue.isEmpty()) {
+            System.out.println(printerQueue.poll());
+        }
+    }
+}
+
 
