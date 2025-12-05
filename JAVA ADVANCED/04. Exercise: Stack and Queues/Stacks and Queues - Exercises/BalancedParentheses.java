15f00feecc7a793e534b54ecae5cb44f29c23a1e
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-12-05 16:17:17 +0100

    Update BalancedParentheses.java

--- JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/BalancedParentheses.java
+++ JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/BalancedParentheses.java
@@ -1,1 +1,42 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
 
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ArrayDeque<Character> stackOpenBrackets = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentBracket = input.charAt(i);
+
+            if(currentBracket == '(' || currentBracket == '[' || currentBracket == '{'){
+                stackOpenBrackets.push(currentBracket);
+            }else {
+                if(stackOpenBrackets.isEmpty() || !machBrackets(stackOpenBrackets.pop(), currentBracket)){
+                    System.out.println("NO");
+                    return;
+                }
+            }
+
+
+        }
+
+        System.out.println("YES");
+
+    }
+
+    private static boolean machBrackets(Character openBracketFromStack, char closedBracket) {
+
+        if(openBracketFromStack == '('){
+            return closedBracket == ')';
+        } else if (openBracketFromStack == '[') {
+            return closedBracket == ']';
+
+        }else {
+            return closedBracket == '}';
+        }
+    }
+}
