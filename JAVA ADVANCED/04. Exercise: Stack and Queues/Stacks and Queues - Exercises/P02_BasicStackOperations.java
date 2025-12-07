8fabc250a8fcb48124d10ded52ecd42e50b680f7
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-12-07 23:29:24 +0100

    Implement basic stack operations with user input

--- JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/P02_BasicStackOperations.java
+++ JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/P02_BasicStackOperations.java
@@ -1,1 +1,36 @@
+package stack_and_queues_exercise;
+
+import java.util.*;
+
+public class P02_BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // n-5 s-2 x-13
+        int[] input = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int n = input[0];
+        int s = input[1];
+        int x = input[2];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int count = 1; count <= n; count++) {
+            int number = scanner.nextInt();
+            stack.push(number);
+        }
+
+        for (int count = 1; count <= s; count++) {
+            stack.pop();
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println(0);
+        } else if (stack.contains(x)) {
+            System.out.println(true);
+        } else {
+            System.out.println(Collections.min(stack));
+        }
+    }
+}
 
