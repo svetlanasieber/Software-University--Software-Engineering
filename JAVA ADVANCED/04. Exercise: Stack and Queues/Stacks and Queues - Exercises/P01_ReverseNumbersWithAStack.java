7dcc2de6d9cedbd9a3f7bcd59300f49521d4efff
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-12-07 22:12:37 +0100

    Create P01_ReverseNumbersWithAStack.java

---
+++ JAVA ADVANCED/04. Exercise: Stack and Queues/Stacks and Queues - Exercises/P01_ReverseNumbersWithAStack.java
@@ -0,0 +1,22 @@
+package stack_and_queues_exercise;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01_ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(stack::push);
+
+
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
