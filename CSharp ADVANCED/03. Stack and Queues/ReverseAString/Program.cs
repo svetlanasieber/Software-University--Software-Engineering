ecd41494ad105b0d5529b8da0df034f2735e7ffa
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2026-01-09 12:31:36 +0100

    Update Program.cs

--- CSharp ADVANCED/03. Stack and Queues/ReverseAString/Program.cs
+++ CSharp ADVANCED/03. Stack and Queues/ReverseAString/Program.cs
@@ -1,1 +1,15 @@
+string text = Console.ReadLine();
 
+//Stack<string> stack = new Stack<string>();
+Stack<char> stack = new Stack<char>();
+
+foreach (char symbol in text)
+{
+    stack.Push(symbol);
+}
+
+while (stack.Count > 0)
+{
+    var symbol = stack.Pop();
+    Console.Write(symbol);
+}
