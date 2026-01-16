be6a9c645a4fa115ae59ad29018294265a4efd16
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2026-01-16 16:44:11 +0100

    Update SoftUniParty.java

--- JAVA ADVANCED/07. Sets and Dictionaries Advanced/Sets And Maps Advanced - Lab/SoftUniParty.java
+++ JAVA ADVANCED/07. Sets and Dictionaries Advanced/Sets And Maps Advanced - Lab/SoftUniParty.java
@@ -1,1 +1,47 @@
+package sets_and_maps_advanced;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        Set<String> vipReservations = new TreeSet<>();
+        Set<String> regularReservations = new TreeSet<>();
+
+        String reservationNumber = scanner.nextLine();
+         while (!reservationNumber.equals("PARTY")) {
+             char firstSymbol = reservationNumber.charAt(0);
+
+             if (Character.isDigit(firstSymbol)) {
+                 vipReservations.add(reservationNumber);
+             } else {
+                 regularReservations.add(reservationNumber);
+             }
+             reservationNumber = scanner.nextLine();
+         }
+         //Party
+
+        String goneReservations = scanner.nextLine();
+         while (!goneReservations.equals("END")) {
+             vipReservations.remove(goneReservations);
+             regularReservations.remove(goneReservations);
+
+             goneReservations = scanner.nextLine();
+         }
+
+         //END
+
+        System.out.println(vipReservations.size() + regularReservations.size());
+
+         vipReservations.forEach(System.out::println);
+         regularReservations.forEach(System.out::println);
+
+        }
+
+
+    }
+
 
