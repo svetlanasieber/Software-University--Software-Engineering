4ab40cf11b29353eca19a12880f10d6f4dbf6df9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2026-01-17 17:51:53 +0100

    Update WarNumberGame.java

--- JAVA ADVANCED/07. Sets and Dictionaries Advanced/Sets And Maps Advanced - Lab/WarNumberGame.java
+++ JAVA ADVANCED/07. Sets and Dictionaries Advanced/Sets And Maps Advanced - Lab/WarNumberGame.java
@@ -1,1 +1,48 @@
+package sets_and_maps_advanced;
+
+import java.util.*;
+
+public class WarNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Integer> firstPlayerCards = new LinkedHashSet<>();
+        Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).forEach(number -> firstPlayerCards.add(number));
+
+        Set<Integer> secondPlayerCards = new LinkedHashSet<>();
+        Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).forEach(number -> secondPlayerCards.add(number));
+
+
+        int rounds = 50;
+        while (rounds > 0 && !firstPlayerCards.isEmpty() && !secondPlayerCards.isEmpty()) {
+
+            int firstPlayerCard = firstPlayerCards.iterator().next();
+            firstPlayerCards.remove(firstPlayerCard);
+
+            int secondPlayerCard = secondPlayerCards.iterator().next();
+            secondPlayerCards.remove(secondPlayerCard);
+
+            if (firstPlayerCard > secondPlayerCard) {
+                firstPlayerCards.add(firstPlayerCard);
+                firstPlayerCards.add(secondPlayerCard);
+            } else if (secondPlayerCard > firstPlayerCard) {
+                secondPlayerCards.add(firstPlayerCard);
+                secondPlayerCards.add(secondPlayerCard);
+            }
+
+
+            rounds--;
+        }
+
+        if (firstPlayerCards.size() > secondPlayerCards.size()) {
+            System.out.println("First player win!");
+        } else if (secondPlayerCards.size() > firstPlayerCards.size()) {
+            System.out.println("Second player win!");
+        } else {
+            System.out.println("Draw!");
+        }
+
+    }
+
+}
 
